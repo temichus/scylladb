54274de5e0d1faf38f15682f8f466b661dc8d71c
Shlomi Livne <shlomi@cloudius-systems.com>
2015-08-11 08:52:48 +0300

    Add a test, add a delay after stop
    Signed-off-by: Shlomi Livne <shlomi@cloudius-systems.com>

--- simple_boot_shutdown.py
+++ simple_boot_shutdown.py
@@ -30,6 +30,8 @@ class TestSimpleBootShutdown(Tester):
         """)
 
         node1.stop()
+        # FIXME remove sleep and add a check to veirfy node has stopped
+        time.sleep(30)
 
         node1.start(update_pid=True)       
         cursor = self.patient_cql_connection(node1,'ks')
@@ -52,3 +54,43 @@ class TestSimpleBootShutdown(Tester):
 
         assert len(res) == 0, res
 
+    def boot_create_keyspace_table_insert_shutdown_select_test(self):
+        cluster = self.prepare()
+        cluster.populate(1).start()
+        node1 = cluster.nodelist()[0]
+
+        cursor = self.patient_cql_connection(node1)
+        self.create_ks(cursor, 'ks', 1)
+
+        cursor.execute("""
+            CREATE TABLE test1 (
+                k int PRIMARY KEY,
+                c int
+            )
+        """)
+
+
+        cursor.execute("insert into ks.test1  (k,c) values (1,2);")
+
+        node1.flush()
+        node1.stop()
+        # FIXME remove sleep and add a check to veirfy node has stopped
+        time.sleep(30)
+
+        node1.start(update_pid=True)       
+        cursor = self.patient_cql_connection(node1,'ks')
+        # Select
+        res = cursor.execute("""
+                SELECT * FROM ks.test1
+                WHERE k=1
+        """)
+
+        assert len(res) == 1, res
+
+        # Select
+        res = cursor.execute("""
+                SELECT * FROM ks.test1
+                WHERE k=2
+        """)
+
+        assert len(res) == 0, res
