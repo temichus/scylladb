d91f515db844031ed26034cde3b0061dcd02a0df
Shlomi Livne <shlomi@cloudius-systems.com>
2015-08-31 18:01:07 +0300

    Add a simple commitlog replay test
    Signed-off-by: Shlomi Livne <shlomi@cloudius-systems.com>

--- simple_boot_shutdown.py
+++ simple_boot_shutdown.py
@@ -90,3 +90,45 @@ class TestSimpleBootShutdown(Tester):
         """)
 
         assert len(res) == 0, res
+
+
+    def boot_create_keyspace_table_insert_shutdown_commitlog_replay_select_test(self):
+        cluster = self.prepare()
+        cluster.populate(1).start()
+        node1 = cluster.nodelist()[0]
+
+        cursor = self.patient_cql_connection(node1)
+        self.create_ks(cursor, 'ks', 1)
+
+        cursor.execute("""
+            CREATE TABLE test1 (
+                k int PRIMARY KEY,
+                c int
+            )
+        """)
+
+
+        cursor.execute("insert into ks.test1  (k,c) values (1,2);")
+
+        # wait for the commitlog to be fsynched
+        time.sleep(10)
+        node1.stop(gently=False)
+
+        node1.start(update_pid=True)
+        cursor = self.patient_cql_connection(node1,'ks')
+        # Select
+        res = cursor.execute("""
+                SELECT * FROM ks.test1
+                WHERE k=1
+        """)
+
+        assert len(res) == 1, res
+
+        # Select
+        res = cursor.execute("""
+                SELECT * FROM ks.test1
+                WHERE k=2
+        """)
+
+        assert len(res) == 0, res
+
