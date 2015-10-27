2fba7b7b76141aa34c901f0223bcad75a60939e3
Asias He <asias@scylladb.com>
2015-10-27 18:04:04 +0800

    Add env file scylla_dtest_env.sh
    This env file helps to set up the *all* the environment variables
    required to run dtest, including the one for scylla-ccm.
    
    And gives a simple example how to run a dtest.

---
+++ scylla_dtest_env.sh
@@ -0,0 +1,15 @@
+CCM_DIR=../urchin-ccm;
+export PATH=$CCM_DIR:$PATH
+export PERL5LIB=$CCM_DIR
+export PYTHONPATH=$CCM_DIR
+export CASSANDRA_DIR=`pwd`/../scylla
+echo "Examples:"
+echo "=== To run a simple boot and shutdown test:"
+echo 'nosetests -v -s simple_boot_shutdown.py'
+echo
+echo "=== To run a test and keep the cluster data"
+echo 'KEEP_TEST_DIR=true REUSE_CLUSTER=false nosetests -v -s simple_boot_shutdown.py'
+echo
+echo "=== To remove dtest produced clsuter data"
+echo "ls -l ~/.dtest; rm -rf ~/.dtest"
+
