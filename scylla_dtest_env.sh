d50bbb052a4de8a8c4f11d2cbe67fb881f45c430
Asias He <asias@scylladb.com>
2015-11-06 10:52:23 +0800

    Add CASSANDRA_DIR pointing to scylla-tools-java
    It is an example to run dtest against c*.

--- scylla_dtest_env.sh
+++ scylla_dtest_env.sh
@@ -2,6 +2,7 @@ CCM_DIR=../scylla-ccm;
 export PATH=$CCM_DIR:$PATH
 export PERL5LIB=$CCM_DIR
 export PYTHONPATH=$CCM_DIR
+export CASSANDRA_DIR=`pwd`/../scylla-tools-java
 export CASSANDRA_DIR=`pwd`/../scylla
 echo "Examples:"
 echo "=== To run a simple boot and shutdown test:"
