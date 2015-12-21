ebc193ff61c84b0f615c07703a969fa2d29b53b8
Shlomi Livne <shlomi@scylladb.com>
2015-12-21 10:55:47 +0200

    Add a decomission test that validates cache invalidation
    Invalidation of cache is required when ownership of key range is
    transfered between nodes. The added test completes a cycle of:
    1. Cluster of a single node with data
    2. Adding a new node (taking ownership of a range)
    3. Delete all data
    4. Decomissioning the new node (returing owenrship of a range)
    
    Relate to issue scylla#484
    
    Signed-off-by: Shlomi Livne <shlomi@scylladb.com>

--- update_cluster_layout_tests.py
+++ update_cluster_layout_tests.py
@@ -93,68 +93,6 @@ class TestUpdateClusterLayout(Tester):
         self.check_rows_on_node(node2, 2000)
         self.check_rows_on_node(node1, 2000)
 
-    def simple_add_node_2_test(self):
-        """
-        We are using the row_cache to rvalue the number of entries in each node
-        We do not yet support the nodetool cleanup operation that removes old data - yet the cache sould be cleared
-        If the cache is not cleared then if a range is returned the data will be wroung
-
-        Test bootstrapped node streams part of its data
-        1. Create a cluster with a single node with rf=1,insert data
-        2. Check the row cahe can be used as an estimator
-        3. Add a new node
-        4. Check that all data can be read
-        5. Check that the sum of cache entires on both nodes is logical
-        """
-
-        cluster = self.cluster
-
-        # Disable hinted handoff and set batch commit log so this doesn't
-        # interfer with the test (this must be after the populate)
-        cluster.set_configuration_options(values={'hinted_handoff_enabled': False}, batch_commitlog=True)
-        cluster.populate(1).start()
-        node1 = cluster.nodelist()[0]
-
-        session_node1 = self.patient_cql_connection(node1)
-        self.create_ks(session_node1, 'ks', 1)
-        session_node1.execute("""
-            CREATE TABLE ks.cf (
-                key varchar,
-                c1 text,
-                c2 text,
-                PRIMARY KEY(key)
-            ) WITH read_repair_chance = 0.0
-            AND caching = { 'keys' : 'NONE', 'rows_per_partition' : '2000' };
-        """)
-
-        node1.flush()
-        pre_insert = node1.row_cache_entries()
-
-        insert_c1c2(session_node1, keys=range(1000), consistency=ConsistencyLevel.ONE)
-
-        node1.flush()
-        node1_cache_entries = node1.row_cache_entries() - pre_insert
-        self.assertEqual(node1_cache_entries, 1000, "node1 cache %d expected 1000" % node1_cache_entries)
-
-        # We booted the new node and it got part of the items
-        node2 = new_node(cluster)
-        node2.start(wait_for_binary_proto=True)
-
-        session_node2 = self.patient_exclusive_cql_connection(node2)
-        node1.watch_log_for_alive(node2)
-        node2.watch_log_for_alive(node1)
-
-        result = session_node1.execute("SELECT * FROM ks.cf")
-        self.assertEqual(len(result), 1000, "expected 1000 lines got %d" % len(result))
-
-        # We are flushing on all nodes - to update the cache (we know all fits into the cache)
-        self.cluster.flush()
-
-        # We are checking the number of elemnts in the cache - we know some should have been removed as we moved some elements
-        node1_cache_entries = node1.row_cache_entries() - pre_insert
-        node2_cache_entries = node2.row_cache_entries()
-        self.assertEqual(node1_cache_entries + node2_cache_entries, 1000, "node1 cache %d node2 cache %d expected total of 1000" % (node1_cache_entries, node2_cache_entries))
-
     def simple_add_two_nodes_in_parallel_test(self):
         """
         Test bootstrapped node streams all data
@@ -530,6 +468,69 @@ class TestUpdateClusterLayout(Tester):
         node2.stop()
 
         self.check_rows_on_node(node1, 1000, restart=False)
+
+    def simple_decommission_node_2_test(self):
+        """
+        Test that on decommission row cache entries of non owned transfeered range are invalidated
+
+        1. Create a cluster with a single node with rf=1,insert data
+        2. Check the row cahe can be used as an estimator
+        3. Add a new node
+        4. Check that all data can be read
+        5. Delete all the data
+        6. Compact data on new node
+        7. Restart new node (it should not have any data including tombstones)
+        8. Decommission the new node
+        9. Test if any data exists in the cluster
+        """
+
+        cluster = self.cluster
+
+        # Disable hinted handoff and set batch commit log so this doesn't
+        # interfer with the test (this must be after the populate)
+        cluster.set_configuration_options(values={'hinted_handoff_enabled': False}, batch_commitlog=True)
+        cluster.populate(1).start()
+        node1 = cluster.nodelist()[0]
+
+        session_node1 = self.patient_cql_connection(node1)
+        self.create_ks(session_node1, 'ks', 1)
+        self.create_cf(session_node1, 'cf', gc_grace=0, read_repair=0.0, columns={'c1': 'text', 'c2': 'text'})
+
+        node1.flush()
+
+        insert_c1c2(session_node1, keys=range(1000), consistency=ConsistencyLevel.ONE)
+
+        node1.flush()
+
+        # We booted the new node and it got part of the items
+        node2 = new_node(cluster)
+        node2.start(wait_for_binary_proto=True,wait_other_notice=True)
+
+        session_node2 = self.patient_exclusive_cql_connection(node2)
+        session_node2.execute("use ks;")
+        node1.watch_log_for_alive(node2)
+        node2.watch_log_for_alive(node1)
+
+        result = session_node1.execute("SELECT * FROM ks.cf limit 2000;")
+        self.assertEqual(len(result), 1000, "expected 1000 lines got %d" % len(result))
+
+        session_node1.execute("DELETE from ks.cf where key in (\'k%s\');" % "\',\'k".join(str(x) for x in range(1000)))
+
+        result = session_node1.execute("SELECT * FROM ks.cf limit 2000;")
+        self.assertEqual(len(result), 0, "expected 0 lines got %d %s" % (len(result),result))
+
+        cluster.flush()
+        # lets make sure all the data in ssstables is removed
+        node2.compact()
+        # restart the node to make sure no data is left
+        node2.stop()
+        node2.start(wait_for_binary_proto=True,wait_other_notice=True)
+
+        node2.decommission()
+        node1.flush()
+
+        result = session_node1.execute("SELECT * FROM ks.cf")
+        self.assertEqual(len(result), 0, "expected 0 lines got %d" % len(result))
 
     def simple_kill_node_while_decommissioning_test(self):
         """
