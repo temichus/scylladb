0eb6777994bf6e3febb21745bfa9631d6f08faf8
Nadav Har'El <nyh@scylladb.com>
2016-01-11 15:39:45 +0200

    repair_additional_test: repair_cell_delete_test
    This test is similar to repair_cell_update_test, except instead of updating
    existing cells, we delete the value from one. And then we check that repair
    propagates this change as it should.
    
    Fixes #121
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- repair_additional_test.py
+++ repair_additional_test.py
@@ -238,6 +238,64 @@ class RepairAdditionalTest(Tester):
         self.assertEqual(result[0].c1, 'new', result[0].c1)
         self.assertEqual(result[0].c2, 'yo', result[0].c2)
 
+    def repair_cell_delete_test(self):
+        """
+        With data replicated on two nodes, update an existing partition on only
+        one of these nodes (with the other node down) to delete an existing cell.
+        Then confirm that repair can fix this on the second node as well.
+        """
+        debug("Starting cluster and inserting data...");
+        # Start a cluster of two nodes, and create a keyspace with RF=2, and
+        # a table with one partition. Hinted handoff and read repair are disabled
+        # so they don't fix the problems which repair is supposed to fix
+        self.cluster.set_configuration_options(values={'hinted_handoff_enabled': False})
+        self.cluster.populate(2).start()
+        node1, node2 = self.cluster.nodelist()
+        session = self.patient_cql_connection(node1);
+        self.create_ks(session, 'ks', 2);
+        self.create_cf(session, 'cf', read_repair=0.0, columns={'c1': 'text', 'c2': 'text'});
+        query = SimpleStatement("INSERT INTO cf (key, c1, c2) VALUES ('key', 'hello', 'hi')", consistency_level=ConsistencyLevel.ALL)
+        session.execute(query)
+
+        # Bring down node2, and change the existing data on node 1
+        debug("Bringing down node2 and updating data on node 1...");
+        node2.flush()
+        node2.stop(wait_other_notice=True)
+        query = SimpleStatement("DELETE c1 FROM cf WHERE key ='key'", consistency_level=ConsistencyLevel.ONE)
+        session.execute(query)
+
+        # Confirm that node1 has new data, and (by bringing only node 2 up) that
+        # node2 still has old data
+        result = session.execute("SELECT * from cf")
+        self.assertEqual(len(result), 1, len(result))
+        self.assertEqual(result[0].key, 'key', result[0].key)
+        self.assertEqual(result[0].c1, None, result[0].c1)
+        self.assertEqual(result[0].c2, 'hi', result[0].c2)
+        node2.start(wait_other_notice=True, wait_for_binary_proto=True)
+        node1.flush()
+        node1.stop(wait_other_notice=True)
+        session = self.patient_cql_connection(node2, 'ks')
+        result = session.execute("SELECT * from cf")
+        self.assertEqual(len(result), 1, len(result))
+        self.assertEqual(result[0].key, 'key', result[0].key)
+        self.assertEqual(result[0].c1, 'hello', result[0].c1)
+        self.assertEqual(result[0].c2, 'hi', result[0].c2)
+
+        # Finally bring both nodes up, repair, and confirm (by bringing up only
+        # node 2) that the data on node2 is now up to date.
+        node1.start(wait_other_notice=True, wait_for_binary_proto=True)
+        info=node2.repair(['ks'])
+        debug(info[0])
+        debug(info[1])
+        node1.flush()
+        node1.stop(wait_other_notice=True)
+        session = self.patient_cql_connection(node2, 'ks')
+        result = session.execute("SELECT * from cf")
+        self.assertEqual(len(result), 1, len(result))
+        self.assertEqual(result[0].key, 'key', result[0].key)
+        self.assertEqual(result[0].c1, None, result[0].c1)
+        self.assertEqual(result[0].c2, 'hi', result[0].c2)
+
     def repair_partition_delete_test(self):
         """
         With data replicated on two nodes, delete partition on only one of these
