57576665be2ca74cb40d4e6a6659cc5e437e1a7d
Paweł Dziepak <pdziepak@scylladb.com>
2016-03-23 12:18:57 +0100

    paging_additional_test: limit number of rows in debug build tests
    Debug builds are much slower than the release ones and with large number
    of rows the request may time out. Work around this issue by using smaller
    limits for debug builds.
    
    Signed-off-by: Paweł Dziepak <pdziepak@scylladb.com>

--- paging_additional_test.py
+++ paging_additional_test.py
@@ -5,13 +5,14 @@ from cassandra.query import SimpleStatement
 
 from datahelp import create_rows
 from paging_test import PageFetcher, BasePagingTester, PageAssertionMixin
+from scylla_tools import scylla_mode
 
 class TestAggregatePaging(BasePagingTester, PageAssertionMixin):
     """
     Basic aggregation tests using paging
     """
 
-    def test_paged_count_with_limit(self):
+    def _test_paged_count_with_limit(self, sizes):
         session = self.prepare()
         self.create_ks(session, 'test_aggregate_paging', 2)
         session.execute("CREATE TABLE paging_test ( id uuid PRIMARY KEY, value text )")
@@ -30,8 +31,6 @@ class TestAggregatePaging(BasePagingTester, PageAssertionMixin):
         # Thus we should always get a count == limit
         # They main thing we are testing is that page size > limit does not break the 
         # count result. (#650)
-        sizes = [10, 100, 1000, 3000, 5000]
-
         for page_size in sizes:
             for limit in sizes:                
                 future = session.execute_async(
@@ -40,3 +39,12 @@ class TestAggregatePaging(BasePagingTester, PageAssertionMixin):
                 pf = PageFetcher(future).request_all()
                 self.assertEqual(pf.num_results_all(), [1])
                 self.assertEqual(pf.all_data(), [{u'count': limit}])
+
+    @scylla_mode('release')
+    def test_paged_count_with_limit(self):
+        self._test_paged_count_with_limit([10, 100, 1000, 3000, 5000])
+
+    @scylla_mode('debug')
+    def test_paged_count_with_limit_debug(self):
+        self._test_paged_count_with_limit([10, 100, 250])
+
