e0d9b4be707f18db1f6eeb355d4540543415f0be
Shlomi Livne <shlomi@scylladb.com>
2016-12-27 17:43:54 +0200

    Add an option to set timeout for tests
    To overcome the issue of tests getting stuck - this patch adds an
    option to set a timeout for all tests execuion. Environment variable
    TEST_TIMEOUT provides the timeout value that will be used for tests
    in seconds its defaulted to 600 seconds. If set to 0 timeout will not be
    applied.
    
    Signed-off-by: Shlomi Livne <shlomi@scylladb.com>

--- dtest.py
+++ dtest.py
@@ -7,6 +7,7 @@ import logging
 import os
 import re
 import shutil
+import signal
 import subprocess
 import sys
 import tempfile
@@ -14,6 +15,7 @@ import threading
 import time
 import traceback
 import types
+import thread
 from unittest import TestCase
 
 import psutil
@@ -63,6 +65,7 @@ RECORD_COVERAGE = os.environ.get('RECORD_COVERAGE', '').lower() in ('yes', 'true
 REUSE_CLUSTER = os.environ.get('REUSE_CLUSTER', '').lower() in ('yes', 'true')
 SILENCE_DRIVER_ON_SHUTDOWN = os.environ.get('SILENCE_DRIVER_ON_SHUTDOWN', 'true').lower() in ('yes', 'true')
 IGNORE_REQUIRE = os.environ.get('IGNORE_REQUIRE', '').lower() in ('yes', 'true')
+TEST_TIMEOUT = os.environ.get('TEST_TIMEOUT', '600')
 
 CURRENT_TEST = ""
 
@@ -171,9 +174,15 @@ class Runner(threading.Thread):
         if self.__error is not None:
             raise self.__error
 
+def test_thread_timeout_signal(signum,stack):
+    raise Exception("Test timed out")
+
+def test_timeout(test):
+    debug("test %s timed out, killing it" % CURRENT_TEST)
+    test.execution_timer = None;
+    os.kill(os.getpid(), signal.SIGUSR1)
 
 class Tester(TestCase):
-
     def __init__(self, *argv, **kwargs):
         # if False, then scan the log of each node for errors after every test.
         if not hasattr(self, '_preserve_cluster'):
@@ -366,6 +375,11 @@ class Tester(TestCase):
         self.modify_log(self.cluster)
         self.connections = []
         self.runners = []
+        testtimeout =  int(TEST_TIMEOUT)
+        if testtimeout > 0:
+            signal.signal(signal.SIGUSR1, test_thread_timeout_signal)
+            self.execution_timer = threading.Timer(testtimeout,test_timeout, args=[self])
+            self.execution_timer.start()
 
     def copy_logs(self, directory=None, name=None):
         """Copy the current cluster's log files somewhere, by default to LOG_SAVED_DIR with a name of 'last'"""
@@ -573,7 +587,12 @@ class Tester(TestCase):
                 # Ignore - see comment above
                 pass
 
+
     def tearDown(self):
+        if self.execution_timer:
+           self.execution_timer.cancel()
+           self.execution_timer = None
+
         reset_environment_vars()
 
         for con in self.connections:
