00c8fd63aa9e682fbf8537cceabb51ede53ab3cb
Andrei Baranouski <andrei.baranouski@yahoo.com>
2017-04-14 17:12:56 +0300

    issue #2250 fixed
    https://github.com/scylladb/scylla/issues/2250

--- commitlog_test.py
+++ commitlog_test.py
@@ -149,7 +149,6 @@ class TestCommitLog(Tester):
         with open(os.devnull, 'w') as devnull:
             self.node1.stress(['write', 'n=1M', '-col', 'size=FIXED(1000)', '-rate', 'threads=25'], stdout=devnull, stderr=subprocess.STDOUT)
 
-    @skip('scylladb/scylla#2250')
     def test_commitlog_replay_on_startup(self):
         """ Test commit log replay """
         node1 = self.node1
