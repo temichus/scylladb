090a4ffef4e97904dee6c85c12cba7db3646adae
Larisa Ustalov <larisau@scylladb.com>
2017-05-01 14:50:51 +0300

    add 50 nodes test: fixed class inheritance problem

--- update_cluster_layout_tests.py
+++ update_cluster_layout_tests.py
@@ -105,39 +105,6 @@ class TestUpdateClusterLayout(Tester):
         self.check_rows_on_node(node2, 2000)
         self.check_rows_on_node(node1, 2000)
 
-    def add_multi_nodes(self, starting_size=3, node_count=10, rf=1):
-        """
-        1. Create a cluster with 3 nodes and rf=1, insert data
-        2. In a loop add new nodes
-        3. Check that all data exists
-        """
-        cluster = self.cluster
-
-        self.allow_log_errors = True
-
-        node1 = cluster.nodelist()[0]
-
-        session = self.patient_cql_connection(node1)
-        self.create_ks(session, 'ks', rf)
-        self.create_cf(session, 'cf', read_repair=0.0, columns={'c1': 'text', 'c2': 'text'})
-
-        consistency = ConsistencyLevel.ALL
-        debug("just before first insert")
-        insert_c1c2(session, keys=range(starting_size * 100 + 1000), consistency=ConsistencyLevel.ONE)
-
-        query = SimpleStatement("SELECT key FROM ks.cf limit 7000", consistency_level=consistency)
-        for i in range(starting_size + 1, node_count + 1):
-            node_i = new_node(cluster)
-            node_i.start(wait_for_binary_proto=True, wait_other_notice=True)
-            session_i = self.patient_exclusive_cql_connection(node_i)
-            session_i.execute("use ks;")
-            insert_c1c2(session_i, keys=range(100000 + i * 2000, 100000 + i * 2000 + 100), consistency=consistency)
-            debug("added %s" % node_i.name)
-
-            result = list(session.execute(query))
-            self.assertEqual(len(result), i * 100 + 1000, "data loss after increasing size to %d expecting %d rows %d" %
-                             (len(cluster.nodelist()), i * 100 + 1000, len(result)))
-
     def _iterative_add_decommission(self, iterations=2, node_count=2, rf=1):
         """
         Test growing and shrinking a cluster
@@ -1607,8 +1574,41 @@ class TestUpdateClusterLayout(Tester):
                     c, result[c], res[c][1])
 
 
-class TestLargeScaleCluster(TestUpdateClusterLayout):
+class TestLargeScaleCluster(Tester):
     _multiprocess_can_split_ = False
+
+    def add_multi_nodes(self, starting_size=3, node_count=10, rf=1):
+        """
+        1. Create a cluster with 3 nodes and rf=1, insert data
+        2. In a loop add new nodes
+        3. Check that all data exists
+        """
+        cluster = self.cluster
+
+        self.allow_log_errors = True
+
+        node1 = cluster.nodelist()[0]
+
+        session = self.patient_cql_connection(node1)
+        self.create_ks(session, 'ks', rf)
+        self.create_cf(session, 'cf', read_repair=0.0, columns={'c1': 'text', 'c2': 'text'})
+
+        consistency = ConsistencyLevel.ALL
+        debug("just before first insert")
+        insert_c1c2(session, keys=range(starting_size * 100 + 1000), consistency=ConsistencyLevel.ONE)
+
+        query = SimpleStatement("SELECT key FROM ks.cf limit 7000", consistency_level=consistency)
+        for i in range(starting_size + 1, node_count + 1):
+            node_i = new_node(cluster)
+            node_i.start(wait_for_binary_proto=True, wait_other_notice=True)
+            session_i = self.patient_exclusive_cql_connection(node_i)
+            session_i.execute("use ks;")
+            insert_c1c2(session_i, keys=range(100000 + i * 2000, 100000 + i * 2000 + 100), consistency=consistency)
+            debug("added %s" % node_i.name)
+
+            result = list(session.execute(query))
+            self.assertEqual(len(result), i * 100 + 1000, "data loss after increasing size to %d expecting %d rows %d" %
+                             (len(cluster.nodelist()), i * 100 + 1000, len(result)))
 
     def add_50_nodes_test(self):
         """
