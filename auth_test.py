99c8863cf21f4738bc9ecf30fb9f1372e061c1d0
Amos Kong <amos@scylladb.com>
2017-12-26 21:14:48 +0800

    verify the REVOKE/SELECT permission in the end
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- auth_test.py
+++ auth_test.py
@@ -1800,19 +1800,21 @@ class TestAuth(Tester):
             node.stop()
             node.start(wait_for_binary_proto=True)
 
-        session = self.get_session(user='cassandra', password='cassandra')
+        cassandra = self.get_session(user='cassandra', password='cassandra')
         debug('STEP: create normal user by super cassandra')
-        session.execute("CREATE USER normal WITH PASSWORD '123456' NOSUPERUSER")
+        cassandra.execute("CREATE USER normal WITH PASSWORD '123456' NOSUPERUSER")
 
         debug('STEP: check permissions (LIST/CREATE/GRANT/REVOKE) of normal user')
         session = self.get_session(user='normal', password='123456')
         session.execute('LIST USERS')
         session.execute("CREATE KEYSPACE ks WITH replication = {'class':'SimpleStrategy', 'replication_factor':1}")
         session.execute("CREATE TABLE ks.cf (id int primary key)")
+        session.execute("SELECT * FROM ks.cf")
         self.assertUnauthorized("User normal has no AUTHORIZE permission on <table ks.cf> or any of its parents",
-                                session, "GRANT ALTER ON ks.cf TO normal")
+                                session, "GRANT SELECT ON ks.cf TO normal")
         self.assertUnauthorized("User normal has no AUTHORIZE permission on <table ks.cf> or any of its parents",
                                 session, "REVOKE SELECT ON ks.cf from normal")
+        cassandra.execute("GRANT AUTHORIZE ON ks.cf TO normal")
 
         debug('STEP: verify user will login as anonymous if authentication fails')
         session = self.get_session(user='normal', password='wrongpwd')
@@ -1855,6 +1857,12 @@ class TestAuth(Tester):
             assert isinstance(e.errors.values()[0], AuthenticationFailed)
         else:
             self.fail('Session should not be created')
+
+        session = self.get_session(user='normal', password='123456')
+        self.assertUnauthorized("User normal has no SELECT permission on <table ks.cf> or any of its parents",
+                                session, "SELECT * FROM ks.cf")
+        self.assertUnauthorized("User normal has no SELECT permission on <table ks.cf> or any of its parents",
+                                 session, "REVOKE SELECT ON ks.cf from normal")
 
     def transitional_auth_from_pwdauth_test(self):
         """
