d88a48117b5cf4d449ceacbbbab605ba69814a9d
juliayakovlev <34435448+juliayakovlev@users.noreply.github.com>
2018-05-03 16:46:21 +0300

    Run sstableloader with prepared statement (#631)

--- sstableloader_test.py
+++ sstableloader_test.py
@@ -35,6 +35,8 @@ class TestSSTableLoader(MigrationTestBase):
 
         ip = node.address()
         args = [node.get_tool('sstableloader'), '-v', '-d', ip, dir]
+        if self.prepared:
+            args.append(self.prepared)
         p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = p.communicate()
         exit_status = p.wait()
@@ -49,6 +51,42 @@ class TestSSTableLoader(MigrationTestBase):
             raise Exception("sstableloader command '%s' failed; exit status: %d'; stdout: %s; stderr: %s" %
                             (" ".join(args), exit_status, stdout, stderr))
 
+    def load_migrated_tables_expect_fail(self, node, migrated_files_dir, message=None, ks='ks', cf='cf'):
+        cassandra_sstable_dir = self.get_cassandra_sstable_dir(self.version, migrated_files_dir)
+        debug("cassandra sstable dir is {}".format(cassandra_sstable_dir))
+
+        tmpdir = safe_mkdtemp()
+        dir = os.path.join(tmpdir, ks, cf)
+
+        os.mkdir(os.path.join(tmpdir, ks))
+        os.mkdir(dir)
+
+        debug("Copying sstables created by Cassandra...")
+        self.copy_files_to(cassandra_sstable_dir, dir)
+
+        ip = node.address()
+        args = [node.get_tool('sstableloader'), '-v', '-d', ip, dir]
+        if self.prepared:
+            args.append(self.prepared)
+        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        debug('STDOUT: {}'.format(stdout))
+        exit_status = p.wait()
+        if stderr:
+            debug("=== sstableloader stderr ===")
+            debug(stderr)
+            debug("====")
+
+        shutil.rmtree(tmpdir)
+
+        if exit_status != 0:
+            if message:
+                assert message in str(stderr), stderr
+            else:
+                raise Exception("sstableloader command '%s' failed; exit status: %d'; stdout: %s; stderr: %s" %
+                            (" ".join(args), exit_status, stdout, stderr))
+
+
     def load_migrated_tables_with_old_counter_test(self):
         if self.__class__.__name__ == 'TestMigration_with_2_1_x':
             self.migrate_sstable_with_old_format_counter_helper()
@@ -56,5 +94,6 @@ class TestSSTableLoader(MigrationTestBase):
 
 versions = ['2_1_x', '2_2_x', '3_0_x']
 for version in versions:
-    cls_name = ('TestMigration_with_' + version)
-    vars()[cls_name] = type(cls_name, (TestSSTableLoader,), {'version': version, '__test__': True})
+    for prepared in ['-x', '']:
+        cls_name = ('TestMigration_with_{0}{1}'.format(version, '_prepared' if prepared else ''))
+        vars()[cls_name] = type(cls_name, (TestSSTableLoader,), {'version': version, 'prepared': prepared, '__test__': True})
