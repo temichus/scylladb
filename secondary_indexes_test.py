5997e4d9087ffc787241289c845fed51c5371560
Yakovlev Julia <julia.yakovlev@scylladb.com>
2018-06-14 12:20:54 +0300

    tests: Use list() to find ResultSet length
    One of the changes from the python driver 2.7.2 to 3.6.0
    is the fact that ResultSet now is an iterator. Therefore,
    it doesn't have a __len__ property that allows you to
    directly call len() on it.
    
    Many of our tests use the old behavior of ResultSet, so
    let's fix them to use list() on the ResultSet prior to
    trying to evaluate its length.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@scylladb.com>

--- secondary_indexes_test.py
+++ secondary_indexes_test.py
@@ -113,17 +113,17 @@ class TestSecondaryIndexes(Tester):
                 self.fail("Didn't find matching trace event")
 
         query = SimpleStatement("SELECT * FROM ks.cf WHERE b='1';")
-        result = session.execute(query, trace=True)
+        result = list(session.execute(query, trace=True))
         self.assertEqual(3, len(list(result)))
         check_trace_events(result.get_query_trace())
 
         query = SimpleStatement("SELECT * FROM ks.cf WHERE b='1' LIMIT 100;")
-        result = session.execute(query, trace=True)
+        result = list(session.execute(query, trace=True))
         self.assertEqual(3, len(list(result)))
         check_trace_events(result.get_query_trace())
 
         query = SimpleStatement("SELECT * FROM ks.cf WHERE b='1' LIMIT 3;")
-        result = session.execute(query, trace=True)
+        result = list(session.execute(query, trace=True))
         self.assertEqual(3, len(list(result)))
         check_trace_events(result.get_query_trace())
 
@@ -574,7 +574,7 @@ class TestSecondaryIndexes(Tester):
             check_trace_events(trace, regex, expected_matches, halt_on_failure)
 
         query = SimpleStatement("SELECT * FROM ks.cf WHERE b='1';")
-        result = session.execute(query, trace=True)
+        result = list(session.execute(query, trace=True))
         self.assertEqual(3, len(list(result)))
 
         trace = result.get_query_trace()
