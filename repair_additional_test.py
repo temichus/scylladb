d4781470ffac31043920521a42348f666b01f892
Benny Halevy <bhalevy@scylladb.com>
2019-03-26 14:28:28 +0200

    repair_additional_test: add next-gating tests
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- repair_additional_test.py
+++ repair_additional_test.py
@@ -2,6 +2,7 @@
 
 from dtest import Tester, debug
 from unittest import skip
+from nose.plugins.attrib import attr
 
 from tools import insert_c1c2, query_c1c2
 from cassandra import ConsistencyLevel
@@ -2449,6 +2450,7 @@ class RepairAdditionalTest(RepairAdditionalBase):
     def repair_partition_delete_test(self):
        return RepairAdditionalBase._repair_partition_delete_test(self)
 
+    @attr('next-gating')
     def repair_ttl_update_test(self):
        return RepairAdditionalBase._repair_ttl_update_test(self)
 
@@ -2488,6 +2490,7 @@ class RepairAdditionalTest(RepairAdditionalBase):
     def repair_kill_3_test(self):
        return RepairAdditionalBase._repair_kill_3_test(self)
 
+    @attr('next-gating')
     def repair_during_update_test(self, more_options=[]):
        return RepairAdditionalBase._repair_during_update_test(self,more_options)
 
