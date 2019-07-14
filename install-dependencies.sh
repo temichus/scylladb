3344ddbc1d2a9ca1ae2ac83d86a1630319e59689
Benny Halevy <bhalevy@scylladb.com>
2019-07-14 10:49:47 +0300

    install-dependencies: add psutil
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -8,6 +8,7 @@ fedora_packages=(
     python2-pyyaml python3-pyyaml
     python2-six python3-six
     python2-requests python3-requests
+    python2-psutil python3-psutil
 )
 
 if [ "$ID" = "fedora" ]; then
