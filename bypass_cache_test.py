bd231d54d46584ef008cabee4d7d8b41f2cf5346
Fabio Gelcer <fabio@scylladb.com>
2019-07-15 16:55:51 +0300

    Changes per PR review

--- bypass_cache_test.py
+++ bypass_cache_test.py
@@ -1,9 +1,8 @@
-from nose.tools import assert_equal
-from dtest import Tester, debug
-import datetime
+from nose.tools import assert_true, assert_false
+from dtest import Tester
+
 import time
-import random
-import subprocess
+import tools
 
 
 class TestBypassCache(Tester):
@@ -22,52 +21,49 @@ class TestBypassCache(Tester):
         session = self.patient_cql_connection(node1)
         self.create_ks(session=session, name=keyspace_name, rf=rf)
 
-        self.create_table(session=session)
-        self.populate_table(session=session)
+        tools.create_c1c2_table(self, session)
+        tools.insert_c1c2(session, n=100)
 
         return session
 
-    def create_table(self, session):
-        session.execute('CREATE TABLE user_events (userid text, event timestamp, value text, '
-                        'PRIMARY KEY (userid, event))')
+    def get_scylla_cache_reads_metrics(self, node, metrics=None):
+        if metrics is None:
+            metrics = ['scylla_cache_reads']
+        return self.get_node_metrics(self.get_ip_from_node(node), metrics=metrics)
 
-    def populate_table(self, session):
-        partitions = random.randint(1, 50)
-        lines = random.randint(1, 1000)
-        debug('Adding {} lines with {} partitions'.format(lines, partitions))
-
-        for partition in xrange(0, partitions):
-            for line in xrange(0, lines):
-                session.execute("INSERT INTO user_events (userid, event, value)"
-                                "VALUES ('{}', '{}', '{}')".format('user{}'.format(partition),
-                                                                   str(datetime.datetime.now())[:-7],
-                                                                   random.random()))
-
-    def count_disk_ios(self):
-        cmd = 'cat /sys/block/md0/stat'
-        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-
-        return stdout
-
-    def restart_scylla(self, node):
-        node.stop()
-        time.sleep(1)
-        node.start()
-
-    def get_scylla_cache_reads_metrics(self, node):
-        return self.get_node_metrics(self.get_ip_from_node(node), metrics=['scylla_cache_reads'])
-
-    def verify_read_was_from_disk(self, node, query, session):
+    def is_read_from_disk(self, node, query, session):
         cache_read_before_bypass_read = self.get_scylla_cache_reads_metrics(node=node)
         session.execute(query)
         cache_read_after_bypass_read = self.get_scylla_cache_reads_metrics(node=node)
+        return cache_read_before_bypass_read['scylla_cache_reads'] == cache_read_after_bypass_read['scylla_cache_reads']
 
-        assert_equal(cache_read_before_bypass_read, cache_read_after_bypass_read)
+    def verify_read_was_from_disk(self, node, query, session):
+        assert_true(self.is_read_from_disk(node, query, session), 'Read was made from cache instead of bypass it')
+
+    def verify_read_was_from_cache(self, node, query, session):
+        assert_false(self.is_read_from_disk(node, query, session), 'Read was made from disk instead of from cache')
 
     def test_simple_bypass_cache(self):
         session = self.prepare()
         node = self.cluster.nodelist()[0]
-        query = 'SELECT * FROM user_events BYPASS CACHE'
+        query = 'SELECT * FROM cf BYPASS CACHE'
 
         self.verify_read_was_from_disk(node=node, query=query, session=session)
+
+    def test_multiple_bypass_cache(self):
+        session = self.prepare()
+        node = self.cluster.nodelist()[0]
+        time.sleep(3)
+        for _ in xrange(20):
+            query = 'SELECT * FROM cf BYPASS CACHE'
+            self.verify_read_was_from_disk(node=node, query=query, session=session)
+
+    def test_read_from_cache_and_then_bypass_cache(self):
+        session = self.prepare()
+        node = self.cluster.nodelist()[0]
+
+        no_bypass_query = 'SELECT * FROM cf'
+        self.verify_read_was_from_cache(node=node, query=no_bypass_query, session=session)
+
+        bypass_query = 'SELECT * FROM cf BYPASS CACHE'
+        self.verify_read_was_from_disk(node=node, query=bypass_query, session=session)
