0c6a7a8e5956861ad340421d898472eecc436784
Botond Dénes <bdenes@scylladb.com>
2019-07-15 13:19:41 +0300

    Move test to compection_additional_test.py

--- compaction_additional_test.py
+++ compaction_additional_test.py
@@ -3,13 +3,15 @@ import time
 import os
 import shutil
 import glob
+import tools
 from dtest import Tester, debug, run_with_params
 from scylla_tools import get_sstables_files, insert_c1c2, get_cf_dir
-from cassandra import ConsistencyLevel
+from cassandra import ConsistencyLevel, concurrent
 from assertions import assert_none
 
 from datetime import datetime as dt
 from nose.plugins.attrib import attr
+import sstable_tools.statistics
 
 
 class CompactionAdditionalTest(Tester):
@@ -394,3 +396,93 @@ strategies = ['LeveledCompactionStrategy', 'SizeTieredCompactionStrategy', 'Date
 for strategy in strategies:
     cls_name = ('CompactionAdditionalStrategyTests_with_' + strategy)
     vars()[cls_name] = type(cls_name, (CompactionAdditionalStrategyTests,), {'strategy': strategy, '__test__': True})
+
+
+def micros_to_seconds(micros):
+    return int(micros/(1000 * 1000))
+
+
+def seconds_to_micros(seconds):
+    return seconds * 1000 * 1000
+
+
+class TestTimeWindowDataSegregation(Tester):
+    keyspace_name = "ks"
+    table_name = "test"
+    window_size = 1
+
+    def _get_time_window_in_seconds(self, statistics_file):
+        with open(statistics_file, 'rb') as f:
+            data = f.read()
+
+        metadata = sstable_tools.statistics.parse(data, 'mc')
+        min_timestamp = metadata['Stats']['min_timestamp']
+        max_timestamp = metadata['Stats']['max_timestamp']
+
+        return micros_to_seconds(max_timestamp - min_timestamp)
+
+    def _check_sstable_timestamps(self, node):
+        ks_path = os.path.join(node.get_path(), 'data', self.keyspace_name)
+        statistics_files = []
+        for dirpath, dirnames, filenames in os.walk(ks_path):
+            elems = os.path.split(dirpath)
+            # We are in the table's dir
+            if elems[-1].startswith(self.table_name):
+                statistics_files += [os.path.join(dirpath, f) for f in filenames if f.endswith('-Statistics.db')]
+                continue
+
+            # prune all dirs that are not a table dir
+            for d in dirnames:
+                if not d.startswith(self.table_name):
+                    dirnames.remove(d)
+
+        self.assertTrue(len(statistics_files) > 0)
+
+        for sf in statistics_files:
+            tw = self._get_time_window_in_seconds(sf)
+            # Allow an error margin of a half-window.
+            self.assertTrue(tw <= 1.5 * self.window_size * 60)
+
+
+    def test_streaming(self):
+        cluster = self.cluster
+        cluster.populate(1)
+        cluster.start(wait_for_binary_proto=True)
+
+        node1 = cluster.nodelist()[0]
+
+        session = self.patient_cql_connection(node1)
+
+        session.execute("CREATE KEYSPACE {} WITH replication = {{'class': 'SimpleStrategy', 'replication_factor': 1}}".format(self.keyspace_name))
+        session.execute("CREATE TABLE {}.{} (pk int, ck int, v int, PRIMARY KEY(pk, ck))"
+                "WITH compaction = {{"
+                    "'class': 'TimeWindowCompactionStrategy',"
+                    "'compaction_window_unit': 'MINUTES',"
+                    "'compaction_window_size': {}}}".format(self.keyspace_name, self.table_name, self.window_size))
+
+        insert_statement = session.prepare("INSERT INTO {}.{} (pk, ck, v) VALUES (?, ?, ?) USING TIMESTAMP ?".format(self.keyspace_name, self.table_name))
+
+        # Simulate a write process across 20 minutes.
+        # We use `USING TIMESTAMP` to distribute the writes evenly
+        # across the entire range, simulating a write every second (to
+        # several partitions).
+        for t in range(20 * 60):
+            concurrent.execute_concurrent_with_args(
+                    session,
+                    insert_statement,
+                    [(pk, t, 0, seconds_to_micros(t)) for pk in range(10)])
+
+            # Flush every half minute to ensure each sstable contains at
+            # max a single window.
+            if t % 30 == 0:
+                node1.flush()
+
+        # Not really relevant to the test, just for sanity.
+        self._check_sstable_timestamps(node1)
+
+        node2 = tools.new_node(cluster)
+        node2.start(wait_for_binary_proto=True)
+
+        # After streaming the new node should also have at max one
+        # window per sstable.
+        self._check_sstable_timestamps(node2)
