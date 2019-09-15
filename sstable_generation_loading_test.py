6d698e1caf047376deb35ac63849c691ab8b47ea
Benny Halevy <bhalevy@scylladb.com>
2019-09-15 09:25:14 +0300

    remove_index_file_test: use log mark for watching for malformed_sstable_exception
    Otherwise messages form previous iterations maybe be falsely detected.
    
    Refs #1104
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- sstable_generation_loading_test.py
+++ sstable_generation_loading_test.py
@@ -130,8 +130,9 @@ class TestSSTableGenerationAndLoading(Tester):
         for comp in comps:
             os.system("mv {path}/*{comp} {path}/snapshots/".format(**locals()))
 
+            mark = node1.mark_log()
             node1.start()
-            node1.watch_log_for("malformed_sstable_exception", timeout=10)
+            node1.watch_log_for("malformed_sstable_exception", timeout=10, from_mark=mark)
             node1.stop(wait=False, gently=False)
             time.sleep(1)
 
