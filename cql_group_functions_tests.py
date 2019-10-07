c05a32afcc76bfe8ea450969c93e1edffb472cd9
Fabio Gelcer <fabio@scylladb.com>
2019-10-07 10:33:57 +0300

    Added test for group functions to all types:
    * min, max and count are supposed to work for all types
    * following relevant task https://trello.com/c/3OXcNvCv/1156-
    * cql-group-functions-min-max-count-now-works-with-all-types-3824
    * and https://github.com/scylladb/scylla/issues/3824.
    
    * 2 bugs were opened as they didn't work as expected:
    * https://github.com/scylladb/scylla/issues/5147
    * https://github.com/scylladb/scylla/issues/5139
    
    * those faied types were removed from the list of types
    * and will be re-inserted once the issues will be fixed.
    
    Added new cql_group_functions_tests.py to scylla_tests

---
+++ cql_group_functions_tests.py
@@ -0,0 +1,110 @@
+from dtest import Tester, debug
+from assertions import assert_one
+from cassandra import ConsistencyLevel
+from tools import require, rows_to_list, ColumnType
+
+
+class TestGroupFunctions(Tester):
+    # failed types:
+    # ascii failure - https://github.com/scylladb/scylla/issues/5147
+    # unexpected hexa failure - https://github.com/scylladb/scylla/issues/5139
+    # blob, inet, list, map, set, time, tuple and udt failed for min and max (returned unexpected result - a hexa)
+    types_to_skip = ['ascii', 'blob', 'inet', 'list', 'map', 'set', 'time', 'tuple', 'udt']
+
+    text_types_list = ['ascii', 'blob', 'inet', 'uuid', 'text', 'varchar']
+    numeric_type_list = ['bigint', 'boolean', 'decimal', 'double', 'float', 'int', 'smallint', 'tinyint', 'varint']
+    lists_types_list = ['list', 'map', 'set', 'tuple', 'udt']
+    dates_type_list = ['date', 'time', 'timestamp', 'timeuuid']
+    table_name = 'all_types'
+    loop_number = 20000
+
+    def prepare(self, nodes=3, keyspace_name='group_functions', rf=3, options_dict=None):
+        self.keyspace_name = keyspace_name
+        cluster = self.cluster
+
+        if options_dict:
+            cluster.set_configuration_options(values=options_dict)
+        cluster.populate(nodes).start()
+        node1 = cluster.nodelist()[0]
+        session = self.patient_cql_connection(node1)
+        self.create_ks(session=session, name=keyspace_name, rf=rf)
+
+        return session
+
+    def create_udt(self, session):
+        query = 'create type my_udt (a text, b text)'
+        session.execute(query)
+
+    def create_table(self, session, table_name, single_column):
+        query = 'CREATE TABLE {} ({});'.format(table_name, single_column)
+        debug(query)
+        session.execute(query)
+
+    def check_results(self, session, table_name, single_type):
+        full_res = sorted(
+            rows_to_list(session.execute('select {} from {};'.format('my_{}'.format(single_type), table_name))))
+        assert_one(session, 'select count({}) from {}'.format('my_{}'.format(single_type), table_name), [len(full_res)],
+                   cl=ConsistencyLevel.QUORUM)
+        assert_one(session, 'select min({}) from {}'.format('my_{}'.format(single_type), table_name), full_res[0],
+                   cl=ConsistencyLevel.QUORUM)
+        assert_one(session, 'select max({}) from {}'.format('my_{}'.format(single_type), table_name), full_res[-1],
+                   cl=ConsistencyLevel.QUORUM)
+
+    def create_tables_and_run_group_functions(self, session, table_name, single_type):
+        if single_type in self.types_to_skip:
+            return
+        column = 'my_{0} {0}'
+        if single_type in self.lists_types_list:
+            if single_type == 'tuple':
+                type_format = 'frozen<{}<int>>'.format(single_type)
+            elif single_type == 'udt':
+                self.create_udt(session=session)
+                type_format = 'frozen<my_{}>'.format(single_type)
+            elif single_type == 'map':
+                type_format = '{}<text, int>'.format(single_type)
+            # here will be: 'uuid', 'bigint', 'boolean', 'date', 'decimal', 'double', 'float', 'int', 'smallint',
+            # 'text', 'timestamp', 'timeuuid', 'tinyint', 'varchar', 'varint'
+            else:
+                type_format = '{}<int>'.format(single_type)
+            single_column = 'id int PRIMARY KEY, {}'.format('my_{} {}'.format(single_type, type_format))
+        else:
+            single_column = 'id int PRIMARY KEY, {}'.format(column.format(single_type))
+        self.create_table(session=session, table_name=table_name, single_column=single_column)
+        for i in xrange(self.loop_number):
+            self.populate_all_types_table(session=session, table_name=table_name, column_name=single_type, index=i)
+        self.check_results(session=session, table_name=table_name, single_type=single_type)
+        session.execute('DROP TABLE {}'.format(table_name))
+
+    def populate_all_types_table(self, session, table_name, column_name, index):
+        value = self.generate_all_types(column_name=column_name)
+        query = 'INSERT INTO {} (id, {}) VALUES ({}, {});'.format(table_name, 'my_{}'.format(column_name), index, value)
+        session.execute(query)
+    
+    def generate_all_types(self, column_name):
+        return ColumnType(column_name).get_value() 
+
+    def test_numeric_type_group(self):
+        session = self.prepare()
+        for single_type in self.numeric_type_list:
+            self.create_tables_and_run_group_functions(session=session, table_name=self.table_name,
+                                                       single_type=single_type)
+    
+    def test_text_type_group(self):
+        session = self.prepare()
+        for single_type in self.text_types_list:
+            self.create_tables_and_run_group_functions(session=session, table_name=self.table_name,
+                                                       single_type=single_type)
+    
+    @require('5139')
+    def test_lists_type_group(self):
+        session = self.prepare()
+        for single_type in self.lists_types_list:
+            self.create_tables_and_run_group_functions(session=session, table_name=self.table_name,
+                                                       single_type=single_type)
+    
+    def test_dates_type_group(self):
+        session = self.prepare()
+        for single_type in self.dates_type_list:
+            self.create_tables_and_run_group_functions(session=session, table_name=self.table_name,
+                                                       single_type=single_type)
+
