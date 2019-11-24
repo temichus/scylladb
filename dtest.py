04a652daae1c91235b5acf8ed3d8593cb964354c
Benny Halevy <bhalevy@scylladb.com>
2019-11-24 12:24:54 +0200

    tools, dtest: create_cf: add debug_query param
    When creating a large number of tables, allow disabling
    debug printouts so not to spam the console
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- dtest.py
+++ dtest.py
@@ -823,7 +823,7 @@ class Tester(TestCase):
     # We default to UTF8Type because it's simpler to use in tests
     def create_cf(self, session, name, key_type="varchar", speculative_retry=None, read_repair=None, compression=None,
                   gc_grace=None, columns=None, validation="UTF8Type", compaction=None, compact_storage=False,
-                  default_ttl=None, dclocal_read_repair_chance=None):
+                  default_ttl=None, dclocal_read_repair_chance=None, debug_query=True):
 
         additional_columns = ""
         if columns is not None:
@@ -856,7 +856,8 @@ class Tester(TestCase):
         if compact_storage:
             query += ' AND COMPACT STORAGE'
 
-        debug(query)
+        if debug_query:
+            debug(query)
 
         session.execute(query)
         time.sleep(0.2)
