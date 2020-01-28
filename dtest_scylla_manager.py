bfe62ed294e21cd7ae6fac6ea5bcc0adac1f5cd8
Shlomi Balalis <Balalis@scylladb.com>
2020-01-28 13:50:53 +0200

    Added ABORTED to the list of final statuses of a manager task

--- dtest_scylla_manager.py
+++ dtest_scylla_manager.py
@@ -575,7 +575,7 @@ class ManagerTask(ScyllaManagerBase):
         2) return the final status.
         :return:
         """
-        list_final_status = [TaskStatus.ERROR, TaskStatus.STOPPED, TaskStatus.DONE]
+        list_final_status = [TaskStatus.ERROR, TaskStatus.STOPPED, TaskStatus.DONE, TaskStatus.ABORTED]
         debug("Waiting for task: {} getting to a final status ({})..".format(self.id, [str(s) for s in
                                                                                               list_final_status]))
         res = self.wait_for_status(list_status=list_final_status, timeout=timeout, step=step)
