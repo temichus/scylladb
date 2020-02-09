d688bce4ad3c37afb923e3148c70b4258456c65f
Pavel Solodovnikov <pa.solodovnikov@scylladb.com>
2020-02-09 23:20:13 +0300

    lwt: re-enable TestCQL.null_value_tuple_test
    Corresponding bug was fixed by https://github.com/scylladb/scylla/commit/bcc464755227898fb67ea77d4b48dd224d3ab5b3
    
    Signed-off-by: Pavel Solodovnikov <pa.solodovnikov@scylladb.com>

--- cql_prepared_test.py
+++ cql_prepared_test.py
@@ -4,7 +4,6 @@ from cassandra import ConsistencyLevel
 from cassandra.util import Time, Date, uuid_from_time, SortedSet
 from tools import rows_to_list
 from assertions import assert_one
-from unittest import skip
 
 from decimal import Decimal
 from datetime import datetime, date
@@ -57,7 +56,6 @@ class TestCQL(Tester):
 
         session.execute(pq, ['foo', 4])
 
-    @skip('fails for Scylla, need to investigate')
     def null_value_tuple_test(self):
         session = self.prepare(options={'experimental_features': ['lwt']})
 
