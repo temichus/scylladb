75e9cd215659338b4e883cd7cde0161047d3c4ec
Pavel Solodovnikov <pa.solodovnikov@scylladb.com>
2020-02-20 19:50:44 +0300

    lwt: split lwt_nested_collections_test into two tests
    This patch splits one combined lwt_nested_collections_test into
    two smaller tests, each one testing one variant of nested
    collections:
     * lwt_nested_collections_list_set_test for list<frozen<set<T>>>
     * lwt_nested_collections_set_list_test for set<frozen<list<T>>>
    
    This is more maintainable and in case some checks fail for
    one of the collections, the other will still run and will not
    be interrupted by a failure.
    
    An dalso we are getting the knowledge of which one precisely
    failed without even looking into the test logs.
    
    Signed-off-by: Pavel Solodovnikov <pa.solodovnikov@scylladb.com>

--- cql_prepared_test.py
+++ cql_prepared_test.py
@@ -584,9 +584,10 @@ class TestCQL(Tester):
             assert_one_prepared(session, ti.frozen_stmt, [True, None], {'update_val': ti.non_empty, 'v': None})
             assert_one_prepared(session, ti.frozen_stmt, [False, ti.non_empty], {'update_val': ti.empty, 'v': ti.empty})
 
-    def lwt_nested_collections_test(self):
+    def lwt_nested_collections_list_set_test(self):
         """
         Test that nested collections are working with parameter markers.
+        This particular test verifies list<frozen<set<T>>> cases.
 
         Note: only non_frozen<frozen<T>> combinations are supported.
         """
@@ -598,45 +599,16 @@ class TestCQL(Tester):
         session.execute('''
             CREATE TABLE test (
                 k INT PRIMARY KEY,
-                set_list set<frozen<list<int>>>,
                 list_set list<frozen<set<int>>>
             )
         ''')
 
         insert_stmt = prepare_statement(session, '''
-            INSERT INTO test (k, set_list, list_set) VALUES (1, ?, ?)
+            INSERT INTO test (k, list_set) VALUES (1, ?)
         ''')
 
-        # INSERT INTO test (k, set_list, list_set) VALUES (1, {[1,2], [1,2]}, [{1,2}, {1,2}])
-        session.execute(insert_stmt, [
-                SortedSet([[1, 2], [1, 2]]),
-                [SortedSet([1, 2]), SortedSet([1, 2])]
-            ])
-
-        # Sets
-
-        # UPDATE test SET set_list={[3,3], [4,4]} WHERE k = 1 IF b = {[1,2], [1,2]}
-        stmt = prepare_statement(session, '''
-            UPDATE test SET set_list=:update_val WHERE k=1 IF set_list=:v
-        ''')
-        assert_one_prepared(session, stmt, [True, SortedSet([[1, 2]])],
-            {'update_val': SortedSet([[3, 3], [4, 4]]), 'v': SortedSet([[1, 2], [1, 2]])})
-
-        # UPDATE test SET set_list={[5,5,5], [4,4,4]} WHERE k = 1 if set_list > {[3,3], [4,4]}
-        stmt = prepare_statement(session, '''
-            UPDATE test SET set_list=:update_val WHERE k=1 IF set_list > :v
-        ''')
-        assert_one_prepared(session, stmt, [False, SortedSet([[3, 3], [4, 4]])],
-            {'update_val': SortedSet([[5, 5, 5], [4, 4, 4]]), 'v': SortedSet([[3, 3], [4, 4]])})
-
-        # UPDATE test SET set_list={[5,5,5], [4,4,4]} WHERE k = 1 IF set_list >= {[3,3], [4,4]}
-        stmt = prepare_statement(session, '''
-            UPDATE test SET set_list=:update_val WHERE k=1 IF set_list >= :v
-        ''')
-        assert_one_prepared(session, stmt, [True, SortedSet([[3, 3], [4, 4]])],
-            {'update_val': SortedSet([[5, 5, 5], [4, 4, 4]]), 'v': SortedSet([[3, 3], [4, 4]])})
-
-        # Lists
+        # INSERT INTO test (k, list_set) VALUES (1, [{1,2}, {1,2}])
+        session.execute(insert_stmt, [[SortedSet([1, 2]), SortedSet([1, 2])]])
 
         # UPDATE test SET list_set=[{3,4}, {4,5}] WHERE a = 1 IF list_set = [{1,2}, {1,2}]
         stmt = prepare_statement(session, '''
@@ -665,3 +637,50 @@ class TestCQL(Tester):
         ''')
         assert_one_prepared(session, stmt, [True, [SortedSet([5, 6, 7]), SortedSet([7, 8, 9])]],
             {'update_val': [SortedSet([5, 6, 7]), SortedSet([7, 8, 9])], 'v': [SortedSet([3, 4]), SortedSet([4, 5])]})
+
+    def lwt_nested_collections_set_list_test(self):
+        """
+        Test that nested collections are working with parameter markers.
+        This particular test verifies set<frozen<list<T>>> cases.
+
+        Note: only non_frozen<frozen<T>> combinations are supported.
+        """
+
+        session = self.prepare(options={'experimental_features': ['lwt']})
+
+        # Create test table and prepare data
+
+        session.execute('''
+            CREATE TABLE test (
+                k INT PRIMARY KEY,
+                set_list set<frozen<list<int>>>,
+            )
+        ''')
+
+        insert_stmt = prepare_statement(session, '''
+            INSERT INTO test (k, set_list) VALUES (1, ?)
+        ''')
+
+        # INSERT INTO test (k, set_list) VALUES (1, {[1,2], [1,2]})
+        session.execute(insert_stmt, [SortedSet([[1, 2], [1, 2]])])
+
+        # UPDATE test SET set_list={[3,3], [4,4]} WHERE k = 1 IF b = {[1,2], [1,2]}
+        stmt = prepare_statement(session, '''
+            UPDATE test SET set_list=:update_val WHERE k=1 IF set_list=:v
+        ''')
+        assert_one_prepared(session, stmt, [True, SortedSet([[1, 2]])],
+            {'update_val': SortedSet([[3, 3], [4, 4]]), 'v': SortedSet([[1, 2], [1, 2]])})
+
+        # UPDATE test SET set_list={[5,5,5], [4,4,4]} WHERE k = 1 if set_list > {[3,3], [4,4]}
+        stmt = prepare_statement(session, '''
+            UPDATE test SET set_list=:update_val WHERE k=1 IF set_list > :v
+        ''')
+        assert_one_prepared(session, stmt, [False, SortedSet([[3, 3], [4, 4]])],
+            {'update_val': SortedSet([[5, 5, 5], [4, 4, 4]]), 'v': SortedSet([[3, 3], [4, 4]])})
+
+        # UPDATE test SET set_list={[5,5,5], [4,4,4]} WHERE k = 1 IF set_list >= {[3,3], [4,4]}
+        stmt = prepare_statement(session, '''
+            UPDATE test SET set_list=:update_val WHERE k=1 IF set_list >= :v
+        ''')
+        assert_one_prepared(session, stmt, [True, SortedSet([[3, 3], [4, 4]])],
+            {'update_val': SortedSet([[5, 5, 5], [4, 4, 4]]), 'v': SortedSet([[3, 3], [4, 4]])})
