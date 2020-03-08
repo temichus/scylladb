a0a576b01dfb0135162560c288d66a5e4ced1988
Benny Halevy <bhalevy@scylladb.com>
2020-03-08 09:27:19 +0200

    secondary_indexes_test: fix exclude_errors regex
    Parenthesis must be escaped.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- secondary_indexes_test.py
+++ secondary_indexes_test.py
@@ -1098,9 +1098,9 @@ class TestSecondaryIndexes(Tester, SecondaryIndexesHelpers):
                                    num_rows_expected=num_rows, consistency_level=ConsistencyLevel.QUORUM)
 
         exclude_errors = ['Can\'t send migration request: node {} is down'.format(node2_ip),
-                          'Error applying view update to {}: exceptions::unavailable_exception (Cannot achieve consistency level for cl ONE. Requires 1, alive 0)'.format(node2_ip),
-                          'Error applying view update to {}: exceptions::mutation_write_timeout_exception (Operation timed out for {}.{}_index - received only 0 responses from 1 CL=ONE.)'.format(node2_ip, keyspace_name, index_name),
-                          'Error applying view update to {}: exceptions::mutation_write_failure_exception (Operation failed for {}.{}_index - received 0 responses and 1 failures from 1 CL=ONE.)'.format(node2_ip, keyspace_name, index_name),
+                          'Error applying view update to {}: exceptions::unavailable_exception \(Cannot achieve consistency level for cl ONE. Requires 1, alive 0\)'.format(node2_ip),
+                          'Error applying view update to {}: exceptions::mutation_write_timeout_exception \(Operation timed out for {}.{}_index - received only 0 responses from 1 CL=ONE.\)'.format(node2_ip, keyspace_name, index_name),
+                          'Error applying view update to {}: exceptions::mutation_write_failure_exception \(Operation failed for {}.{}_index - received 0 responses and 1 failures from 1 CL=ONE.\)'.format(node2_ip, keyspace_name, index_name),
                          ]
         self.check_errors(self.cluster.nodelist()[0], exclude_errors=exclude_errors)
 
@@ -2144,7 +2144,7 @@ class TestLocalIndexes(Tester, SecondaryIndexesHelpers):
                                    num_rows_expected=num_rows, consistency_level=ConsistencyLevel.QUORUM)
 
         exclude_errors = ['Can\'t send migration request: node {} is down'.format(node2_ip),
-                          'Error applying view update to {}: exceptions::unavailable_exception (Cannot achieve consistency level for cl ONE. Requires 1, alive 0)'.format(node2_ip),
+                          'Error applying view update to {}: exceptions::unavailable_exception \(Cannot achieve consistency level for cl ONE. Requires 1, alive 0\)'.format(node2_ip),
                           'Operation timed out for ks.b_index_index - received only 0 responses from 1 CL=ONE.']
         self.check_errors(self.cluster.nodelist()[0], exclude_errors=exclude_errors)
 
