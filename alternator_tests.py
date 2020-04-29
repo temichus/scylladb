5e1bb1102fce83b34548a2f0cb2ee05fa8f1aca7
Oren Efraimov <oren.efraimov@scylladb.com>
2020-04-30 00:17:56 +0300

    test(alternator.py): Adding snapshot and refresh test
    * Create new tests that check snapshot for Alternator API:
      * `test_load_snapshot_and_refresh` - Checks old snapshot and verify the data is valid
      * `test_create_snapshot_and_refresh` - Create snapshot for current version and verify the data is valid
    * `TesterAlternator` - Create a new class that contain all Alternator API and common methods
    * `deepdiff`- Added new package to checking diff between 2 mutable objects
    * Create new docker version - fedora-29-py3-20200428

--- alternator_tests.py
+++ alternator_tests.py
@@ -1,103 +1,94 @@
-import boto3
+import os
+import shutil
+import tempfile
+from pprint import pformat
+
 from botocore import exceptions as boto3_exceptions
-from nose.plugins.attrib import attr
 
-from alternator_utils import create_dynamodb_table, generate_put_request_items, StoppableThread, get_table_items
-from dtest import Tester, debug
+from alternator_utils import TesterAlternator, ALTERNATOR_SNAPSHOT_FOLDER, TABLE_NAME, NUM_OF_ITEMS
+from dtest import debug
 
-TABLE_NAME = 'user_table'
-NUM_OF_NODES = 3
-NUM_OF_ITEMS = 100
 
-@attr('dtest-full')
-class AlternatorTest(Tester):
+class AlternatorTest(TesterAlternator):
 
-    # #############################  Utils #########################################
+    def test_load_older_snapshot_and_refresh(self):
+        """
+        The test loading older snapshot files and checking the refresh command works
+        Test will:
+           - Create a cluster of 3 nodes.
+           - Load older snapshot files
+           - Execute nodetool `refresh` command
+           - Verify after `refresh` commands are equal to snapshot data
+        """
+        table_name, num_of_items, node_idx = TABLE_NAME, NUM_OF_ITEMS, 0
+        snapshot_folder = os.path.join(ALTERNATOR_SNAPSHOT_FOLDER, "scylla4.0.rc1")
 
-    def _prepare_alternator_cluster(self, num_of_nodes: int = NUM_OF_NODES, is_multi_dc: bool = False) -> None:
-        cluster_type = 'single DC' if not is_multi_dc else 'multi DC'
-        debug(f'Populate a cluster with: {num_of_nodes} nodes for {cluster_type}')
-        cluster = self.cluster
-        cluster.set_configuration_options({'start_native_transport': True})
-        cluster.set_configuration_options({'experimental': True, 'alternator_port': 8080})
-        if not is_multi_dc:
-            cluster.populate([num_of_nodes])
-        else:
-            cluster.populate([num_of_nodes, num_of_nodes])
-        debug('Starting cluster..')
-        cluster.start(wait_for_binary_proto=True, wait_other_notice=True)
+        self.prepare_cluster(num_of_nodes=3)
+        node1 = self.cluster.nodelist()[node_idx]
+        self.create_table(table_name=table_name, node=node1)
+        table_data = self.create_items()
+        self.load_snapshot_and_refresh(table_name=table_name, node=node1, snapshot_folder=snapshot_folder)
+        diff = self.compare_table_data(table_name=table_name, table_data=table_data, node=node1)
+        self.assertTrue(expr=not diff, msg=f"The following items are missing:\n{pformat(diff)}")
 
-    def _run_alternator_stress(self, consistent_read: bool = True) -> StoppableThread:
-        debug('Start Alternator stress..')
-        resource_table = self._get_dynamodb_client().Table(TABLE_NAME)
-        get_items_thread = StoppableThread(target=get_table_items,
-                                           kwargs={'table': resource_table,
-                                                   'consistent_read': consistent_read})
+    def test_create_snapshot_and_refresh(self):
+        """
+        The test checks the behavior of the `snapshot` and `refresh` commands for Alternator
+        Test will:
+          - Create a cluster of 3 nodes.
+          - Create a new table with 100 items.
+          - Create a snapshot of the table we created earlier.
+          - Copy the files from the `snapshot` command and moved it to the table folder and used the `refresh`
+            command to load the data.
+          - Verify the data before `snapshot` and after `refresh` commands are equal.
+        """
+        table_name, num_of_items = TABLE_NAME, NUM_OF_ITEMS
 
-        self.addCleanup(get_items_thread.join)
-        get_items_thread.start()
-        return get_items_thread
+        self.prepare_cluster(num_of_nodes=1)
+        node1 = self.cluster.nodelist()[0]
+        self.create_table(table_name=table_name, node=node1)
+        self.generate_request_items(table_name=table_name, num_of_items=num_of_items, node=node1)
+        data_before_refresh = self.scan_table(table_name=table_name, node=node1)
 
-    def _get_dynamodb_client(self, node=None):
-        node = node or self.cluster.nodelist()[0]
-        node_ip = self.get_ip_from_node(node)
-        debug(f"Getting a resource for node {node.name} {node_ip}")
-        return boto3.resource('dynamodb', endpoint_url=f'http://{node_ip}:8080',
-                              region_name='None', aws_access_key_id='None', aws_secret_access_key='None')
-
-    def _create_dynamodb_table(self, node=None):
-        node_resource = self._get_dynamodb_client(node=node)
-        debug(f"Creating a table, using {node_resource.meta.client.meta.endpoint_url} resource..")
-        create_dynamodb_table(dynamodb_resource=node_resource)
-        return node_resource
-
-    def _batch_writer_item_list(self, dynamo_client=None):
-        dynamo_client = dynamo_client or self._get_dynamodb_client()
-        debug(f"Executing batch_write_item, using {dynamo_client.meta.client.meta.endpoint_url} resource..")
-        table = dynamo_client.Table(TABLE_NAME)
-        with table.batch_writer() as batch:
-            for item in generate_put_request_items(num_of_items=NUM_OF_ITEMS):
-                debug(f"put item: {item}")
-                batch.put_item(item)
-        return table
-
-    def _prefill_alternator_table(self, node=None):
-        node_resource = self._create_dynamodb_table(node=node)
-        node_resource_table = self._batch_writer_item_list(dynamo_client=node_resource)
-        return node_resource_table
-
-    def _generate_alternator_cluster_with_stress(self, num_of_nodes: int = NUM_OF_NODES) -> StoppableThread:
-        self._prepare_alternator_cluster(num_of_nodes=num_of_nodes)
-        self._prefill_alternator_table()
-        return self._run_alternator_stress()
-
-    # #############################  Utils #########################################
+        snapshot_folder = tempfile.mkdtemp()
+        self.addCleanup(lambda: shutil.rmtree(snapshot_folder))
+        self.create_snapshot(table_name=TABLE_NAME, node=node1, snapshot_folder=snapshot_folder)
+        self.delete_table(table_name=table_name, node=node1)
+        self.create_table(table_name=table_name, node=node1)
+        self.load_snapshot_and_refresh(table_name=table_name, node=node1, snapshot_folder=snapshot_folder)
+        diff = self.compare_table_data(table_name=table_name, table_data=data_before_refresh, node=node1)
+        self.assertTrue(expr=not diff, msg=f"The following items are missing:\n{pformat(diff)}")
 
     def test_drain_during_dynamo_load(self):
-        get_items_thread = self._generate_alternator_cluster_with_stress()
-        node = self.cluster.nodelist()[-1]
-        debug('Start drain for: {}'.format(node.name))
-        node.drain()
+        self.prepare_cluster()
+        node1, node2, node3 = self.cluster.nodelist()
+        self.create_table(table_name=TABLE_NAME, node=node1)
+        self.generate_request_items(table_name=TABLE_NAME, node=node1)
+        get_items_thread = self.run_stress(table_name=TABLE_NAME, node=node1)
+        debug(f'Start drain for: {node3.name}')
+        node3.drain()
         debug('Drain finished')
         get_items_thread.join()
 
     def test_decommission_during_dynamo_load(self):
-        alternator_consistent_stress = self._generate_alternator_cluster_with_stress()
+        self.prepare_cluster()
         node1, node2, node3 = self.cluster.nodelist()
-        debug('Start first decommission during consistent Alternator-load for: {}'.format(node2.name))
+        self.create_table(table_name=TABLE_NAME, node=node1)
+        self.generate_request_items(table_name=TABLE_NAME, node=node1)
+        alternator_consistent_stress = self.run_stress(table_name=TABLE_NAME, node=node1)
+        debug(f'Start first decommission during consistent Alternator-load for: {node2.name}')
         node2.decommission()
         debug('Decommission finished')
         alternator_consistent_stress.join()
-        alternator_non_consistent_stress = self._run_alternator_stress(consistent_read=False)
-        debug('Start a second decommission during non-consistent alternator-load for: {}'.format(node3.name))
+        alternator_non_consistent_stress = self.run_stress(table_name=TABLE_NAME, node=node1, consistent_read=False)
+        debug(f'Start a second decommission during non-consistent alternator-load for: {node3.name}')
         node3.decommission()
         debug('Decommission finished')
         alternator_non_consistent_stress.join()
 
         debug("Check that the correct error is returned for a consistent read where cluster has no quorum")
         try:
-            get_table_items(table=self._get_dynamodb_client(node=node1).Table(TABLE_NAME), num_of_items=10,
-                            consistent_read=True)
+            self.get_table_items(table_name=TABLE_NAME, node=node1, num_of_items=10, consistent_read=True)
             self.fail(msg="Expected ClientError for Alternator query.")
         except boto3_exceptions.ClientError as query_exp:
             self.assertIn('Cannot achieve consistency level for cl LOCAL_QUORUM',
@@ -105,37 +96,34 @@ class AlternatorTest(Tester):
             self.assertIn('Internal Server Error', query_exp.response['Error']['Code'], msg=query_exp)
 
         debug("Check that the correct error is returned for a resource of a decommissioned node")
-        dynamodb_node2 = self._get_dynamodb_client(node=node2)
-        self.node2_resource_table = dynamodb_node2.Table(TABLE_NAME)
+        dynamodb_api_node2 = self.get_dynamodb_api(node=node2)
+        self.node2_resource_table = dynamodb_api_node2.resource.Table(TABLE_NAME)
         with self.assertRaisesRegexp(boto3_exceptions.EndpointConnectionError, "Could not connect to the endpoint URL"):
-            get_table_items(table=self.node2_resource_table, num_of_items=10, consistent_read=True)
+            self.get_table_items(table_name=TABLE_NAME, node=node2, num_of_items=10, consistent_read=True)
 
     def test_dynamo_reads_after_repair(self):
-        self._prepare_alternator_cluster(num_of_nodes=3)
+        self.prepare_cluster(num_of_nodes=3)
         node1, node2, node3 = self.cluster.nodelist()
         debug(f"Adding data for all nodes except {node2.name}...")
         node2.flush()
         debug(f"Stopping {node2.name}")
         node2.stop(wait_other_notice=True)
-        self._prefill_alternator_table(node=node1)
+        self.create_table(table_name=TABLE_NAME, node=node1)
+        self.generate_request_items(table_name=TABLE_NAME, node=node1)
         debug(f"Starting {node2.name}")
         node2.start(wait_other_notice=True, wait_for_binary_proto=True)
         debug(f"starting repair on {node2.name}...")
         info = node2.repair()
-        debug(info[0])
-        debug(info[1])
-        node2_resource = self._get_dynamodb_client(node=node2)
-        node2_table = node2_resource.Table(TABLE_NAME)
+        debug(f"{info[0]}\n{info[1]}")
         debug(f"Reading Alternator queries from node {node2.name}")
-        get_table_items(table=node2_table, num_of_items=NUM_OF_ITEMS, consistent_read=False)
+        self.get_table_items(table_name=TABLE_NAME, node=node2)
 
     def test_dynamo_queries_on_multi_dc(self):
-        self._prepare_alternator_cluster(num_of_nodes=3, is_multi_dc=True)
+        self.prepare_cluster(num_of_nodes=3, is_multi_dc=True)
         dc1_node = self.cluster.nodelist()[0]
+        self.create_table(table_name=TABLE_NAME, node=dc1_node)
         debug(f"Writing Alternator queries to node {dc1_node.name} on data-center {dc1_node.data_center}")
-        self._prefill_alternator_table(node=dc1_node)
-        dc2_node = [node for node in self.cluster.nodelist() if node.data_center != dc1_node.data_center][0]
-        dc2_node_resource = self._get_dynamodb_client(node=dc2_node)
-        dc2_node_table = dc2_node_resource.Table(TABLE_NAME)
+        self.generate_request_items(table_name=TABLE_NAME, node=dc1_node)
+        dc2_node = next(node for node in self.cluster.nodelist() if node.data_center != dc1_node.data_center)
         debug(f"Reading Alternator queries from node {dc2_node.name} on data-center {dc2_node.data_center}")
-        get_table_items(table=dc2_node_table, num_of_items=NUM_OF_ITEMS, consistent_read=False)
+        self.get_table_items(table_name=TABLE_NAME, node=dc2_node, consistent_read=False)
--- alternator_utils.py
+++ alternator_utils.py
@@ -1,12 +1,26 @@
-from concurrent.futures import ThreadPoolExecutor
+import os
+import shutil
 import threading
-from dtest import debug
+from collections import namedtuple
+from concurrent.futures import ThreadPoolExecutor
+from typing import List, Dict
 
+import boto3
+from ccmlib.scylla_node import ScyllaNode
+from deepdiff import DeepDiff
+from nose.plugins.attrib import attr
+
+from dtest import debug, Tester, info
+
+AlternatorApi = namedtuple("AlternatorApi", ["resource", "client"])
+ALTERNATOR_SNAPSHOT_FOLDER = os.path.join(os.getcwd(), "alternator", "snapshot")
 TABLE_NAME = 'user_table'
+NUM_OF_NODES = 3
 NUM_OF_ITEMS = 100
+ALTERNATOR_PORT = 8080
 
 
-class StoppableThread():
+class StoppableThread:
     """Thread class with a stop() method. it runs the given "target" function in a loop
         until the 'stop-event' is set."""
 
@@ -14,7 +28,7 @@ class StoppableThread():
         self._stop_event = threading.Event()
         self.target = target
         self.target_name = target.__name__
-        self.kwargs = kwargs
+        self.kwargs = kwargs or {}
         self.pool = ThreadPoolExecutor(max_workers=1)
         self.future = None
 
@@ -36,43 +50,185 @@ class StoppableThread():
         debug(f"{self.target_name} is stopped!")
 
 
-def create_dynamodb_table(dynamodb_resource, table_name=TABLE_NAME):
-    debug(f"Creating a table using a resource with node URL: {dynamodb_resource.meta.client.meta.endpoint_url}..")
-    dynamodb_resource.create_table(
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'key',
-                'AttributeType': 'S'
-            },
-        ],
-        BillingMode='PAY_PER_REQUEST',
-        TableName=table_name,
-        KeySchema=[
-            {
-                'AttributeName': 'key',
-                'KeyType': 'HASH'
-            },
-        ])
+@attr('dtest-full')
+class TesterAlternator(Tester):
 
+    def __init__(self, *argv, **kwargs):
+        super().__init__(*argv, **kwargs)
+        self._nodes_url_list = None
+        self.keyspace_name_template = "alternator_{}"
+        self._table_pk = "key"
+        self._dynamo_params = dict(service_name="dynamodb", aws_access_key_id="None", aws_secret_access_key="None",
+                                   region_name="None")
+        self.alternator_apis = {}
 
-def generate_put_request_items(num_of_items: int = NUM_OF_ITEMS) -> list:
-    debug(f"Generating {num_of_items} put request items..")
-    put_request_items = []
-    for idx in range(num_of_items):
-        item = {
-                    'key': f'test{idx}', 'x': {'hello': f'world{idx}'}
-                }
-        put_request_items.append(item)
-    return put_request_items
+    def get_dynamodb_api(self, node: ScyllaNode) -> AlternatorApi:
+        return self.alternator_apis[node.name]
 
+    def prepare_cluster(self, num_of_nodes: int = NUM_OF_NODES, is_multi_dc: bool = False) -> None:
+        cluster_type = "single DC" if not is_multi_dc else "multi DC"
+        debug(f"Populating a cluster with {num_of_nodes} nodes for {cluster_type}..")
+        cluster = self.cluster
+        cluster.set_configuration_options(
+            {"start_native_transport": True, "experimental": True, "alternator_port": ALTERNATOR_PORT})
+        cluster.populate([num_of_nodes, num_of_nodes] if is_multi_dc else num_of_nodes)
+        debug("Starting cluster..")
+        cluster.start(wait_for_binary_proto=True, wait_other_notice=True)
+        for node_idx, node in enumerate(self.cluster.nodelist()):
+            node_alternator_address = f"http://{self.get_ip_from_node(node=node)}:{ALTERNATOR_PORT}"
+            self.alternator_apis[node_idx] = self.alternator_apis[node.name] = AlternatorApi(
+                resource=boto3.resource(endpoint_url=node_alternator_address, **self._dynamo_params),
+                client=boto3.client(endpoint_url=node_alternator_address, **self._dynamo_params)
+            )
 
-def get_table_items(table, num_of_items: int = NUM_OF_ITEMS, verbose: bool = True, consistent_read: bool = True):
-    debug(f"Starting queries of: {num_of_items} items with ConsistentRead = {consistent_read}")
-    if verbose:
-        debug("First Item in range: {}".format(
-            table.get_item(ConsistentRead=consistent_read, Key={'key': 'test0'})['Item']))
-        debug("Last Item in range: {}".format(
-            table.get_item(ConsistentRead=consistent_read, Key={'key': f'test{num_of_items - 1}'})['Item']))
+    # pylint:disable=too-many-arguments
+    def create_table(self, table_name: str, node: ScyllaNode, key_schema: List[Dict[str, str]] = None,
+                     attribute_definitions: List[Dict[str, str]] = None, wait_until_table_exists: bool = True):
+        dynamodb_api = self.get_dynamodb_api(node=node)
+        debug(f"Creating a new table '{table_name}' for node '{node.name}'..")
+        dynamodb_api.resource.create_table(
+            TableName=table_name,
+            KeySchema=key_schema or [{"AttributeName": self._table_pk, "KeyType": "HASH"}],
+            AttributeDefinitions=attribute_definitions or [
+                {"AttributeName": self._table_pk, "AttributeType": "S"}, ],
+            BillingMode="PAY_PER_REQUEST",
+        )
+        if wait_until_table_exists:
+            waiter = dynamodb_api.client.get_waiter('table_exists')
+            waiter.wait(TableName=table_name)
+        info(f"The table '{table_name}' successfully created..")
 
-    for idx in range(num_of_items):
-        table.get_item(ConsistentRead=consistent_read, Key={'key': f'test{idx}'})
+    def delete_table_items(self, table_name: str, node: ScyllaNode, items: List[Dict[str, str]], primary_key: str = None
+                           ) -> None:
+        dynamodb_api = self.get_dynamodb_api(node=node)
+        table = dynamodb_api.resource.Table(name=table_name)
+        primary_key = primary_key or self._table_pk
+        with table.batch_writer() as batch:
+            for item in items:
+                batch.delete_item(Key={primary_key: item[primary_key]})
+        debug(f"Executing flush on node '{node.name}'")
+        node.flush()
+        info(f"All items of table '{table_name}' successfully removed..")
+
+    def delete_table(self, table_name: str, node: ScyllaNode) -> None:
+        node_ks_path = self.get_table_folder(table_name=table_name, node=node)
+        dynamodb_api = self.get_dynamodb_api(node=node)
+        table = dynamodb_api.resource.Table(name=table_name)
+        debug(f"Removing table '{table_name}'")
+        table.delete()
+        waiter = dynamodb_api.client.get_waiter('table_not_exists')
+        waiter.wait(TableName=table_name)
+        debug(f"Removing table keyspace folder '{node_ks_path}' from node '{node.name}'")
+        shutil.rmtree(path=node_ks_path)
+
+    def create_items(self, primary_key: str = None, items: List[Dict[str, str]] = None,
+                     num_of_items: int = NUM_OF_ITEMS) -> List[Dict[str, str]]:
+        items = items or num_of_items
+        primary_key = primary_key or self._table_pk
+        if isinstance(items, int):
+            if items < 1:
+                raise ValueError("The number of items should be greater from 1")
+            items = [{primary_key: f"test{item_idx}", "x": {"hello": f"world{item_idx}"}, }
+                     for item_idx in range(0, items)]
+        return items
+
+    # pylint:disable=too-many-arguments
+    def generate_request_items(self, table_name: str, node: ScyllaNode, items: List[Dict[str, str]] = None,
+                               num_of_items: int = NUM_OF_ITEMS, primary_key: str = None) -> None:
+        dynamodb_api = self.get_dynamodb_api(node=node)
+        items = self.create_items(primary_key=primary_key, items=items, num_of_items=num_of_items)
+        debug(f"Generating '{len(items)}' items for table '{table_name}'..")
+        with dynamodb_api.resource.Table(name=table_name).batch_writer() as batch:
+            for item in items:
+                batch.put_item(item)
+
+    def scan_table(self, table_name: str, node: ScyllaNode) -> list:
+        dynamodb_api = self.get_dynamodb_api(node=node)
+        result, still_running_while = [], True
+        table = dynamodb_api.resource.Table(name=table_name)
+        while still_running_while:
+            response = table.scan()
+            result.extend(response["Items"])
+            still_running_while = 'LastEvaluatedKey' in response
+
+        return result
+
+    def is_table_exists(self, table_name: str, node: ScyllaNode) -> bool:
+        dynamodb_api = self.get_dynamodb_api(node=node)
+        is_table_exists = table_name in dynamodb_api.client.list_tables()["TableNames"]
+        debug(f"The table '{table_name}'{'' if is_table_exists else 'not'} exists in node {node.name}..")
+        return is_table_exists
+
+    def get_table_folder(self, table_name: str, node: ScyllaNode) -> str:
+        node_data_folder_path = os.path.join(node.get_path(), "data")
+        table_folder_name = next((name for name in os.listdir(node_data_folder_path) if name.endswith(table_name)),
+                                 None)
+        if table_folder_name is None:
+            raise FileNotFoundError(f"The folder of table '{table_name}' not found in following path "
+                                    f"'{node_data_folder_path}'")
+        table_folder_path = os.path.join(node_data_folder_path, table_folder_name)
+        scylla_table_files = next((name for name in os.listdir(table_folder_path) if name.startswith(table_name)), None)
+        if scylla_table_files is None:
+            raise FileNotFoundError(f"The folder that contain Scylla files for table '{table_name}' not found in"
+                                    f" following path '{scylla_table_files}'")
+        return os.path.join(table_folder_path, scylla_table_files)
+
+    def create_snapshot(self, table_name: str, snapshot_folder: str, node: ScyllaNode) -> None:
+        keyspace = self.keyspace_name_template.format(table_name)
+        debug(f"Making Alternator snapshot for node '{node.name}'..")
+        debug(node.nodetool(f"snapshot {keyspace} -t {table_name} "))
+        node_table_folder_path = self.get_table_folder(table_name=table_name, node=node)
+        node_snapshot_folder_path = os.path.join(node_table_folder_path, "snapshots", table_name)
+
+        debug(f"Creating local snapshot folder in following path '{snapshot_folder}' and moving all snapshot files to"
+              f" this folder..")
+        for file_name in os.listdir(node_snapshot_folder_path):
+            shutil.copyfile(src=os.path.join(node_snapshot_folder_path, file_name),
+                            dst=os.path.join(snapshot_folder, file_name))
+
+    def load_snapshot_and_refresh(self, table_name: str, node: ScyllaNode, snapshot_folder: str = ""):
+        keyspace_folder_path = self.get_table_folder(table_name=table_name, node=node)
+        snapshot_folder = snapshot_folder or os.path.join(keyspace_folder_path, "snapshots", table_name)
+        upload_folder = os.path.join(keyspace_folder_path, "upload")
+        if not os.path.exists(path=snapshot_folder):
+            raise NotADirectoryError(f"The snapshot folder '{snapshot_folder}' not exists")
+        if not len(os.listdir(snapshot_folder)):
+            raise IsADirectoryError(f"The snapshot folder '{snapshot_folder}' not contain any files")
+
+        debug(f"Loading snapshot files from folder '{snapshot_folder}' to '{upload_folder}'..")
+        for file_name in os.listdir(snapshot_folder):
+            shutil.copyfile(src=os.path.join(snapshot_folder, file_name),
+                            dst=os.path.join(upload_folder, file_name))
+
+        refresh_cmd = f"refresh -- {self.keyspace_name_template.format(table_name)} {table_name}"
+        debug(f"Running following refresh cmd '{refresh_cmd}'..")
+        node.nodetool(refresh_cmd)
+
+    def compare_table_data(self, table_name: str, table_data: List[Dict[str, str]], node: ScyllaNode) -> DeepDiff:
+        data = self.scan_table(table_name=table_name, node=node)
+        return DeepDiff(t1=table_data, t2=data, ignore_order=True)
+
+    def run_stress(self, table_name: str, node: ScyllaNode, num_of_item: int = NUM_OF_ITEMS,
+                   verbose: bool = True, consistent_read: bool = True) -> StoppableThread:
+        debug("Start Alternator stress..")
+        get_items_thread = StoppableThread(target=self.get_table_items, kwargs=dict(
+            table_name=table_name, node=node, num_of_items=num_of_item, verbose=verbose,
+            consistent_read=consistent_read))
+
+        self.addCleanup(get_items_thread.join)
+        get_items_thread.start()
+        return get_items_thread
+
+    def get_table_items(self, table_name: str, node: ScyllaNode, num_of_items: int = NUM_OF_ITEMS,
+                        verbose: bool = True, consistent_read: bool = True):
+        dynamodb_api = self.get_dynamodb_api(node=node)
+        table = dynamodb_api.resource.Table(name=table_name)
+        debug(f"Starting queries of: {num_of_items} items with ConsistentRead = {consistent_read}")
+        if verbose:
+            debug("First Item in range: {}".format(
+                table.get_item(ConsistentRead=consistent_read, Key={'key': 'test0'})['Item']))
+            debug("Last Item in range: {}".format(
+                table.get_item(ConsistentRead=consistent_read, Key={'key': f'test{num_of_items - 1}'})['Item']))
+
+        for idx in range(num_of_items):
+            table.get_item(ConsistentRead=consistent_read, Key={'key': f'test{idx}'})
