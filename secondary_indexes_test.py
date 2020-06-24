c583ebc832eba90d9b3e96828f0c4277adb2b211
Benny Halevy <bhalevy@scylladb.com>
2020-06-24 15:02:28 +0300

    secondary_indexes_test: update view builder error
    Since https://jenkins.scylladb.com/view/master/job/scylla-master/job/dtest-release/531/testReport/secondary_indexes_test/TestSecondaryIndexes/test_stop_node_after_index_build/
    https://jenkins.scylladb.com/view/master/job/scylla-master/job/dtest-release/531/testReport/secondary_indexes_test/TestSecondaryIndexes/test_stop_node_during_index_build/
    and recent changes in scylla
    we're seeing the follwing benign error messages:
    ```
    ERROR 2020-06-24 02:39:58,555 [shard 1] view - Error applying view update to 127.0.68.4: exceptions::mutation_write_failure_exception (Operation failed for ks.b_index_index - received 0 responses and 1 failures from 1 CL=ONE.)
    ```
    
    Update the tests expected errors accordingly.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>
    
    Test: secondary_indexes_test:TestSecondaryIndexes.test_stop_node_{during,after}_index_build

--- secondary_indexes_test.py
+++ secondary_indexes_test.py
@@ -1079,7 +1079,7 @@ class TestSecondaryIndexes(Tester, SecondaryIndexesHelpers):
         exclude_errors = ['Can\'t send migration request: node {} is down'.format(node2_ip),
                           'Error applying view update to {}: exceptions::unavailable_exception \(Cannot achieve consistency level for cl ONE. Requires 1, alive 0\)'.format(node2_ip),
                           'Error applying view update to {}: exceptions::mutation_write_timeout_exception \(Operation timed out for {}.{}_index - received only 0 responses from 1 CL=ONE.\)'.format(node2_ip, keyspace_name, index_name),
-                          'Error applying view update to {}: exceptions::mutation_write_failure_exception \(Operation failed for {}.{}_index - received 0 responses and 1 failures from 1 CL=ONE.\)'.format(node2_ip, keyspace_name, index_name),
+                          'Error applying view update to .*: exceptions::mutation_write_failure_exception \(Operation failed for {}.{}_index - received 0 responses and 1 failures from 1 CL=ONE.\)'.format(keyspace_name, index_name),
                          ]
         self.ignore_log_patterns += exclude_errors
 
@@ -1161,7 +1161,10 @@ class TestSecondaryIndexes(Tester, SecondaryIndexesHelpers):
                                     compaction=self.compaction_strategy),
                         msg='Index %s is not built' % index_name)
 
-        exclude_errors = ['Can\'t send migration request: node {} is down'.format(node2_ip)]
+        exclude_errors = [
+            'Can\'t send migration request: node {} is down'.format(node2_ip),
+            'Error applying view update to .*: exceptions::mutation_write_failure_exception \(Operation failed for {}.{}_index - received 0 responses and 1 failures from 1 CL=ONE.\)'.format(keyspace_name, index_name),
+        ]
         self.ignore_log_patterns += exclude_errors
 
         # Perform action on second node
