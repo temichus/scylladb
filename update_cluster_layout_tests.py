166a7ae316d8224c23b2ed4025037ad2cb7ae868
Benny Halevy <bhalevy@scylladb.com>
2020-08-06 11:10:56 +0300

    update_cluster_layout_tests: simple_kill_remained_node_while_decommissioning_test: no need to sleep when creating ks/cf
    Delete needless sleeps in the code.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- update_cluster_layout_tests.py
+++ update_cluster_layout_tests.py
@@ -848,11 +848,8 @@ class TestUpdateClusterLayout(Tester):
         node1, node2, node3 = cluster.nodelist()
 
         session = self.patient_cql_connection(node1)
-        time.sleep(5)
         self.create_ks(session, 'ks', 1)
-        time.sleep(5)
         self.create_cf(session, 'cf', read_repair=0.0, columns={'c1': 'text', 'c2': 'text'})
-        time.sleep(5)
         insert_c1c2(session, keys=range(1000), consistency=ConsistencyLevel.ONE)
 
         def run():
