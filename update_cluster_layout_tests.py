43ea643596cfa50de19e0ec01aff532cd53b4def
Benny Halevy <bhalevy@scylladb.com>
2020-09-14 17:50:43 +0300

    update_cluster_layout_tests: simple_kill_streaming_node_while_bootstrapping_test: adjust timeout for debug mode
    As seen in https://jenkins.scylladb.com/view/master/job/scylla-master/job/dtest-debug-random/274/testReport/update_cluster_layout_tests/TestUpdateClusterLayout/simple_kill_streaming_node_while_bootstrapping_test/
    It takes a long time for repair in debug mode so increase the deafault timeout of 600 seconds in debug mode
    when watching the log for repair strating message.
    
    Also, the final watch_for_log timeout was broken in fa43234b60d6d48b9cb76f3b545dd618251ff445.
    Fix it too.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- update_cluster_layout_tests.py
+++ update_cluster_layout_tests.py
@@ -297,7 +297,10 @@ class TestUpdateClusterLayout(Tester):
         node4 = new_node(cluster)
         node4.start(jvm_args=['--logger-log-level', 'stream_session=debug'], no_wait=True)
         node4.watch_log_for("JOINING: Starting to bootstrap")
-        node4.watch_log_for("Beginning stream session|sync data for keyspace=ks[1-3]?, status=started")
+        log_timeout=600
+        if isinstance(self.cluster, ScyllaCluster) and self.cluster.scylla_mode == 'debug':
+            log_timeout *= 3
+        node4.watch_log_for("Beginning stream session|sync data for keyspace=ks[1-3]?, status=started", timeout=log_timeout)
 
         self.ignore_log_patterns += [
             r'Repair .* status=failed: mandatory neighbor={} is not alive'.format(node2.address()),
@@ -311,7 +314,7 @@ class TestUpdateClusterLayout(Tester):
         debug("Look for Stream/Startup failed in node 4...")
         # The keep alive timer expires in 10 minutes.
         # Wait 5 minutes more in the test to wait for the stream to fail
-        node4.watch_log_for("Stream failed|Startup failed|sync data for keyspace=ks[1-3]?, status=failed", timeout=300)
+        node4.watch_log_for("Stream failed|Startup failed|sync data for keyspace=ks[1-3]?, status=failed", timeout=900)
 
     def simple_kill_new_node_while_bootstrapping_test(self):
         """
