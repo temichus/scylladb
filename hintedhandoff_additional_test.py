temichus/scylladb

commit 1e221d68e55223d855e31f475478eb42713d549f

hintedhandoff_additional_test: hintedhandoff_dont_revive_test: fix call to __jvm_args
Node parameter removed in 08d51171f40f6be98d8faa2906e48afb69c3d8fd
but this call site was overlooked unintentionally.

Caused following failure in
https://jenkins.scylladb.com/view/master/job/scylla-master/job/dtest-release/647/testReport/hintedhandoff_additional_test/TestHintedHandoff/hintedhandoff_dont_revive_test/:
```
Traceback (most recent call last):
  File "/usr/lib64/python3.7/unittest/case.py", line 60, in testPartExecutor
    yield
  File "/usr/lib64/python3.7/unittest/case.py", line 645, in run
    testMethod()
  File "/jenkins/workspace/scylla-master/dtest-release/scylla-dtest/hintedhandoff_additional_test.py", line 398, in hintedhandoff_dont_revive_test
    hinting_node.start(wait_for_binary_proto=True, jvm_args=self.__jvm_args(hinting_node))
  File "/jenkins/workspace/scylla-master/dtest-release/scylla-ccm/ccmlib/scylla_node.py", line 427, in start
    if '=' in jvm_arg:
TypeError: argument of type 'ScyllaNode' is not iterable
```

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

hintedhandoff_additional_test.py
@@ -395,7 +395,7 @@ class TestHintedHandoff(Tester):
         delete_c1c2(session, n=1, consistency=ConsistencyLevel.TWO)
 
         debug("Starting {}".format(hinting_node.name))
-        hinting_node.start(wait_for_binary_proto=True, jvm_args=self.__jvm_args(hinting_node))
+        hinting_node.start(wait_for_binary_proto=True, jvm_args=self.__jvm_args())
 
         debug("Waiting {}s for hints to be sent...".format(self.__hint_flush_threshold))
         time.sleep(self.__hint_flush_threshold)
@@ -467,6 +467,12 @@ class TestHintedHandoff(Tester):
     def __jvm_args(self, hh_enabled_value=None):
         hh_enabled = 'true'
         if not hh_enabled_value is None:
+            if isinstance(hh_enabled_value, bool):
+                hh_enabled_value = str(hh_enabled_value).lower()
+            else:
+                assert isinstance(hh_enabled_value, str)
+                hh_enabled_value = hh_enabled_value.lower()
+                assert hh_enabled_value == 'true' or hh_enabled_value == 'false'
             hh_enabled = hh_enabled_value
 
         return ['--hinted-handoff-enabled', hh_enabled, '--logger-log-level', 'hints_manager=trace']

commit a4cdadf45a4d1ad4c3d947239797b6cb0695de93

hintedhandoff_additional_test: improve hh_enabled_value sanitization
The assert introduced in 1e221d68e55223d855e31f475478eb42713d549f
is too strict as scylla `--hinted-handoff-enabled` option can
be either boolean (true|false|1|0), or a comma separated list
of data center names for which to enable hintedhandoffs.

Thus syntax is used in `hintedhandoff_rebalance_test` that
passes a non-existing dc name ('dont_send_hints') on purpose.

The assertion failure can be seen in
https://jenkins.scylladb.com/view/master/job/scylla-master/job/dtest-release/648/testReport/hintedhandoff_additional_test/TestHintedHandoff/hintedhandoff_rebalance_test/
```
Traceback (most recent call last):
  File "/usr/lib64/python3.7/unittest/case.py", line 60, in testPartExecutor
    yield
  File "/usr/lib64/python3.7/unittest/case.py", line 645, in run
    testMethod()
  File "/jenkins/workspace/scylla-master/dtest-release/scylla-dtest/hintedhandoff_additional_test.py", line 57, in hintedhandoff_rebalance_test
    self.__start_all([node1, node2, node3], hh_enabled_value='dont_send_hints', extra_jvm_args=['--smp', '3'])
  File "/jenkins/workspace/scylla-master/dtest-release/scylla-dtest/hintedhandoff_additional_test.py", line 520, in __start_all
    jvm_args=self.__jvm_args(hh_enabled_value=hh_enabled_value) + extra_jvm_args)
  File "/jenkins/workspace/scylla-master/dtest-release/scylla-dtest/hintedhandoff_additional_test.py", line 475, in __jvm_args
    assert hh_enabled_value == 'true' or hh_enabled_value == 'false'
```

This change introduces `__sanitize_hh_enabled_value` that
verifies the value and returns a sanitized string.

We call it both in `__jvm_args` and in `__start_all`
for printing a sensible debug string.
Previously, it would falsely print "enabled" even if
the value was "false", as a string holding "false" is
implicitly converted to a True boolean value since it
is not an empty.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

hintedhandoff_additional_test.py
@@ -1,5 +1,6 @@
 from unittest import skip
 from cassandra import ConsistencyLevel
+from distutils.util import strtobool
 
 from dtest import Tester, debug, wait_for
 import glob
@@ -464,17 +465,29 @@ class TestHintedHandoff(Tester):
         """
         return 15
 
-    def __jvm_args(self, hh_enabled_value=None):
-        hh_enabled = 'true'
-        if not hh_enabled_value is None:
-            if isinstance(hh_enabled_value, bool):
-                hh_enabled_value = str(hh_enabled_value).lower()
-            else:
-                assert isinstance(hh_enabled_value, str)
-                hh_enabled_value = hh_enabled_value.lower()
-                assert hh_enabled_value == 'true' or hh_enabled_value == 'false'
-            hh_enabled = hh_enabled_value
+    @staticmethod
+    def __sanitize_hh_enabled_value(v):
+        """
+        hh_enabled_value should be set either to a boolean value (true|false|1|0)
+        or to one or more DCs for which hintedhandoffs are to be enabled.
+        If None, this function returns 'true', otherwise it verifies the value's
+        syntax and normalizes it.
+        """
+        if v is None:
+            return 'true'
+        if isinstance(v, bool):
+            return str(v).lower()
+        if isinstance(v, int):
+            assert v in [ 0, 1 ], f"'{v}' must be either '0' or '1'"
+            return str(v)
+        assert isinstance(v, str), f"'{v}' is not a string"
+        assert v != '', "hh_enabled_value must not be empty"
+        if v.lower() in [ 'true', 'false' ]:
+            return v.lower()
+        return v
 
+    def __jvm_args(self, hh_enabled_value=None):
+        hh_enabled = self.__sanitize_hh_enabled_value(hh_enabled_value)
         return ['--hinted-handoff-enabled', hh_enabled, '--logger-log-level', 'hints_manager=trace']
 
     def __start_cluster_with_hints(self, num, custom_args=[], hh_enabled_value=None):
@@ -514,8 +527,14 @@ class TestHintedHandoff(Tester):
         self.cluster.stop_nodes(nodes)
 
     def __start_all(self, nodes, hh_enabled_value=None, extra_jvm_args=[]):
+        hh_enabled = self.__sanitize_hh_enabled_value(hh_enabled_value)
+        hh_description = ''
+        try:
+            hh_description = "enabled" if strtobool(hh_enabled) else "disabled"
+        except ValueError:
+            hh_description = f'"{hh_enabled}"'
         debug("Starting {} with hintedhandoff {}".format([n.name for n in nodes],
-                                                         "enabled" if hh_enabled_value is None or hh_enabled_value else "disabled"))
+                                                         hh_description))
         self.cluster.start_nodes(wait_for_binary_proto=True,
                                  jvm_args=self.__jvm_args(hh_enabled_value=hh_enabled_value) + extra_jvm_args)
 