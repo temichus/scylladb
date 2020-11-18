776c9c1581eb676d5f7047cd5c2f8b62cc0a7b7e
juliayakovlev <julia.yakovlev@scylladb.com>
2020-11-18 17:49:41 +0200

    tests(sstableloader): support sstableloader --ignore-missing-columns option
    New tests for support sstableloader --ignore-missing-columns option

--- sstableloader_test.py
+++ sstableloader_test.py
@@ -1,17 +1,19 @@
-import subprocess
-import shutil
 import os
+import shutil
+import subprocess
+from typing import Optional
 
+from cassandra.cluster import Session
 
+from ccmlib.scylla_node import ScyllaNode
 from migration_test import MigrationTestBase
 from dtest import Tester, debug
-from tools import safe_mkdtemp
-from scylla_tools import insert_c1c2
+from tools import safe_mkdtemp, require
+from scylla_tools import insert_c1c2, get_cf_dir, copy_files_to
+
 # from nose import tools
 from nose.plugins.attrib import attr
 from assertions import assert_none, assert_one, assert_all
-
-# @tools.istest
 
 
 @attr('dtest-full', 'single_node')
@@ -189,52 +191,350 @@ for version in versions:
 
 @attr('dtest-full', 'single_node')
 class AdditionalTestSSTableLoader(Tester):
-
     __test__ = True
 
-    def option_ignore_missing_columns_test(self):
-        """
-        Verify `--ignore-missing-columns` option of sstableloader works
-        Related issue: https://github.com/scylladb/scylla/issues/6990
-        """
+    def prepare_cluster(self, nodes: int = 1) -> (Session, ScyllaNode):
         cluster = self.cluster
-        cluster.populate(1).start(wait_for_binary_proto=True)
+        cluster.populate(nodes).start(wait_for_binary_proto=True)
         node1 = cluster.nodelist()[0]
 
         # Prepare test table and test data
         session = self.patient_cql_connection(node1)
-        self.create_ks(session, 'ks', 1)
-        self.create_cf(session, 'cf', columns={'c1': 'text', 'c2': 'text'})
-        insert_c1c2(session, n=10)
+        return session, node1
+
+    def create_cf_sstable_copy(self, ks: str, cf: str) -> (str, str):
+        orig_cf_dir = get_cf_dir(os.path.join(self.test_path, 'test', 'node1', 'data', ks), cf_name=cf)
+        assert orig_cf_dir is not None, 'table folder is not found'
+
+        tmpdir = safe_mkdtemp()
+        copy_cf_dir = os.path.join(tmpdir, ks, cf)
+        os.mkdir(os.path.join(tmpdir, ks))
+        os.mkdir(copy_cf_dir)
+
+        debug("Copying sstables ")
+        copy_files_to(orig_cf_dir, copy_cf_dir, files_only=True)
+        return copy_cf_dir, tmpdir
+
+    def prepare_cf(self, session: Session, cf_name: str, data: list):
+        session.execute(f"CREATE COLUMNFAMILY {cf_name} (key int, c1 text, c2 text, c3 text, "
+                        f"PRIMARY KEY(key, c1))")
+
+        for insert in ["insert into cf (key, c1, c2, c3) values ({}, '{}', '{}', '{}')".format(*one_set)
+                       for one_set in data]:
+            session.execute(insert)
+
+        # for i in range(rows):
+        #     session.execute(f"insert into cf (key, c1, c2, c3) values ({i}, 'a', 'b', 'c')")
+
+    @staticmethod
+    def set_test_data(rows: int) -> list:
+        return [[i, f'a{i}', f'b{i}', f'c{i}'] for i in range(rows)]
+
+    @staticmethod
+    def remove_column_from_data(data: list, element_to_remove_index: list) -> list:
+        """
+        element_to_remove_index: index of the removed column in the data
+        Example:
+                     key c1    c2    c3
+                     ====================
+            data = [[0, 'a0', 'b0', 'c0'],
+                    [1, 'a1', 'b1', 'c1']
+                  ]
+            Column "c2" was dropped. So to remove its values, we need to remove element with "c2" index in the list = 2
+        """
+        for i, one in enumerate(data):
+            for ind in element_to_remove_index:
+                del data[i][ind]
+
+        return data
+
+    def run_sstableloader(self, node: ScyllaNode, copy_cf_dir: str, ignore_columns: str = '', err: str = None,
+                          completed: Optional[None or bool] = None):
+        debug('Run sstableloader on node1')
+        ip = node.address()
+
+        cmd = [node.get_tool('sstableloader'), '-d', ip]
+        if ignore_columns:
+            cmd.extend(['--ignore-missing-columns', ignore_columns])
+        cmd.extend([copy_cf_dir, '-v'])
+
+        debug(cmd)
+        result = subprocess.run(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE, check=True)
+
+        if completed is not None:
+            if completed:
+                assert '100% done.' in str(result.stdout)
+            else:
+                assert ' 0% done.' in str(result.stdout)
+
+        if err is not None:
+            assert err in str(result.stderr)
+
+        if not err and result.stderr:
+            raise Exception(
+                f"sstableloader command failed, exit status: {result.returncode},"
+                f"\n\nSTDERR: {result.stderr}\n,\nSTDOUT: {result.stdout}")
+
+    @require('scylla-tools-java/#216')
+    def ignore_missing_columns_by_drop_column_from_snapshot_test(self):
+        """
+        Verify `--ignore-missing-columns` option of sstableloader works
+        Drop one column and load the data from snapshot with `--ignore-missing-columns` parameter
+
+        - create table
+        - insert 10 rows and flush
+        - take snapshot
+        - drop one column
+        - load from snapshot
+        - validate that all 10 rows in the table
+
+        Related issues: https://github.com/scylladb/scylla/issues/6990 and
+                        https://github.com/scylladb/scylla-tools-java/issues/216
+        """
+        ks = 'ks'
+        cf = 'cf'
+        rows = 10
+        session, node1 = self.prepare_cluster()
+        self.create_ks(session, ks, 1)
+        data = self.set_test_data(rows=rows)
+        self.prepare_cf(session=session, cf_name=cf, data=data)
         node1.flush()
+        assert_one(session, f'SELECT count(*) FROM {ks}.{cf}', expected=[rows])
 
         # Create snapshot
         snapshot_name = 'test_snapshot'
-        node1.nodetool(f"snapshot -t {snapshot_name} -cf cf -- ks")
+        node1.nodetool(f"snapshot -t {snapshot_name} -cf {cf} -- {ks}")
 
         snapshot_dir = None
-        for root, dirs, files in os.walk(os.path.join(self.test_path, 'test', 'node1', 'data', 'ks')):
+        for root, dirs, files in os.walk(os.path.join(self.test_path, 'test', 'node1', 'data', ks)):
             for name in dirs:
                 if name == snapshot_name:
                     snapshot_dir = os.path.join(root, name)
         assert snapshot_dir is not None, 'snapshot_dir is not found'
-        cf_dir = os.path.join(snapshot_dir, '..')
 
         # Clean data and drop one column
-        session.execute("TRUNCATE ks.cf")
-        session.execute("ALTER TABLE ks.cf DROP c2")
+        session.execute(f"TRUNCATE {ks}.{cf}")
+        session.execute(f"ALTER TABLE {ks}.{cf} DROP c2")
 
         # Load snapshot by sstableloader
-        ip = node1.address()
-        debug('Run sstableloader on node1')
-        cmd = [node1.get_tool('sstableloader'), '-d', ip, '--ignore-missing-columns', 'c2', f'{snapshot_dir}']
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = p.communicate()
-        exit_status = p.wait()
-        if exit_status != 0:
-            raise Exception(
-                f"sstableloader command failed, exit status: {exit_status}, stdout: {stdout}, stderr: {stderr}")
-        assert_none(session, 'SELECT * FROM ks.cf')
+        self.run_sstableloader(node=node1, copy_cf_dir=snapshot_dir, ignore_columns='c2')
+
+        # Remove data of dropped column
+        data = self.remove_column_from_data(data=data, element_to_remove_index=[2])
+        assert_all(session, f'SELECT key, c1, c3 FROM {ks}.{cf}', expected=data, ignore_order=True)
+
+    @require('scylla-tools-java/#216')
+    def ignore_missing_columns_by_drop_table_from_snapshot_test(self):
+        """
+        Verify `--ignore-missing-columns` option of sstableloader works
+        Drop one column and load the data from snapshot with `--ignore-missing-columns` parameter
+
+        - create table
+        - insert 10 rows and flush
+        - take snapshot
+        - drop one column
+        - load from snapshot
+        - validate that all 10 rows in the table
+
+        Related issues: https://github.com/scylladb/scylla/issues/6990 and
+                        https://github.com/scylladb/scylla-tools-java/issues/216
+        """
+        ks = 'ks'
+        cf = 'cf'
+        rows = 10
+        session, node1 = self.prepare_cluster()
+        self.create_ks(session, ks, 1)
+        data = self.set_test_data(rows=rows)
+        self.prepare_cf(session=session, cf_name=cf, data=data)
+        node1.flush()
+        assert_one(session, f'SELECT count(*) FROM {ks}.{cf}', expected=[rows])
+
+        # Create snapshot
+        snapshot_name = 'test_snapshot'
+        node1.nodetool(f"snapshot -t {snapshot_name} -cf {cf} -- {ks}")
+
+        snapshot_dir = None
+        for root, dirs, files in os.walk(os.path.join(self.test_path, 'test', 'node1', 'data', ks)):
+            for name in dirs:
+                if name == snapshot_name:
+                    snapshot_dir = os.path.join(root, name)
+        assert snapshot_dir is not None, 'snapshot_dir is not found'
+
+        # Clean data and drop one column
+        session.execute(f"DROP TABLE {ks}.{cf}")
+        session.execute(f"CREATE COLUMNFAMILY {cf} (key int, c1 text, c3 text, PRIMARY KEY(key, c1))")
+
+        # Load snapshot by sstableloader
+        self.run_sstableloader(node=node1, copy_cf_dir=snapshot_dir, ignore_columns='c2')
+
+        # Remove data of dropped column
+        data = self.remove_column_from_data(data=data, element_to_remove_index=[2])
+        assert_all(session, f'SELECT key, c1, c3 FROM {ks}.{cf}', expected=data, ignore_order=True)
+
+    def ignore_missing_one_column_by_drop_table_from_backup_test(self):
+        """
+        Verify `--ignore-missing-columns` option of sstableloader works
+        Recreate table with one column less and load the data from backup folder with `--ignore-missing-columns`
+        parameter
+
+        - create table
+        - insert 10 rows and flush
+        - copy sstables to tmp backup folder
+        - drop table
+        - create table with one column less
+        - load from backup folder
+        - validate that all 10 rows in the table
+
+        Related issue: https://github.com/scylladb/scylla/issues/6990
+        """
+        ks = 'ks'
+        cf = 'cf'
+        rows = 10
+        session, node1 = self.prepare_cluster()
+        self.create_ks(session=session, name=ks, rf=1)
+        data = self.set_test_data(rows=rows)
+        self.prepare_cf(session=session, cf_name=cf, data=data)
+        node1.flush()
+        assert_one(session, f'SELECT count(*) FROM {ks}.{cf}', expected=[rows])
+
+        copy_cf_dir, tmpdir = self.create_cf_sstable_copy(ks, cf)
+
+        # Drop table and recreate with miss 1 column
+        session.execute(f"DROP TABLE {cf}")
+        session.execute(f"CREATE COLUMNFAMILY {cf} (key int, c1 text, c3 text, PRIMARY KEY(key, c1))")
+
+        # Load snapshot by sstableloader
+        self.run_sstableloader(node=node1, copy_cf_dir=copy_cf_dir, ignore_columns='c2')
+
+        # Drop temp directory
+        shutil.rmtree(tmpdir)
+
+        # Remove data of dropped column
+        data = self.remove_column_from_data(data=data, element_to_remove_index=[2])
+        assert_all(session, f'SELECT key, c1, c3 FROM {ks}.{cf}', expected=data, ignore_order=True)
+
+    def ignore_missing_one_column_by_drop_column_from_backup_test(self):
+        """
+        Verify `--ignore-missing-columns` option of sstableloader works
+        Drop one column and load the data from backup folder with `--ignore-missing-columns` parameter
+
+        - create table
+        - insert 10 rows and flush
+        - copy sstables to tmp backup folder
+        - drop one column
+        - load from backup folder
+        - validate that all 10 rows in the table
+
+        Related issue: https://github.com/scylladb/scylla/issues/6990
+        """
+        ks = 'ks'
+        cf = 'cf'
+        rows = 10
+        session, node1 = self.prepare_cluster()
+        self.create_ks(session, ks, 1)
+        data = self.set_test_data(rows=rows)
+        self.prepare_cf(session=session, cf_name=cf, data=data)
+        node1.flush()
+        assert_one(session, f'SELECT count(*) FROM {ks}.{cf}', expected=[rows])
+
+        copy_cf_dir, tmpdir = self.create_cf_sstable_copy(ks, cf)
+
+        # Truncate table and drop 1 column
+        session.execute(f"TRUNCATE TABLE {cf}")
+        session.execute(f"ALTER TABLE {cf} DROP c2")
+
+        # Load snapshot by sstableloader
+        self.run_sstableloader(node=node1, copy_cf_dir=copy_cf_dir, ignore_columns='c2')
+
+        # Drop temp directory
+        shutil.rmtree(tmpdir, ignore_errors=True)
+
+        # Remove data of dropped column
+        data = self.remove_column_from_data(data=data, element_to_remove_index=[2])
+        assert_all(session, f'SELECT key, c1, c3 FROM {ks}.{cf}', expected=data, ignore_order=True)
+
+    def ignore_missing_two_column_by_drop_column_from_backup_test(self):
+        """
+        Verify `--ignore-missing-columns` option of sstableloader works
+        Drop two columns and load the data from backup folder with `--ignore-missing-columns` parameter
+
+        - create table
+        - insert 10 rows and flush
+        - copy sstables to tmp backup folder
+        - drop two columns
+        - load from backup folder
+        - validate that all 10 rows in the table
+
+        Related issue: https://github.com/scylladb/scylla/issues/6990
+        """
+        ks = 'ks'
+        cf = 'cf'
+        rows = 10
+        session, node1 = self.prepare_cluster()
+        self.create_ks(session, ks, 1)
+        data = self.set_test_data(rows=rows)
+        self.prepare_cf(session=session, cf_name=cf, data=data)
+        node1.flush()
+        assert_one(session, f'SELECT count(*) FROM {ks}.{cf}', expected=[rows])
+
+        copy_cf_dir, tmpdir = self.create_cf_sstable_copy(ks, cf)
+
+        # Truncate table and drop 2 column
+        session.execute(f"TRUNCATE TABLE {cf}")
+        session.execute(f"ALTER TABLE {cf} DROP (c2, c3)")
+
+        # Load snapshot by sstableloader
+        self.run_sstableloader(node=node1, copy_cf_dir=copy_cf_dir, ignore_columns='c2,c3')
+
+        # Drop temp directory
+        shutil.rmtree(tmpdir, ignore_errors=True)
+
+        # Remove data of dropped columns
+        data = self.remove_column_from_data(data=data, element_to_remove_index=[2, -1])
+        assert_all(session, f'SELECT key, c1 FROM {ks}.{cf}', expected=data, ignore_order=True)
+
+    @require('scylla-tools-java/214')
+    def ignore_missing_two_column_by_drop_table_from_backup_test(self):
+        """
+        Verify `--ignore-missing-columns` option of sstableloader works
+        Recreate table with two columns less and load the data from backup folder with `--ignore-missing-columns`
+        parameter
+
+        - create table
+        - insert 10 rows and flush
+        - copy sstables to tmp backup folder
+        - drop table
+        - create table with two columns less
+        - load from backup folder
+        - validate that all 10 rows in the table
+
+        Related issue: https://github.com/scylladb/scylla/issues/6990
+        """
+        ks = 'ks'
+        cf = 'cf'
+        rows = 10
+        session, node1 = self.prepare_cluster()
+        self.create_ks(session, ks, 1)
+        data = self.set_test_data(rows=rows)
+        self.prepare_cf(session=session, cf_name=cf, data=data)
+        node1.flush()
+        assert_one(session, f'SELECT count(*) FROM {ks}.{cf}', expected=[rows])
+
+        copy_cf_dir, tmpdir = self.create_cf_sstable_copy(ks, cf)
+
+        # Drop table and recreate with miss 2 column
+        session.execute(f"DROP TABLE {cf}")
+        session.execute(f"CREATE COLUMNFAMILY {cf} (key int, c1 text, PRIMARY KEY(key, c1))")
+
+        # Load snapshot by sstableloader
+        self.run_sstableloader(node=node1, copy_cf_dir=copy_cf_dir, ignore_columns='c2,c3')
+
+        # Drop temp directory
+        shutil.rmtree(tmpdir, ignore_errors=True)
+
+        # Remove data of dropped columns
+        data = self.remove_column_from_data(data=data, element_to_remove_index=[2, -1])
+        assert_all(session, f'SELECT key, c1 FROM {ks}.{cf}', expected=data, ignore_order=True)
 
     def invalid_sstable_test(self):
         """Test with unsupported version and index missing, sstable won't success"""
@@ -246,38 +546,17 @@ class AdditionalTestSSTableLoader(Tester):
         self.create_ks(session, 'ks', 1)
         self.create_cf(session, 'cf', columns={'c1': 'text', 'c2': 'text'})
 
-        def run_sstableloader(sstable_dir, err, completed=True):
-            """Execute sstableloader to load the sstables"""
-            ip = node1.address()
-            debug(f'Run sstableloader on node1, sstable_dir: {sstable_dir}')
-            cmd = [node1.get_tool('sstableloader'), '-d', ip, sstable_dir, '-v']
-            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stdout, stderr = p.communicate()
-            exit_status = p.wait()
-
-            debug(f"sstableloader stderr: {stderr}")
-            if completed:
-                assert '100% done.' in str(stdout)
-            else:
-                assert ' 0% done.' in str(stdout)
-            if err is not None:
-                assert err in str(stderr)
-            if exit_status != 0:
-                raise Exception("sstableloader command failed, "
-                                f"exit status: {exit_status}, stdout: {stdout}, stderr: {stderr}")
-
         # Missing index
         # INFO: "Skipping file md-2-big-Data.db - missing index"
-        run_sstableloader('test-sstables/missing_index/ks/cf-test',
-                          None, completed=False)
+        self.run_sstableloader(node=node1, copy_cf_dir='test-sstables/missing_index/ks/cf-test', completed=False)
         assert_none(session, "SELECT * FROM ks.cf")
 
         # Unknown format / unsupported version
-        run_sstableloader('test-sstables/unknown_format/ks/cf-test',
-                          'Skipping file md---big-Data.db - unsupported SSTable format or version',
-                          completed=False)
+        self.run_sstableloader(node=node1, copy_cf_dir='test-sstables/unknown_format/ks/cf-test',
+                               err='Skipping file md---big-Data.db - unsupported SSTable format or version',
+                               completed=False)
         assert_none(session, "SELECT * FROM ks.cf")
 
         # Test with right sstable
-        run_sstableloader('test-sstables/original/ks/cf-test', None, completed=True)
+        self.run_sstableloader(node=node1, copy_cf_dir='test-sstables/original/ks/cf-test', completed=True)
         assert_all(session, "SELECT * FROM ks.cf", [['k0', 'c1', 'c2']])
