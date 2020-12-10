947ce643839552c3f3750484e7d68e99219e3687
Benny Halevy <bhalevy@scylladb.com>
2020-12-10 10:54:16 +0200

    internode_ssl_test: add expected error
    As seen in https://jenkins.scylladb.com/view/master/job/scylla-master/job/dtest-debug/671/artifact/logs-debug.2/1607560242137_internode_ssl_test.TestInternodeSSL.putget_with_internode_rack_ssl_with_dc_compression_test/node5.log
    this error is benign in this test.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- internode_ssl_test.py
+++ internode_ssl_test.py
@@ -76,7 +76,8 @@ class TestInternodeSSL(Tester):
             'connection dropped: The TLS connection was non-properly terminated',
             'connection dropped: The certificate is NOT trusted',
             'connection dropped: sendmsg: Broken pipe',
-            'connection dropped: The specified session has been invalidated for some reason'
+            'connection dropped: The specified session has been invalidated for some reason',
+            'storage_service -.*fail to update tokens for',
         ]
 
         if reload_certs:
