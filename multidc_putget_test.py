875103d03354b9683ed34cd8bfd43d78f4295201
Israel Fruchter <fruch@scylladb.com>
2021-01-17 15:33:31 +0200

    multidc_putget_test.py: migrate to pytest

--- multidc_putget_test.py
+++ multidc_putget_test.py
@@ -1,31 +1,32 @@
-from dtest import Tester
-from tools import putget
-from nose.plugins.attrib import attr
+import pytest
+
+from dtest_class import Tester, create_ks, create_cf
+from tools.data import putget
 
 
-@attr('dtest-full')
+@pytest.mark.dtest_full
 class TestMultiDCPutGet(Tester):
 
-    @attr('next-gating')
-    @attr('dtest-debug')
-    def putget_2dc_rf1_test(self):
+    @pytest.mark.next_gating
+    @pytest.mark.dtest_debug
+    def test_putget_2dc_rf1(self):
         """ Simple put-get test for 2 DC with one node each (RF=1) [catches #3539] """
         cluster = self.cluster
         cluster.populate([1, 1]).start()
 
         session = self.patient_cql_connection(cluster.nodelist()[0])
-        self.create_ks(session, 'ks', {'dc1': 1, 'dc2': 1})
-        self.create_cf(session, 'cf')
+        create_ks(session=session, name='ks', rf={'dc1': 1, 'dc2': 1})
+        create_cf(session=session, name='cf')
 
         putget(cluster, session)
 
-    def putget_2dc_rf2_test(self):
+    def test_putget_2dc_rf2(self):
         """ Simple put-get test for 2 DC with 2 node each (RF=2) -- tests cross-DC efficient writes """
         cluster = self.cluster
         cluster.populate([2, 2]).start()
 
         session = self.patient_cql_connection(cluster.nodelist()[0])
-        self.create_ks(session, 'ks', {'dc1': 2, 'dc2': 2})
-        self.create_cf(session, 'cf')
+        create_ks(session=session, name='ks', rf={'dc1': 2, 'dc2': 2})
+        create_cf(session=session, name='cf')
 
         putget(cluster, session)
