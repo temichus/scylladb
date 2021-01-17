153eed08acb658f530f26f6085254d6e220627a6
Israel Fruchter <fruch@scylladb.com>
2021-01-17 16:19:20 +0200

    consistent_bootstrap_test.py: migrate to pytest

--- consistent_bootstrap_test.py
+++ consistent_bootstrap_test.py
@@ -1,16 +1,21 @@
-from dtest import Tester, debug
-from tools import (create_c1c2_table, insert_c1c2, query_c1c2,
-                   new_node, no_vnodes)
+import logging
+
+import pytest
 from cassandra import ConsistencyLevel
-from nose.plugins.attrib import attr
+
+from dtest_class import Tester, create_ks
+from tools.data import create_c1c2_table, insert_c1c2, query_c1c2
 
 
-@attr('dtest-full')
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.dtest_full
 class TestBootstrapConsistency(Tester):
 
-    @no_vnodes()
-    def consistent_reads_after_move_test(self):
-        debug("Creating a ring")
+    @pytest.mark.no_vnodes
+    def test_consistent_reads_after_move(self):
+        logger.info("Creating a ring")
         cluster = self.cluster
         cluster.set_configuration_options(values={'hinted_handoff_enabled': False, 'write_request_timeout_in_ms': 60000,
                                                   'read_request_timeout_in_ms': 60000, 'dynamic_snitch_badness_threshold': 0.0}, batch_commitlog=True)
@@ -19,39 +24,39 @@ class TestBootstrapConsistency(Tester):
         node1, node2, node3 = cluster.nodelist()
         cluster.start()
 
-        debug("Set to talk to node 2")
+        logger.info("Set to talk to node 2")
         n2session = self.patient_cql_connection(node2)
-        self.create_ks(n2session, 'ks', 2)
+        create_ks(n2session, 'ks', 2)
         create_c1c2_table(self, n2session)
 
-        debug("Generating some data for all nodes")
+        logger.info("Generating some data for all nodes")
         insert_c1c2(n2session, keys=range(10, 20), consistency=ConsistencyLevel.ALL)
 
         node1.flush()
-        debug("Taking down node1")
+        logger.info("Taking down node1")
         node1.stop(wait_other_notice=True)
 
-        debug("Writing data to node2")
+        logger.info("Writing data to node2")
         insert_c1c2(n2session, keys=range(30, 1000), consistency=ConsistencyLevel.ONE)
         node2.flush()
 
-        debug("Restart node1")
+        logger.info("Restart node1")
         node1.start(wait_other_notice=True)
 
-        debug("Move token on node3")
+        logger.info("Move token on node3")
         node3.move(2)
 
-        debug("Checking that no data was lost")
+        logger.info("Checking that no data was lost")
         for n in range(10, 20):
             query_c1c2(n2session, n, ConsistencyLevel.ALL)
 
         for n in range(30, 1000):
             query_c1c2(n2session, n, ConsistencyLevel.ALL)
 
-    @attr('next-gating')
-    @attr('dtest-debug')
-    def consistent_reads_after_bootstrap_test(self):
-        debug("Creating a ring")
+    @pytest.mark.next_gating
+    @pytest.mark.dtest_debug
+    def test_consistent_reads_after_bootstrap(self):
+        logger.info("Creating a ring")
         cluster = self.cluster
         cluster.set_configuration_options(values={'hinted_handoff_enabled': False, 'write_request_timeout_in_ms': 60000,
                                                   'read_request_timeout_in_ms': 60000, 'dynamic_snitch_badness_threshold': 0.0}, batch_commitlog=True)
@@ -60,32 +65,32 @@ class TestBootstrapConsistency(Tester):
         node1, node2 = cluster.nodelist()
         cluster.start(wait_for_binary_proto=True, wait_other_notice=True)
 
-        debug("Set to talk to node 2")
+        logger.info("Set to talk to node 2")
         n2session = self.patient_cql_connection(node2)
-        self.create_ks(n2session, 'ks', 2)
+        create_ks(n2session, 'ks', 2)
         create_c1c2_table(self, n2session)
 
-        debug("Generating some data for all nodes")
+        logger.info("Generating some data for all nodes")
         insert_c1c2(n2session, keys=range(10, 20), consistency=ConsistencyLevel.ALL)
 
         node1.flush()
-        debug("Taking down node1")
+        logger.info("Taking down node1")
         node1.stop(wait_other_notice=True)
 
-        debug("Writing data to only node2")
+        logger.info("Writing data to only node2")
         insert_c1c2(n2session, keys=range(30, 1000), consistency=ConsistencyLevel.ONE)
         node2.flush()
 
-        debug("Restart node1")
+        logger.info("Restart node1")
         node1.start(wait_other_notice=True)
 
-        debug("Bootstraping node3")
-        node3 = new_node(cluster)
+        logger.info("Bootstraping node3")
+        node3 = self.cluster.new_node(3)
         node3.start(wait_for_binary_proto=True)
 
         n3session = self.patient_cql_connection(node3)
         n3session.execute("USE ks")
-        debug("Checking that no data was lost")
+        logger.info("Checking that no data was lost")
         for n in range(10, 20):
             query_c1c2(n3session, n, ConsistencyLevel.ALL)
 
