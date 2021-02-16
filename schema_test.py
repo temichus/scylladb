c3bd2fccc361c8217445e2a4570997f828762021
Shoshan Dagany <shoshan.dagany@scylladb.com>
2021-02-16 16:00:23 +0200

    schema_test.py: migrate to pytest

--- schema_test.py
+++ schema_test.py
@@ -1,14 +1,14 @@
 import time
 
-from dtest import Tester
-from nose.plugins.attrib import attr
+import pytest
+from dtest_class import Tester, create_ks
 from tools import since, rows_to_list
-from assertions import assert_invalid
+from tools.assertions import assert_invalid, assert_all, assert_one
 from cassandra.concurrent import execute_concurrent
 
 
-@since('2.0')
-@attr('dtest-full', 'single_node')
+@pytest.mark.dtest_full
+@pytest.mark.single_node
 class TestSchema(Tester):
 
     def drop_column_compact_test(self):
@@ -19,9 +19,9 @@ class TestSchema(Tester):
 
         assert_invalid(session, "ALTER TABLE cf DROP c1", "Cannot drop columns from a")
 
-    @attr('next-gating')
-    @attr('dtest-debug')
-    def drop_column_compaction_test(self):
+    @pytest.mark.next_gating
+    @pytest.mark.dtest_debug
+    def test_drop_column_compaction(self):
         session = self.prepare()
         session.execute("USE ks")
         session.execute("CREATE TABLE cf (key int PRIMARY KEY, c1 int, c2 int)")
@@ -44,12 +44,10 @@ class TestSchema(Tester):
 
         # test that c1 values have been compacted away.
         session = self.patient_cql_connection(node)
-        rows = session.execute("SELECT c1 FROM ks.cf")
-        self.assertEqual([[None], [None], [None], [4]], sorted(
-            rows_to_list(rows), key=lambda x: (x and x[0] is not None, x)))
+        assert_all(session, "SELECT c1 FROM ks.cf", [[None], [None], [None], [4]], ignore_order=True)
 
-    @attr('next-gating')
-    @attr('dtest-debug')
+    @pytest.mark.next_gating
+    @pytest.mark.dtest_debug
     def drop_column_queries_test(self):
         session = self.prepare()
 
@@ -71,19 +69,12 @@ class TestSchema(Tester):
         session.execute("INSERT INTO cf (key, c1, c2) VALUES (3, 4, 5)")
 
         # test that old (pre-drop) c1 values aren't returned and new ones are.
-        rows = session.execute("SELECT c1 FROM cf")
-        self.assertEqual([[None], [None], [None], [4]], sorted(
-            rows_to_list(rows), key=lambda x: (x and x[0] is not None, x)))
+        assert_all(session, "SELECT c1 FROM cf", [[None], [None], [None], [4]], ignore_order=True)
+        assert_all(session, "SELECT * FROM cf", [[0, None, 2], [1, None, 3], [2, None, 4], [3, 4, 5]],
+                   ignore_order=True)
+        assert_one(session, "SELECT c1 FROM cf WHERE key = 0", [None])
 
-        rows = session.execute("SELECT * FROM cf")
-        self.assertEqual([[0, None, 2], [1, None, 3], [2, None, 4], [3, 4, 5]], sorted(
-            rows_to_list(rows), key=lambda x: (x and x[0] is not None, x)))
-
-        rows = session.execute("SELECT c1 FROM cf WHERE key = 0")
-        self.assertEqual([[None]], rows_to_list(rows))
-
-        rows = session.execute("SELECT c1 FROM cf WHERE key = 3")
-        self.assertEqual([[4]], rows_to_list(rows))
+        assert_one(session, "SELECT c1 FROM cf WHERE key = 3", [4])
 
         # FIXME: ScyllaDB: Indexes not supported yet
         # rows = session.execute("SELECT * FROM cf WHERE c2 = 2")
@@ -101,11 +92,11 @@ class TestSchema(Tester):
         session = self.patient_cql_connection(nodes[0])
         # It is forbidden to re-add a column with client-side timestamps
         session.use_client_timestamp = False
-        self.create_ks(session, 'ks', 1)
+        create_ks(session, 'ks', 1)
         return session
 
     # Reproducer for https://github.com/scylladb/scylla/issues/2623
-    def restart_with_large_tables_test(self):
+    def test_restart_with_large_tables(self):
         cluster = self.cluster
         cluster.set_configuration_options(values={'max_cached_partition_size_in_kb': 1})
         session = self.prepare()
