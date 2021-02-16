cf93e1b9c40c94d17bf23f75ecc25b3dec54c48a
Shlomi Balalis <Balalis@scylladb.com>
2021-02-16 03:29:49 +0200

    feature(manager): Added functions to suspend and resume the manager

--- dtest_scylla_manager.py
+++ dtest_scylla_manager.py
@@ -1451,6 +1451,16 @@ class ManagerCluster(ScyllaManagerBase):
             return value_list[0]
         return default_value
 
+    def suspend(self):
+        cmd = f"suspend -c {self.id}"
+        self.sctool.run(cmd=cmd)
+
+    def resume(self, start_tasks=True):
+        cmd = f"resume -c {self.id}"
+        if start_tasks:
+            cmd += " --start-tasks"
+        self.sctool.run(cmd=cmd)
+
 
 class ScyllaManagerMixin:
     def config_and_create_cluster(self, nodes, extra_config_options=None):
