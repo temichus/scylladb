f7ca1afdcf33371b5368ff6eed1f79eeeaabff84
Shoshan Dagany <shoshan.dagany@scylladb.com>
2021-02-17 13:42:32 +0200

    fix(tools/assertions.py): fix assert_all to support None

--- tools/assertions.py
+++ tools/assertions.py
@@ -183,11 +183,13 @@ def assert_all(session, query, expected, cl=ConsistencyLevel.ONE, ignore_order=F
     assert_all(session, "LIST USERS", [['aleksey', False], ['cassandra', True]])
     assert_all(self.session1, "SELECT * FROM ttl_table;", [[1, 42, 1, 1]])
     """
-    from tools.data import get_list_res  # to avoid cyclic dependency
-
-    list_res = get_list_res(session, query, cl, ignore_order, result_as_string, timeout=timeout)
+    from tools.misc import list_to_hashed_dict  # to avoid cyclic dependency
+    simple_query = SimpleStatement(query, consistency_level=cl)
+    res = session.execute(simple_query) if timeout is None else session.execute(simple_query, timeout=timeout)
+    list_res = _rows_to_list(res)
     if ignore_order:
-        expected = sorted(expected)
+        expected = list_to_hashed_dict(expected)
+        list_res = list_to_hashed_dict(list_res)
     error = f"Expected {expected} from {query}, but got {list_res}" if print_result_on_failure \
         else f'Actual result ({len(list_res)} rows) is not as expected ({len(expected)} rows). Query: {query}'
     assert list_res == expected, error
