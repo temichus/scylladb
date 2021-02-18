a16117c262169a7e386239147b8daccabed09e66
Fabio Gelcer <fabio@scylladb.com>
2021-02-18 22:36:13 +0200

    test(udtencoding_test.py): added new UDT test
    in this test, we will verify that, when making
    a change to a UDT that is being used as a
    partition key, it fails (but when not used as
    it, it works), see scylladb/scylla@66e8214 for
    further details.

--- udtencoding_test.py
+++ udtencoding_test.py
@@ -9,35 +9,61 @@ from tools.assertions import assert_invalid
 @pytest.mark.dtest_full
 class TestUDTEncoding(Tester):
 
-    def test_udt(self):
-        """ Test (somewhat indirectly) that user queries involving UDT's are properly encoded (due to driver not recognizing UDT syntax) """
-        cluster = self.cluster
-
-        cluster.populate(3).start()
-        node1, node2, node3 = cluster.nodelist()
+    @pytest.fixture(scope="function")
+    def node1_session(self):
+        self.cluster.populate(3).start()
 
         time.sleep(.5)
-        session = self.patient_cql_connection(node1)
-        create_ks(session, 'ks', 3)
+        with self.patient_cql_connection(self.cluster.nodelist()[0]) as session:
+            yield session
+
+    def test_udt(self, node1_session):  # pylint: disable=no-self-use
+        """
+        Test (somewhat indirectly) that user queries involving UDT's are properly encoded
+        (due to driver not recognizing UDT syntax)
+        """
+        create_ks(node1_session, 'ks', 3)
 
         # create udt and insert correctly (should be successful)
-        session.execute('CREATE TYPE address (city text,zip int);')
-        session.execute('CREATE TABLE user_profiles (login text PRIMARY KEY, addresses map<text, frozen<address>>);')
-        session.execute(
-            "INSERT INTO user_profiles(login, addresses) VALUES ('tsmith', { 'home': {city: 'San Fransisco',zip: 94110 }});")
+        node1_session.execute('CREATE TYPE address (city text,zip int);')
+        node1_session.execute('CREATE TABLE user_profiles (login text PRIMARY KEY, addresses'
+                              ' map<text, frozen<address>>);')
+        node1_session.execute("INSERT INTO user_profiles(login, addresses) VALUES"
+                              " ('tsmith', { 'home': {city: 'San Fransisco',zip: 94110 }});")
 
-        # note here address looks likes a map -> which is what the driver thinks it is. udt is encoded server side, we test that if addresses is changed slightly whether encoder recognizes the errors
+        # note here address looks likes a map -> which is what the driver thinks it is. udt is encoded server side,
+        # we test that if addresses is changed slightly whether encoder recognizes the errors
 
         # try adding a field - see if will be encoded to a udt (should return error)
-        assert_invalid(session,
-                       "INSERT INTO user_profiles(login, addresses) VALUES ('jsmith', { 'home': {street: 'El Camino Real', city: 'San Fransisco', zip: 94110 }});",
+        assert_invalid(node1_session,
+                       "INSERT INTO user_profiles(login, addresses) VALUES"
+                       " ('jsmith', { 'home': {street: 'El Camino Real', city: 'San Fransisco', zip: 94110 }});",
                        "Unknown field 'street' in value of user defined type address")
 
         # try modifying a field name - see if will be encoded to a udt (should return error)
-        assert_invalid(session,
-                       "INSERT INTO user_profiles(login, addresses) VALUES ('fsmith', { 'home': {cityname: 'San Fransisco', zip: 94110 }});",
+        assert_invalid(node1_session,
+                       "INSERT INTO user_profiles(login, addresses) VALUES "
+                       "('fsmith', { 'home': {cityname: 'San Fransisco', zip: 94110 }});",
                        "Unknown field 'cityname' in value of user defined type address")
 
         # try modifying a type within the collection - see if will be encoded to a udt (should return error)
-        assert_invalid(session, "INSERT INTO user_profiles(login, addresses) VALUES ('fsmith', { 'home': {city: 'San Fransisco', zip: '94110' }});",
+        assert_invalid(node1_session, "INSERT INTO user_profiles(login, addresses) VALUES"
+                                      " ('fsmith', { 'home': {city: 'San Fransisco', zip: '94110' }});",
                        "Invalid map literal for addresses")
+
+    def test_udt_change_in_partition_key(self, node1_session):  # pylint: disable=no-self-use
+        """
+        This test is meant to verify scylladb/scylla@66e8214
+        Scylla will now detect modifications to user-defined data types (UDTs) that are used as partition keys and
+        forbid them.
+        """
+        create_ks(node1_session, 'ks', 3)
+        node1_session.execute("CREATE TYPE full_name (first_name text, last_name text);")
+        node1_session.execute('CREATE TYPE address (city text, zip int);')
+        node1_session.execute("CREATE TABLE user_profile (name frozen<full_name>, username text, "
+                              "residential_address address, PRIMARY KEY (name, username));")
+        node1_session.execute("INSERT INTO user_profile (name, username, residential_address) "
+                              "VALUES ({first_name: 'John', last_name: 'Smith'}, 'jsmith', "
+                              "{city: 'San Francisco', zip: 93210});")
+        assert_invalid(node1_session, "ALTER TYPE full_name RENAME last_name TO surname;")
+        node1_session.execute("ALTER TYPE address ADD country text")
