5dca94c1a63659fb5907c71f23c4f3857708a406
Benny Halevy <bhalevy@scylladb.com>
2021-03-16 23:01:15 +0200

    native_transport_ssl_test: connect_to_ssl_test: use watch_log_for to wait for the expected error
    As seen in https://jenkins.scylladb.com/view/master/job/scylla-master/job/next/3299/testReport/native_transport_ssl_test/NativeTransportSSL/dtest___connect_to_ssl_test/
    The expected error message may appear right after the grep_log check.
    
    dtest.log:
    ```
    2021-03-16 19:30:48,604 59084   cassandra.cluster              ERROR    | Control connection failed to connect, shutting down Cluster:
    Traceback (most recent call last):
      File "cassandra/cluster.py", line 1683, in cassandra.cluster.Cluster.connect
      File "cassandra/cluster.py", line 3470, in cassandra.cluster.ControlConnection.connect
      File "cassandra/cluster.py", line 3515, in cassandra.cluster.ControlConnection._reconnect_internal
    cassandra.cluster.NoHostAvailable: ('Unable to connect to any servers', {'127.0.7.1:9042': OperationTimedOut('errors=Timed out creating connection (90 seconds), last_host=None')})
    2021-03-16 19:30:48,608 59084   dtest                          DEBUG    | native_transport_ssl_test.py:NativeTransportSSL.connect_to_ssl_test - Test failed with errors: [(<native_transport_ssl_test.NativeTransportSSL testMethod=connect_to_ssl_test>, (<class 'AssertionError'>, AssertionError('Missing SSL handshake exception while connecting with non-SSL enabled client'), <traceback object at 0x7fbe738e2190>))]
    ```
    
    while node1.log has:
    ```
    INFO  2021-03-16 19:30:48,612 [shard 0] cql_server - exception while processing connection: seastar::nested_exception: std::system_error (error system:22, Invalid argument) (while cleaning up after std::system_error (error GnuTLS:-10, The specified session has been invalidated for some reason.))
    ```
    
    which happens a few milliseconds after the test assert failed.
    
    Test: native_transport_ssl_test:NativeTransportSSL.{connect_to_ssl_test,connect_to_ssl_test_client_auth}(release)
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- native_transport_ssl_test.py
+++ native_transport_ssl_test.py
@@ -64,7 +64,9 @@ class TestNativeTransportSSL(Tester):
             logger.info('Should not be able to connect to SSL socket without SSL enabled client')
             self._create_cluster_session(node1, use_ssl=False)
 
-        assert len(node1.grep_log("(^io.netty.handler.ssl.NotSslRecordException.*|^.*An unexpected TLS packet was received.*|^.*The specified session has been invalidated for some reason.*)")) > 0, \
+        assert len(node1.watch_log_for(
+            "(^io.netty.handler.ssl.NotSslRecordException.*|^.*An unexpected TLS packet was received.*|^.*The specified session has been invalidated for some reason.*)",
+            timeout=10)) > 0, \
             "Missing SSL handshake exception while connecting with non-SSL enabled client"
 
         # enabled ssl on the client and try again (this should work)
@@ -86,7 +88,9 @@ class TestNativeTransportSSL(Tester):
             logger.info('Should not be able to connect to SSL socket without SSL enabled client')
             self._create_cluster_session(node1, use_ssl=False)
 
-        assert len(node1.grep_log("(^io.netty.handler.ssl.NotSslRecordException.*|^.*An unexpected TLS packet was received.*|^.*The specified session has been invalidated for some reason.*)")) > 0, \
+        assert len(node1.watch_log_for(
+            "(^io.netty.handler.ssl.NotSslRecordException.*|^.*An unexpected TLS packet was received.*|^.*The specified session has been invalidated for some reason.*)",
+            timeout=10)) > 0, \
             "Missing SSL handshake exception while connecting with non-SSL enabled client"
 
         with pytest.raises(NoHostAvailable):
