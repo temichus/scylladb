9bd47419fdb3db3294e7ce256615568154d98dac
Aleksandr.Bykov <aleksandr.bykov@vdomresearch.com>
2021-03-23 12:45:34 +0700

    test(scrub_test): Convert to scrub_test to pytest
    Convert scrub_test to pytest

--- scrub_test.py
+++ scrub_test.py
@@ -3,12 +3,16 @@ import os
 import re
 import subprocess
 import uuid
-
-from ccmlib import common
-from dtest import Tester, debug
-from tools import since
+import pytest
+import logging
 import time
 
+
+from ccmlib import common
+from dtest_class import Tester, create_cf, create_ks
+
+
+logger = logging.getLogger(__name__)
 KEYSPACE = 'ks'
 
 
@@ -27,24 +31,31 @@ class TestHelper(Tester):
                 break
         return path
 
-    def get_index_path(self, table, index):
+    def get_index_path(self, index):
         """
         Return the path where the index sstables are located
         """
-        path = self.get_table_path(table)
-        return os.path.join(path, '.' + index)
+        node1 = self.cluster.nodelist()[0]
+        basepath = os.path.join(node1.get_path(), 'data', KEYSPACE)
+        index_path = ""
+        for x in os.listdir(basepath):
+            if x.startswith(index):
+                index_path = os.path.join(basepath, x)
+                break
+        return index_path
 
     def get_sstable_files(self, path):
         """
         Return the sstable files at a specific location
         """
         ret = []
-        debug('Checking sstables in %s' % (path))
+        logger.debug(f'Checking sstables in {path}')
 
         for ext in ('*.db', '*.txt', '*.adler32', '*.sha1'):
             for fname in glob.glob(os.path.join(path, ext)):
                 bname = os.path.basename(fname)
-                debug('Found sstable file %s' % (bname))
+                if "-scylla." in bname.lower():
+                    continue
                 ret.append(bname)
         return ret
 
@@ -54,9 +65,9 @@ class TestHelper(Tester):
         -Statistics.db file (only available in >= 3.0)
         """
         for fname in self.get_sstable_files(self.get_table_path(table)):
-            if not fname.endswith("-Data.db") and not fname.endswith("-Statistics.db"):
+            if not fname.lower().endswith(("-data.db", "-statistics.db")):
                 fullname = os.path.join(self.get_table_path(table), fname)
-                debug('Deleting {}'.format(fullname))
+                logger.debug(f'Deleting {fullname}')
                 os.remove(fullname)
 
     def get_sstables(self, table, indexes):
@@ -65,12 +76,12 @@ class TestHelper(Tester):
         """
         sstables = {}
         table_sstables = self.get_sstable_files(self.get_table_path(table))
-        assert len(table_sstables) > 0
+        assert len(table_sstables) > 0, f"sstables were not found in {self.get_table_path(table)}"
         sstables[table] = sorted(table_sstables)
 
         for index in indexes:
-            index_sstables = self.get_sstable_files(self.get_index_path(table, index))
-            assert len(index_sstables) > 0
+            index_sstables = self.get_sstable_files(self.get_index_path(index))
+            assert len(index_sstables) > 0, f"No indexes were found by path: {self.get_index_path(index)}"
             sstables[index] = sorted('%s/%s' % (index, sstable) for sstable in index_sstables)
 
         return sstables
@@ -91,24 +102,24 @@ class TestHelper(Tester):
         node1 = self.cluster.nodelist()[0]
         env = common.make_cassandra_env(node1.get_install_cassandra_root(), node1.get_node_cassandra_root())
         scrub_bin = node1.get_tool('sstablescrub')
-        debug(scrub_bin)
+        logger.debug(scrub_bin)
 
         args = [scrub_bin, ks, cf]
         p = subprocess.Popen(args, env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
-        debug(out)
+        logger.debug(out)
         # if we have less than 64G free space, we get this warning - ignore it
         if err and "Consider adding more capacity" not in err:
-            debug(err)
+            logger.debug(err)
             assert False, 'sstablescrub failed'
 
     def perform_node_tool_cmd(self, cmd, table, indexes):
         """
         Perform a nodetool command on a table and the indexes specified
         """
-        self.launch_nodetool_cmd('%s %s %s' % (cmd, KEYSPACE, table))
+        self.launch_nodetool_cmd(f'{cmd} {KEYSPACE} {table}')
         for index in indexes:
-            self.launch_nodetool_cmd('%s %s %s.%s' % (cmd, KEYSPACE, table, index))
+            self.launch_nodetool_cmd(f'{cmd} {KEYSPACE} {index}_index')
 
     def flush(self, table, *indexes):
         """
@@ -133,7 +144,7 @@ class TestHelper(Tester):
         """
         self.launch_standalone_scrub(KEYSPACE, table)
         for index in indexes:
-            self.launch_standalone_scrub(KEYSPACE, '%s.%s' % (table, index))
+            self.launch_standalone_scrub(KEYSPACE, f'{index}_index')
         return self.get_sstables(table, indexes)
 
     def increment_generation_by(self, sstable, generation_increment):
@@ -151,10 +162,9 @@ class TestHelper(Tester):
             increment_by = len(set(re.match('.*(\d)[^0-9].*', s).group(1) for s in table_sstables))
             sstables[table_or_index] = [self.increment_generation_by(s, increment_by) for s in table_sstables]
 
-        debug('sstables after increment %s' % (str(sstables)))
+        logger.debug(f'sstables after increment {str(sstables)}')
 
 
-@since('2.2')
 class TestScrubIndexes(TestHelper):
     """
     Test that we scrub indexes as well as their parent tables
@@ -163,8 +173,7 @@ class TestScrubIndexes(TestHelper):
     def create_users(self, session):
         columns = {"password": "varchar", "gender": "varchar",
                    "session_token": "varchar", "state": "varchar", "birth_year": "bigint"}
-        self.create_cf(session, 'users', columns=columns)
-
+        create_cf(session, 'users', columns=columns)
         session.execute("CREATE INDEX gender_idx ON users (gender)")
         session.execute("CREATE INDEX state_idx ON users (state)")
         session.execute("CREATE INDEX birth_year_idx ON users (birth_year)")
@@ -196,7 +205,7 @@ class TestScrubIndexes(TestHelper):
         ret.extend(list(session.execute("SELECT * FROM users WHERE state='TX'")))
         ret.extend(list(session.execute("SELECT * FROM users WHERE gender='f'")))
         ret.extend(list(session.execute("SELECT * FROM users WHERE birth_year=1978")))
-        assert len(ret) == 8
+        assert len(ret) == 8, "Invalid number of records in table 'users'"
         return ret
 
     def test_scrub_static_table(self):
@@ -205,7 +214,7 @@ class TestScrubIndexes(TestHelper):
         node1 = cluster.nodelist()[0]
 
         session = self.patient_cql_connection(node1)
-        self.create_ks(session, KEYSPACE, 1)
+        create_ks(session, KEYSPACE, 1)
 
         self.create_users(session)
         self.update_users(session)
@@ -215,18 +224,18 @@ class TestScrubIndexes(TestHelper):
         scrubbed_sstables = self.scrub('users', 'gender_idx', 'state_idx', 'birth_year_idx')
 
         self.increase_sstable_generations(initial_sstables)
-        self.assertEqual(initial_sstables, scrubbed_sstables)
+        assert initial_sstables == scrubbed_sstables, "List of sstables before and after scrub differs"
 
         users = self.query_users(session)
-        self.assertEqual(initial_users, users)
+        assert initial_users == users, "List of users before and after scrub are different"
 
         # Scrub and check sstables and data again
         scrubbed_sstables = self.scrub('users', 'gender_idx', 'state_idx', 'birth_year_idx')
         self.increase_sstable_generations(initial_sstables)
-        self.assertEqual(initial_sstables, scrubbed_sstables)
+        assert initial_sstables == scrubbed_sstables, "List of sstables before and after scrub differs"
 
         users = self.query_users(session)
-        self.assertEqual(initial_users, users)
+        assert initial_users == users, "List of users before and after scrub are different"
 
         # Restart and check data again
         cluster.stop()
@@ -236,15 +245,20 @@ class TestScrubIndexes(TestHelper):
         session.execute('USE %s' % (KEYSPACE))
 
         users = self.query_users(session)
-        self.assertEqual(initial_users, users)
+        assert initial_users == users, "List of users before and after scrub are different"
 
+    @pytest.mark.skip("sstablesscrub utility doesn't use scylla code")
     def test_standalone_scrub(self):
+        # test marked as skipped, because use
+        # cassandra utility sstablesscrub
+        # which doesn't use scylla code and change
+        # sstable format from md -> mc
         cluster = self.cluster
         cluster.populate(1).start()
         node1 = cluster.nodelist()[0]
 
         session = self.patient_cql_connection(node1)
-        self.create_ks(session, KEYSPACE, 1)
+        create_ks(session, KEYSPACE, 1)
 
         self.create_users(session)
         self.update_users(session)
@@ -256,25 +270,24 @@ class TestScrubIndexes(TestHelper):
 
         scrubbed_sstables = self.standalonescrub('users', 'gender_idx', 'state_idx', 'birth_year_idx')
         self.increase_sstable_generations(initial_sstables)
-        self.assertEqual(initial_sstables, scrubbed_sstables)
+        assert initial_sstables == scrubbed_sstables, "List of sstables before and after scrub differs"
 
         cluster.start()
         session = self.patient_cql_connection(node1)
         session.execute('USE %s' % (KEYSPACE))
 
         users = self.query_users(session)
-        self.assertEqual(initial_users, users)
+        assert initial_users == users, "List of users before and after scrub are different"
 
-    def test_scrub_collections_table(self):
+    def test_scrub_collections_table_without_indexes(self):
         cluster = self.cluster
         cluster.populate(1).start()
         node1 = cluster.nodelist()[0]
 
         session = self.patient_cql_connection(node1)
-        self.create_ks(session, KEYSPACE, 1)
+        create_ks(session, KEYSPACE, 1)
 
         session.execute("CREATE TABLE users (user_id uuid PRIMARY KEY, email text, uuids list<uuid>)")
-        session.execute("CREATE INDEX user_uuids_idx on users (uuids)")
 
         _id = uuid.uuid4()
         num_users = 100
@@ -286,26 +299,27 @@ class TestScrubIndexes(TestHelper):
                 id=_id, user_id=user_uuid))
 
         initial_users = list(session.execute(
-            ("SELECT * from users where uuids contains {some_uuid}").format(some_uuid=_id)))
-        self.assertEqual(num_users, len(initial_users))
+            ("SELECT * from users where uuids contains {some_uuid} ALLOW FILTERING").format(some_uuid=_id)))
+        assert num_users == len(initial_users), "Not all users were added to table"
 
-        initial_sstables = self.flush('users', 'user_uuids_idx')
-        scrubbed_sstables = self.scrub('users', 'user_uuids_idx')
-
-        self.increase_sstable_generations(initial_sstables)
-        self.assertEqual(initial_sstables, scrubbed_sstables)
-
-        users = list(session.execute(("SELECT * from users where uuids contains {some_uuid}").format(some_uuid=_id)))
-        self.assertEqual(initial_users, users)
-
-        scrubbed_sstables = self.scrub('users', 'user_uuids_idx')
+        initial_sstables = self.flush('users')
+        scrubbed_sstables = self.scrub('users')
 
         self.increase_sstable_generations(initial_sstables)
-        self.assertEqual(initial_sstables, scrubbed_sstables)
+        assert initial_sstables == scrubbed_sstables, "List of sstables before and after scrub differs"
 
-        users = list(session.execute(("SELECT * from users where uuids contains {some_uuid}").format(some_uuid=_id)))
+        users = list(session.execute(
+            ("SELECT * from users where uuids contains {some_uuid} ALLOW FILTERING").format(some_uuid=_id)))
+        assert initial_users == users, "List of users before and after scrub are different"
 
-        self.assertCountEqual(initial_users, users)
+        scrubbed_sstables = self.scrub('users')
+
+        self.increase_sstable_generations(initial_sstables)
+        assert initial_sstables == scrubbed_sstables, "List of sstables before and after scrub differs"
+
+        users = list(session.execute(
+            ("SELECT * from users where uuids contains {some_uuid} ALLOW FILTERING").format(some_uuid=_id)))
+        assert initial_users == users, "List of users before and after scrub are different"
 
 
 class TestScrub(TestHelper):
@@ -316,7 +330,7 @@ class TestScrub(TestHelper):
     def create_users(self, session):
         columns = {"password": "varchar", "gender": "varchar",
                    "session_token": "varchar", "state": "varchar", "birth_year": "bigint"}
-        self.create_cf(session, 'users', columns=columns)
+        create_cf(session, 'users', columns=columns)
 
     def update_users(self, session):
         session.execute(
@@ -342,7 +356,7 @@ class TestScrub(TestHelper):
 
     def query_users(self, session):
         ret = list(session.execute("SELECT * FROM users"))
-        assert len(ret) == 5
+        assert len(ret) == 5, "Amount of users is different"
         return ret
 
     def test_nodetool_scrub(self):
@@ -351,7 +365,7 @@ class TestScrub(TestHelper):
         node1 = cluster.nodelist()[0]
 
         session = self.patient_cql_connection(node1)
-        self.create_ks(session, KEYSPACE, 1)
+        create_ks(session, KEYSPACE, 1)
 
         self.create_users(session)
         self.update_users(session)
@@ -361,18 +375,18 @@ class TestScrub(TestHelper):
         scrubbed_sstables = self.scrub('users')
 
         self.increase_sstable_generations(initial_sstables)
-        self.assertEqual(initial_sstables, scrubbed_sstables)
+        assert initial_sstables == scrubbed_sstables, "List of sstables before and after scrub differs"
 
         users = self.query_users(session)
-        self.assertEqual(initial_users, users)
+        assert initial_users == users, "List of users before and after scrub are different"
 
         # Scrub and check sstables and data again
         scrubbed_sstables = self.scrub('users')
         self.increase_sstable_generations(initial_sstables)
-        self.assertEqual(initial_sstables, scrubbed_sstables)
+        assert initial_sstables == scrubbed_sstables, "List of sstables before and after scrub differs"
 
         users = self.query_users(session)
-        self.assertEqual(initial_users, users)
+        assert initial_users == users, "List of users before and after scrub are different"
 
         # Restart and check data again
         cluster.stop()
@@ -382,15 +396,20 @@ class TestScrub(TestHelper):
         session.execute('USE %s' % (KEYSPACE))
 
         users = self.query_users(session)
-        self.assertEqual(initial_users, users)
+        assert initial_users == users, "List of users before and after scrub are different"
 
+    @pytest.mark.skip("sstablesscrub utility doesn't use scylla code")
     def test_standalone_scrub(self):
+        # test marked as skipped, because use
+        # cassandra utility sstablesscrub
+        # which doesn't use scylla code and change
+        # sstable format from md -> mc
         cluster = self.cluster
         cluster.populate(1).start()
         node1 = cluster.nodelist()[0]
 
         session = self.patient_cql_connection(node1)
-        self.create_ks(session, KEYSPACE, 1)
+        create_ks(session, KEYSPACE, 1)
 
         self.create_users(session)
         self.update_users(session)
@@ -402,22 +421,27 @@ class TestScrub(TestHelper):
 
         scrubbed_sstables = self.standalonescrub('users')
         self.increase_sstable_generations(initial_sstables)
-        self.assertEqual(initial_sstables, scrubbed_sstables)
+        assert initial_sstables == scrubbed_sstables, "List of sstables before and after scrub differs"
 
         cluster.start()
         session = self.patient_cql_connection(node1)
         session.execute('USE %s' % (KEYSPACE))
 
         users = self.query_users(session)
-        self.assertEqual(initial_users, users)
+        assert initial_users == users, "List of users before and after scrub are different"
 
+    @pytest.mark.skip("sstablesscrub utility doesn't use scylla code")
     def test_standalone_scrub_essential_files_only(self):
+        # test marked as skipped, because use
+        # cassandra utility sstablesscrub
+        # which doesn't use scylla code and change
+        # sstable format from md -> mc
         cluster = self.cluster
         cluster.populate(1).start()
         node1 = cluster.nodelist()[0]
 
         session = self.patient_cql_connection(node1)
-        self.create_ks(session, KEYSPACE, 1)
+        create_ks(session, KEYSPACE, 1)
 
         self.create_users(session)
         self.update_users(session)
@@ -431,14 +455,14 @@ class TestScrub(TestHelper):
 
         scrubbed_sstables = self.standalonescrub('users')
         self.increase_sstable_generations(initial_sstables)
-        self.assertEqual(initial_sstables, scrubbed_sstables)
+        assert initial_sstables == scrubbed_sstables, "List of sstables before and after scrub differs"
 
         cluster.start()
         session = self.patient_cql_connection(node1)
         session.execute('USE %s' % (KEYSPACE))
 
         users = self.query_users(session)
-        self.assertEqual(initial_users, users)
+        assert initial_users == users, "List of users before and after scrub are different"
 
     def test_scrub_with_UDT(self):
         """
@@ -457,4 +481,4 @@ class TestScrub(TestHelper):
         node1.nodetool("scrub")
         time.sleep(2)
         match = node1.grep_log("org.apache.cassandra.serializers.MarshalException: Not enough bytes to read a set")
-        self.assertEqual(len(match), 0)
+        assert len(match) == 0, f"{len(match)} is not equal 0"
