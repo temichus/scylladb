dc643aed6b420eea3a128ec2a80477ca0d41c8cb
Oren Efraimov <Orenef11@gmail.com>
2021-03-25 09:22:56 +0200

    feature(updateable_config_test.py): Converting to Pytest format (#2028)
    * Trello URL: https://trello.com/c/6nO5dlnw

--- updateable_config_test.py
+++ updateable_config_test.py
@@ -10,19 +10,21 @@ Reference:
 - https://github.com/scylladb/scylla/commit/eb496b5eaee29748f810dde00ef1d7c673b73a43
   Merge "Allow changing configuration at runtime" from Avi
 """
-
+import logging
 import os
 import signal
+
+import pytest
 import requests
 
-from nose.plugins.attrib import attr
+from dtest_class import Tester, get_ip_from_node, create_ks, create_cf
+from tools.data import insert_c1c2
 
-from tools import require, insert_c1c2
-
-from dtest import Tester, debug
+logger = logging.getLogger(__name__)
 
 
-@attr('dtest-full', 'single_node')
+@pytest.mark.dtest_full
+@pytest.mark.single_node
 class TestUpdateableConfig(Tester):
     """
     Scylla supported to change some of configuration in runtime, this
@@ -42,32 +44,29 @@ class TestUpdateableConfig(Tester):
         API.
         """
         mark = node.mark_log()
-        debug('Change configuration `%s`' % param)
-        response = requests.get('http://%s:10000/v2/config/%s' % (self.get_ip_from_node(node),
-                                                                  param))
-        debug('Original value before change: %s' % response.text)
+        logger.info('Change configuration `%s`' % param)
+        response = requests.get('http://%s:10000/v2/config/%s' % (get_ip_from_node(node), param))
+        logger.info('Original value before change: %s' % response.text)
         node.set_configuration_options({param: value})
         self.trigger_reload_config(node)
         node.watch_log_for('completed re-reading configuration file', from_mark=mark)
 
-        debug('Using the API to validate the configuration change, expected: %s' % verify_response)
-        response = requests.get('http://%s:10000/v2/config/%s' % (self.get_ip_from_node(node),
-                                                                  param))
-        self.assertEquals(response.text, verify_response, 'response: %s, expected: %s' %
-                          (response.text, verify_response))
+        logger.info('Using the API to validate the configuration change, expected: %s' % verify_response)
+        response = requests.get('http://%s:10000/v2/config/%s' % (get_ip_from_node(node), param))
+        assert response.text == verify_response, f'response: {response.text}, expected: {verify_response}'
 
-    @attr('next-gating')
-    @attr('dtest-debug')
+    @pytest.mark.dtest_debug
+    @pytest.mark.next_gating
     def test_compaction_enforce_min_threshold(self):
         self.cluster.populate(1).start(wait_other_notice=True, wait_for_binary_proto=True)
         node1 = self.cluster.nodelist()[0]
 
         node1.stress(['write', 'n=10000', '-rate', 'threads=8'])
-        session = self.patient_cql_connection(node1)
-        session.execute("""
-            ALTER TABLE keyspace1.standard1 WITH compaction = {
-                'class' : 'SizeTieredCompactionStrategy', 'min_threshold' : 7 }
-        """)
+        with self.patient_cql_connection(node1) as session:
+            session.execute("""
+                ALTER TABLE keyspace1.standard1 WITH compaction = {
+                    'class' : 'SizeTieredCompactionStrategy', 'min_threshold' : 7 }
+            """)
 
         self.change_and_verify_config(node1, 'compaction_enforce_min_threshold', True, 'true')
         node1.stress(['mixed', 'n=10000', '-rate', 'threads=8'])
@@ -83,8 +82,8 @@ class TestUpdateableConfig(Tester):
         min_threshold = 5
         insert_keys_num = 1
 
-        self.create_ks(session, 'ks', 1)
-        self.create_cf(session, 'cf', columns={'c1': 'text', 'c2': 'text'})
+        create_ks(session=session, name='ks', rf=1)
+        create_cf(session=session, name='cf', columns={'c1': 'text', 'c2': 'text'})
         insert_c1c2(session, keys=range(100))
         session.execute("""
             ALTER TABLE ks.cf WITH compaction = {
@@ -95,22 +94,22 @@ class TestUpdateableConfig(Tester):
         mark = node1.mark_log()
         compact_log = "compaction -.*Compacting \[%s" % os.path.join(node1.get_path(), "data/ks/cf")
 
-        for i in range(min_threshold - 1):
+        for _ in range(min_threshold - 1):
             insert_c1c2(session, n=insert_keys_num)
             node1.flush()
         try:
             node1.watch_log_for(compact_log, from_mark=mark, timeout=10)
         except Exception as ex:
-            debug(ex)
+            logger.info(ex)
             assert "Missing: ['compaction -.*Compacting" in str(ex)
 
         insert_c1c2(session, n=insert_keys_num)
         node1.flush()
-        debug('Reach to min threshold, expect compact to be triggered')
+        logger.info('Reach to min threshold, expect compact to be triggered')
         node1.watch_log_for(compact_log, from_mark=mark, timeout=10)
 
         mark = node1.mark_log()
-        debug('Execute compact to clean the threshold counting')
+        logger.info('Execute compact to clean the threshold counting')
         node1.compact()
         node1.watch_log_for(compact_log, from_mark=mark, timeout=10)
 
@@ -118,10 +117,10 @@ class TestUpdateableConfig(Tester):
         mark = node1.mark_log()
         insert_c1c2(session, n=insert_keys_num)
         node1.flush()
-        debug('compaction_enforce_min_threshold is disabled, expect compact to be triggered by one insert')
+        logger.info('compaction_enforce_min_threshold is disabled, expect compact to be triggered by one insert')
         node1.watch_log_for(compact_log, from_mark=mark, timeout=10)
 
-    @require('#5382')
+    @pytest.mark.require('#5382')
     def test_auto_adjust_flush_quota(self):
         """
         auto_adjust_flush_quota isn't a supported updateable parameter.
@@ -129,11 +128,11 @@ class TestUpdateableConfig(Tester):
         self.cluster.populate(1).start(wait_other_notice=True, wait_for_binary_proto=True)
         node1 = self.cluster.nodelist()[0]
 
-        debug('default auto_adjust_flush_quota is `false`, try to set it to false first')
+        logger.info('default auto_adjust_flush_quota is `false`, try to set it to false first')
         self.change_and_verify_config(node1, 'auto_adjust_flush_quota', True, 'false')
         self.change_and_verify_config(node1, 'auto_adjust_flush_quota', False, 'false')
 
-    @require('#5382')
+    @pytest.mark.require('#5382')
     def test_auto_bootstrap(self):
         """
         auto_adjust_flush_quota isn't a supported updateable parameter.
@@ -141,19 +140,19 @@ class TestUpdateableConfig(Tester):
         self.cluster.populate(1).start(wait_other_notice=True, wait_for_binary_proto=True)
         node1 = self.cluster.nodelist()[0]
 
-        debug('default auto_bootstrap is `true`, try to set it to false first')
+        logger.info('default auto_bootstrap is `true`, try to set it to false first')
         self.change_and_verify_config(node1, 'auto_bootstrap', False, 'true')
         self.change_and_verify_config(node1, 'auto_bootstrap', True, 'true')
 
-    @require('#5384')
+    @pytest.mark.require('#5384')
     def test_sighup_flood(self):
         self.cluster.populate(1).start(wait_other_notice=True, wait_for_binary_proto=True)
         node1 = self.cluster.nodelist()[0]
         node1.stress(['write', 'n=10000', '-rate', 'threads=8'])
         sighup_num = 10000
 
-        debug('Sending %s SIGHUP signal to scylla process ...' % sighup_num)
-        for i in range(sighup_num):
+        logger.info('Sending %s SIGHUP signal to scylla process ...' % sighup_num)
+        for _ in range(sighup_num):
             self.trigger_reload_config(node1)
         self.change_and_verify_config(node1, 'compaction_enforce_min_threshold', True, 'true')
         self.change_and_verify_config(node1, 'compaction_enforce_min_threshold', False, 'false')
@@ -166,7 +165,7 @@ class TestUpdateableConfig(Tester):
         node1 = self.cluster.nodelist()[0]
 
         config_file_path = os.path.join(node1.get_path(), 'conf/scylla.yaml')
-        debug('Rename config file to test updateable config without config file')
+        logger.info('Rename config file to test updateable config without config file')
         os.rename(config_file_path, '%s.backup' % config_file_path)
 
         mark = node1.mark_log()
@@ -178,7 +177,7 @@ class TestUpdateableConfig(Tester):
 
         self.check_errors(node1, [err1, err2])
 
-        debug('Recover the config file')
+        logger.info('Recover the config file')
         os.rename('%s.backup' % config_file_path, config_file_path)
         self.change_and_verify_config(node1, 'compaction_enforce_min_threshold', True, 'true')
         self.change_and_verify_config(node1, 'compaction_enforce_min_threshold', False, 'false')
