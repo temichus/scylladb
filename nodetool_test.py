854ae8f9880109f33b22f598f8eb9c115eb3d8ef
Oren Efraimov <Orenef11@gmail.com>
2021-03-30 11:42:40 +0300

    feature(nodetool_test.py): Converting to Pytest format (#2023)
    * Trello URL: https://trello.com/c/JUx5FTE5

--- nodetool_test.py
+++ nodetool_test.py
@@ -1,9 +1,14 @@
-from ccmlib.node import NodetoolError
-from dtest import Tester, debug
-
-from concurrent.futures import ThreadPoolExecutor
+import logging
 import os
 import time
+from concurrent.futures import ThreadPoolExecutor
+
+import pytest
+from ccmlib.node import NodetoolError
+
+from dtest_class import Tester
+
+logger = logging.getLogger(__name__)
 
 
 class TestNodetool(Tester):
@@ -22,12 +27,10 @@ class TestNodetool(Tester):
         node = cluster.nodelist()[0]
         node.drain(block_on_log=True)
 
-        try:
+        with pytest.raises(expected_exception=(NodetoolError,)) as err:
             node.decommission()
-            self.assertFalse("Expected nodetool error")
-        except NodetoolError as e:
-            self.assertEqual('', e.stderr)
-            self.assertTrue('Node in DRAINED state' in e.stdout)
+        assert not err.value.stderr
+        assert 'Node in DRAINED state' in err.value.stdout
 
     def test_correct_dc_rack_in_nodetool_info(self):
         """
@@ -41,33 +44,36 @@ class TestNodetool(Tester):
         cluster.set_configuration_options(
             values={'endpoint_snitch': 'org.apache.cassandra.locator.GossipingPropertyFileSnitch'})
 
-        for i, node in enumerate(cluster.nodelist()):
+        for idx, node in enumerate(cluster.nodelist()):
             with open(os.path.join(node.get_conf_dir(), 'cassandra-rackdc.properties'), 'w') as snitch_file:
-                for line in ["dc={}".format(node.data_center), "rack=rack{}".format(i % 2)]:
+                for line in ["dc={}".format(node.data_center), "rack=rack{}".format(idx % 2)]:
                     snitch_file.write(line + os.linesep)
 
         cluster.start(wait_for_binary_proto=True)
 
-        for i, node in enumerate(cluster.nodelist()):
+        for idx, node in enumerate(cluster.nodelist()):
             out, err = node.nodetool('info')
-            self.assertEqual(0, len(err), err)
-            debug(out)
+            assert not err, err
+            logger.info(out)
             for line in out.split(os.linesep):
                 if line.startswith('Data Center'):
-                    self.assertTrue(line.endswith(node.data_center),
-                                    "Expected dc {} for {} but got {}".format(node.data_center, node.address(), line.rsplit(None, 1)[-1]))
+                    assert line.endswith(node.data_center), f'Expected dc {node.data_center} for {node.address()} ' \
+                                                            f'but got {line.rsplit(None, 1)[-1]}'
                 elif line.startswith('Rack'):
-                    rack = "rack{}".format(i % 2)
-                    self.assertTrue(line.endswith(rack),
-                                    "Expected rack {} for {} but got {}".format(rack, node.address(), line.rsplit(None, 1)[-1]))
+                    rack = f'rack{idx % 2}'
+                    assert line.endswith(rack), f'Expected rack {rack} for {node.address()} but got ' \
+                                                f'{line.rsplit(None, 1)[-1]}'
 
-    def _background_workload(self, node):
-        debug('start write workload in background...')
-        cs_result, cs_err = node.stress(['write', 'duration=180s', 'no-warmup', '-schema', 'replication(factor=3)', '-rate', 'threads=10', '-log', 'interval=10'],
-                                        capture_output=True)
-        debug('background workload finished')
-        debug(cs_result)
-        debug(cs_err)
+    @staticmethod
+    def _background_workload(node):
+        logger.info('start write workload in background...')
+        cs_result, cs_err = node.stress(
+            ['write', 'duration=180s', 'no-warmup', '-schema', 'replication(factor=3)', '-rate', 'threads=10', '-log',
+             'interval=10'],
+            capture_output=True)
+        logger.info('background workload finished')
+        logger.info(cs_result)
+        logger.info(cs_err)
         return cs_result
 
     def _remove_seed(self, method='kill'):
@@ -79,34 +85,29 @@ class TestNodetool(Tester):
         node1, node2 = self.cluster.nodelist()[0:2]
 
         executor = ThreadPoolExecutor(max_workers=1)
-        t = executor.submit(self._background_workload, node1)
+        thread = executor.submit(self._background_workload, node1)
 
         time.sleep(60)
         if method == 'kill':
-            debug('start to kill node1 ...')
+            logger.info('start to kill node1 ...')
             node1.stop(gently=False)
-            debug('node1 has been killed')
+            logger.info('node1 has been killed')
         elif method == 'decommission':
-            debug('start to decommission node1 ...')
+            logger.info('start to decommission node1 ...')
             node1.decommission()
-            debug('decommission node1 finished')
+            logger.info('decommission node1 finished')
         else:
             raise Exception('Unknown method: %s' % method)
 
-        out, err = node2.nodetool('status', capture_output=True)
-        debug(out)
-        cs_result = t.result()
+        out = node2.nodetool('status', capture_output=True)[0]
+        logger.info(out)
+        cs_result = thread.result()
 
-        self.assertTrue('END' in cs_result.split()[-1], "Stress doesn't complete successfully")
+        assert 'END' in cs_result.split()[-1], "Stress doesn't complete successfully"
 
-    def test_decommission_seed(self):
+    @pytest.mark.parametrize('method', ['decommission', 'kill'])
+    def test_seed(self, method):
         """
-        Test if cassandra-stress works well when seed node is decommission.
+        Test if cassandra-stress works well when seed node is  "decommission" or "killed".
         """
-        self._remove_seed(method='decommission')
-
-    def test_kill_seed(self):
-        """
-        Test if cassandra-stress works well when seed node is killed.
-        """
-        self._remove_seed(method='kill')
+        self._remove_seed(method=method)
