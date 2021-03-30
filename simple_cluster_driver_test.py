96a974bf770141dc930689b5b350891636a6888f
Evgeniy Naydanov <evgeniy.naydanov@scylladb.com>
2021-03-30 13:55:52 +0700

    simple_cluster_driver_test.py: migrate to pytest (#2041)
    Co-authored-by: Israel Fruchter <fruch@scylladb.com>

--- simple_cluster_driver_test.py
+++ simple_cluster_driver_test.py
@@ -1,20 +1,21 @@
 import time
+import logging
 
+import pytest
 from cassandra import Unavailable, ConsistencyLevel
 from cassandra.query import SimpleStatement
+
 from ccmlib.scylla_cluster import ScyllaCluster
-from nose.plugins.attrib import attr
-
-from dtest import Tester, debug
+from dtest_class import Tester, create_ks
 
 
-@attr('dtest-full')
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.dtest_full
 class TestSimpleCluster(Tester):
 
     __scylla_args__ = []
-
-    def __init__(self, *args, **kwargs):
-        Tester.__init__(self, *args, **kwargs)
 
     def prepare(self):
         """
@@ -23,7 +24,7 @@ class TestSimpleCluster(Tester):
         cluster = self.cluster
         return cluster
 
-    def simple_create_insert_select_test(self):
+    def test_simple_create_insert_select(self):
         cluster = self.prepare()
         jvm_args = []
         if type(cluster) is ScyllaCluster:
@@ -34,7 +35,7 @@ class TestSimpleCluster(Tester):
         session1 = self.patient_cql_connection(node1)
         session2 = self.patient_cql_connection(node2)
         session3 = self.patient_cql_connection(node3)
-        self.create_ks(session1, 'ks', 3)
+        create_ks(session1, 'ks', 3)
 
         session2.execute("""
             CREATE TABLE ks.test1 (
@@ -79,7 +80,7 @@ class TestSimpleCluster(Tester):
 
     def simple_consistency_level_validate(self, session, node, read_keys, read_cls_pass, read_cls_fail, read_cls_pass_all, write_keys, write_cls_pass, write_cls_fail, write_cls_pass_all):
         for cl in read_cls_pass:
-            debug("read %s cl %s" % (node, self.clname(cl)))
+            logger.info("read %s cl %s", node, self.clname(cl))
             read_pass = 0
             read_fail = 0
             for key in read_keys:
@@ -88,48 +89,41 @@ class TestSimpleCluster(Tester):
                     res = list(session.execute(query1))
                     assert len(res) == 1, res
                     read_pass = read_pass + 1
-                except Exception as ex:
+                except Exception:
                     read_fail = read_fail + 1
-            assert (read_fail == 0 or not read_cls_pass_all), "Expected all reads to pass, pass %s, fail %s" % (
-                read_pass, read_fail)
-            assert (read_fail > 0 or read_cls_pass_all), "Expected some reads to fail, pass %s, fail %s" % (read_pass, read_fail)
+            assert read_fail == 0 or not read_cls_pass_all, \
+                f"Expected all reads to pass, pass {read_pass}, fail {read_fail}"
+            assert read_fail > 0 or read_cls_pass_all, \
+                f"Expected some reads to fail, pass {read_pass}, fail {read_fail}"
 
         for cl in read_cls_fail:
             query1 = SimpleStatement("SELECT * FROM ks.test1 WHERE k=1", consistency_level=cl)
-            try:
-                res = list(session.execute(query1))
-                assert("Consistency level %s is not possible" % self.clname(cl))
-            except Unavailable as ex:
-                assert(True)
-            except Exception as ex:
-                assert("Expected cassandra.Unavilable exception received %s" % ex)
+            with pytest.raises(Unavailable):
+                list(session.execute(query1))
+                pytest.fail(f"Consistency level {self.clname(cl)} is not possible")
 
         for cl in write_cls_pass:
-            debug("write %s cl %s" % (node, self.clname(cl)))
+            logger.info("write %s cl %s", node, self.clname(cl))
             write_pass = 0
             write_fail = 0
             for key in write_keys:
                 try:
                     insert = SimpleStatement("insert into ks.test1  (k,c) values (%s,%s)" %
                                              (key, key), consistency_level=cl)
-                    res = list(session.execute(insert))
+                    list(session.execute(insert))
                     write_pass = write_pass + 1
-                except Exception as ex:
+                except Exception:
                     write_fail = write_fail + 1
-            assert (write_fail == 0 or not write_cls_pass_all), "Expected all writes to pass, pass %s, fail %s" % (
-                write_pass, write_fail)
-            assert (write_fail > 0 or write_cls_pass_all), "Expected some writes to fail, pass %s, fail %s" % (
-                write_pass, write_fail)
+            assert write_fail == 0 or not write_cls_pass_all, \
+                f"Expected all writes to pass, pass {write_pass}, fail {write_fail}"
+            assert write_fail > 0 or write_cls_pass_all, \
+                f"Expected some writes to fail, pass {write_pass}, fail {write_fail}"
 
         for cl in write_cls_fail:
             insert = SimpleStatement("insert into ks.test1 (k,c) values (101,101)", consistency_level=cl)
-            try:
-                res = list(session.execute(insert))
-                assert("Consistency level %s is not possible" % self.clname(cl))
-            except Unavailable as ex:
-                assert(True)
-            except Exception as ex:
-                assert("Expected cassandra.Unavilable exception received %s" % ex)
+            with pytest.raises(Unavailable):
+                list(session.execute(insert))
+                pytest.fail(f"Consistency level {self.clname(cl)} is not possible")
 
     def prepare_cluster(self, ks_rf):
         cluster = self.prepare()
@@ -143,7 +137,7 @@ class TestSimpleCluster(Tester):
         session2 = self.patient_cql_connection(node2)
         session3 = self.patient_cql_connection(node3)
 
-        self.create_ks(session1, 'ks', ks_rf)
+        create_ks(session1, 'ks', ks_rf)
         session1.execute("""
             CREATE TABLE ks.test1 (
                 k int PRIMARY KEY,
@@ -152,7 +146,7 @@ class TestSimpleCluster(Tester):
         """)
         return cluster
 
-    def simple_rf_3_consistency_level_tests(self):
+    def test_simple_rf_3_consistency_level(self):
         cluster = self.prepare_cluster(3)
         node1, node2, node3 = cluster.nodelist()
         session1 = self.patient_cql_connection(node1)
@@ -165,7 +159,7 @@ class TestSimpleCluster(Tester):
                                      (val, val), consistency_level=ConsistencyLevel.ALL)
             session1.execute(insert)
 
-        debug("3 nodes, node1,node2,node3 are running")
+        logger.info("3 nodes, node1,node2,node3 are running")
         read_cls_pass = [ConsistencyLevel.ONE, ConsistencyLevel.TWO,
                          ConsistencyLevel.THREE, ConsistencyLevel.QUORUM, ConsistencyLevel.ALL]
         write_cls_pass = [ConsistencyLevel.ANY, ConsistencyLevel.ONE, ConsistencyLevel.TWO,
@@ -180,7 +174,7 @@ class TestSimpleCluster(Tester):
         node1.stop()
         # FIXME - currently used to make sure that the gossiper has concluded the node is dead - on origin it works without this
         time.sleep(60)
-        debug("node 1 stopped, node2,node3 are running")
+        logger.info("node 1 stopped, node2,node3 are running")
         read_cls_pass = [ConsistencyLevel.ONE, ConsistencyLevel.TWO, ConsistencyLevel.QUORUM]
         read_cls_fail = [ConsistencyLevel.THREE, ConsistencyLevel.ALL]
         write_cls_pass = [ConsistencyLevel.ANY, ConsistencyLevel.ONE, ConsistencyLevel.TWO, ConsistencyLevel.QUORUM]
@@ -193,7 +187,7 @@ class TestSimpleCluster(Tester):
         node2.stop()
         # FIXME - currently used to make sure that the gossiper has concluded the node is dead - on origin it works without this
         time.sleep(60)
-        debug("node 2 stopped, node3 is running")
+        logger.info("node 2 stopped, node3 is running")
         read_cls_pass = [ConsistencyLevel.ONE]
         read_cls_fail = [ConsistencyLevel.TWO, ConsistencyLevel.THREE, ConsistencyLevel.QUORUM, ConsistencyLevel.ALL]
         write_cls_pass = [ConsistencyLevel.ANY, ConsistencyLevel.ONE]
@@ -203,7 +197,7 @@ class TestSimpleCluster(Tester):
 
         # should add additional tests once a node can be entered back into a cluster
 
-    def simple_rf_1_consistency_level_tests(self):
+    def test_simple_rf_1_consistency_level(self):
         cluster = self.prepare_cluster(1)
         node1, node2, node3 = cluster.nodelist()
         session1 = self.patient_cql_connection(node1)
@@ -216,7 +210,7 @@ class TestSimpleCluster(Tester):
                                      (val, val), consistency_level=ConsistencyLevel.ALL)
             session1.execute(insert)
 
-        debug("3 nodes, node1,node2,node3 are running")
+        logger.info("3 nodes, node1,node2,node3 are running")
         read_cls_pass = [ConsistencyLevel.ONE, ConsistencyLevel.QUORUM, ConsistencyLevel.ALL]
         read_cls_fail = [ConsistencyLevel.TWO, ConsistencyLevel.THREE]
         write_cls_pass = [ConsistencyLevel.ANY, ConsistencyLevel.ONE, ConsistencyLevel.QUORUM, ConsistencyLevel.ALL]
@@ -229,7 +223,7 @@ class TestSimpleCluster(Tester):
             301, 400), write_cls_pass, write_cls_fail, True)
 
         node1.stop()
-        debug("node 1 stopped, node2,node3 are running")
+        logger.info("node 1 stopped, node2,node3 are running")
         read_cls_pass = [ConsistencyLevel.ONE, ConsistencyLevel.QUORUM, ConsistencyLevel.ALL]
         read_cls_fail = [ConsistencyLevel.TWO, ConsistencyLevel.THREE]
         write_cls_pass = [ConsistencyLevel.ONE, ConsistencyLevel.QUORUM, ConsistencyLevel.ALL]
@@ -240,7 +234,7 @@ class TestSimpleCluster(Tester):
             501, 600), write_cls_pass, write_cls_fail, False)
 
         node2.stop()
-        debug("node 2 stopped, node3 is running")
+        logger.info("node 2 stopped, node3 is running")
         read_cls_pass = [ConsistencyLevel.ONE, ConsistencyLevel.QUORUM, ConsistencyLevel.ALL]
         read_cls_fail = [ConsistencyLevel.TWO, ConsistencyLevel.THREE]
         write_cls_pass = [ConsistencyLevel.ONE, ConsistencyLevel.QUORUM, ConsistencyLevel.ALL]
@@ -252,33 +246,30 @@ class TestSimpleCluster(Tester):
 
     def simple_query_validate(self, session, node, read_keys, query, read_cls_pass, read_cls_fail, read_cls_pass_all):
         for cl in read_cls_pass:
-            debug("read %s cl %s" % (node, self.clname(cl)))
+            logger.info("read %s cl %s", node, self.clname(cl))
             read_pass = 0
             read_fail = 0
             errors = []
             try:
                 query1 = SimpleStatement(query, consistency_level=cl)
                 res = list(session.execute(query1))
-                assert len(res) == read_keys, "got %s expected %s " % (len(res), read_keys) + " : " + str(res)
+                assert len(res) == read_keys, f"got {len(res)} expected {read_keys} : {res}"
                 read_pass = read_pass + 1
             except Exception as ex:
                 read_fail = read_fail + 1
                 errors = errors + [ex]
-            assert (read_fail == 0 or not read_cls_pass_all), "Expected all reads to pass, pass %s, fail %s" % (
-                read_pass, read_fail) + str(errors)
-            assert (read_fail > 0 or read_cls_pass_all), "Expected some reads to fail, pass %s, fail %s" % (read_pass, read_fail)
+            assert read_fail == 0 or not read_cls_pass_all, \
+                f"Expected all reads to pass, pass {read_pass}, fail {read_fail} {errors}"
+            assert read_fail > 0 or read_cls_pass_all, \
+                f"Expected some reads to fail, pass {read_pass}, fail {read_fail}"
 
         for cl in read_cls_fail:
             query1 = SimpleStatement(query, consistency_level=cl)
-            try:
-                res = list(session.execute(query1))
-                assert("Consistency level %s is not possible" % self.clname(cl))
-            except Unavailable as ex:
-                assert(True)
-            except Exception as ex:
-                assert("Expected cassandra.Unavilable exception received %s" % ex)
+            with pytest.raises(Unavailable):
+                list(session.execute(query1))
+                pytest.fail(f"Consistency level {self.clname(cl)} is not possible")
 
-    def simple_rf_1_query_tests(self):
+    def test_simple_rf_1_query(self):
         cluster = self.prepare_cluster(1)
 
         node1, node2, node3 = cluster.nodelist()
@@ -292,7 +283,7 @@ class TestSimpleCluster(Tester):
                                      (val, val), consistency_level=ConsistencyLevel.ALL)
             session1.execute(insert)
 
-        debug("3 nodes, node1,node2,node3 are running")
+        logger.info("3 nodes, node1,node2,node3 are running")
         read_cls_pass = [ConsistencyLevel.ONE, ConsistencyLevel.QUORUM, ConsistencyLevel.ALL]
         read_cls_fail = [ConsistencyLevel.TWO, ConsistencyLevel.THREE]
         self.simple_query_validate(session1, "node 1", len(
@@ -308,7 +299,7 @@ class TestSimpleCluster(Tester):
         self.simple_query_validate(
             session3, "node 3", 10, "SELECT * FROM ks.test1 where k in (1,10,20,30,40,50,60,70,80,90) ", read_cls_pass, read_cls_fail, True)
 
-    def simple_rf_3_query_tests(self):
+    def test_simple_rf_3_query(self):
         cluster = self.prepare_cluster(3)
 
         node1, node2, node3 = cluster.nodelist()
@@ -322,7 +313,7 @@ class TestSimpleCluster(Tester):
                                      (val, val), consistency_level=ConsistencyLevel.ALL)
             session1.execute(insert)
 
-        debug("3 nodes, node1,node2,node3 are running")
+        logger.info("3 nodes, node1,node2,node3 are running")
         read_cls_pass = [ConsistencyLevel.ONE, ConsistencyLevel.TWO,
                          ConsistencyLevel.THREE, ConsistencyLevel.QUORUM, ConsistencyLevel.ALL]
         self.simple_query_validate(session1, "node 1", len(keys), "SELECT * FROM ks.test1", read_cls_pass, [], True)
