d0fd675415e2083820bd72c71ede5350347f8a5f
juliayakovlev <34435448+juliayakovlev@users.noreply.github.com>
2021-04-05 17:23:55 +0300

    Convert simple_no_driver_test.py to pytest (#2071)

--- simple_no_driver_test.py
+++ simple_no_driver_test.py
@@ -1,18 +1,22 @@
+import logging
 import subprocess
 import tempfile
 
+import pytest
+
 from ccmlib.scylla_cluster import ScyllaCluster
 
-from dtest import Tester, debug
+from dtest_class import Tester
+
+logger = logging.getLogger(__name__)
 
 
+@pytest.mark.parametrize('smp_options', [['--smp', '1'], ['--smp', '2']], ids=['SMP=1', 'SMP=2'])
 class TestSimple(Tester):
 
-    __test__ = False
-    __scylla_args__ = []
-
-    def __init__(self, *args, **kwargs):
-        Tester.__init__(self, *args, **kwargs)
+    @pytest.fixture(scope='function', autouse=True)
+    def fixture_dtest_setup_overrides(self, dtest_config, smp_options):
+        self.scylla_args = smp_options
 
     def prepare(self):
         """
@@ -36,8 +40,8 @@ class TestSimple(Tester):
         # Verify the data
         tmpfile = tempfile.mktemp()
         with open(tmpfile, 'w+') as tmp:
-            node.stress(['read', 'n=100000', '-mode', 'cql3', 'simplenative', '-rate', 'threads=1', '-pop', 'seq=1..100000'],
-                        stdout=tmp, stderr=subprocess.STDOUT)
+            node.stress(['read', 'n=100000', '-mode', 'cql3', 'simplenative', '-rate', 'threads=1', '-pop',
+                         'seq=1..100000'], stdout=tmp, stderr=subprocess.STDOUT)
         return tmpfile
 
     def validate_stress_output(self, outfile, expect_failure=False, expect_errors=False):
@@ -52,20 +56,20 @@ class TestSimple(Tester):
         with open(outfile, 'r') as tmp:
             output = tmp.read()
 
-            debug(output)
+            logger.debug(output)
             failure = output.find("Data returned was not validated")
             if expect_failure:
                 assert failure >= 0, "No missing data detected, despite data loss"
             else:
-                self.assertEqual(failure, -1, "Stress failed to validate all data")
+                assert failure == -1, "Stress failed to validate all data"
 
             failure = output.find("Exception")
             if expect_errors:
                 assert failure >= 0, "No errors detected, despite invalid cluster state"
             else:
-                self.assertEqual(failure, -1, "Error while reading data")
+                assert failure == -1, "Error while reading data"
 
-    def simple_single_node_write_read_test(self):
+    def test_simple_single_node_write_read(self):
         """
         A basic test that writes data at CL=ONE, RF=1
         Tests to ensure no data is lost or errors thrown.
@@ -73,16 +77,9 @@ class TestSimple(Tester):
         cluster = self.prepare()
         jvm_args = []
         if type(cluster) is ScyllaCluster:
-            jvm_args = self.__scylla_args__
+            jvm_args = self.scylla_args
         cluster.populate(1).start(jvm_args=jvm_args)
         node1 = cluster.nodelist()[0]
         self.stress_write(node1)
         out = self.stress_read(node1)
         self.validate_stress_output(out)
-
-
-options = {'Single': ['--smp', '1'], 'SMP': ['--smp', '2']}
-
-for option in options.keys():
-    cls_name = ('SimpleNoDriverTest_with_' + option)
-    vars()[cls_name] = type(cls_name, (TestSimple,), {'__scylla_args__': options[option], '__test__': True})
