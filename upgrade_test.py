249449b401c7dd369a59ef4b309afab0bd7c55ba
julia.yakovlev <julia.yakovlev@scylladb.com>
2021-04-05 13:21:00 +0300

    feature(upgrade test): add 4.5 version to upgrade and rolling upgrade tests
    4.5 relocatables isn't created yet. Need wait for relocatables before merging
    
    (cherry picked from commit acad302a2e0064efb7c8096fc27b377846fa6594)

--- upgrade_test.py
+++ upgrade_test.py
@@ -17,8 +17,8 @@ from dtest_config import DTestConfig
 
 logger = logging.getLogger(__name__)
 
-upgrade_matrix_1 = ['release:4.0', 'release:4.1', 'release:4.2', 'release:4.3', 'release:4.4']
-upgrade_matrix_2 = ['release:4.4']
+upgrade_matrix_1 = ['release:4.0', 'release:4.1', 'release:4.2', 'release:4.3', 'release:4.4', 'release:4.5']
+upgrade_matrix_2 = ['release:4.5']
 
 
 class UpgradeTester(Tester):
