d5b3cc98c14326f2c83dcfcfff8e1965bae4094f
Benny Halevy <bhalevy@scylladb.com>
2021-04-25 12:08:25 +0300

    database_metrics_test: total_reads_user_test: debug metrics
    Print a debug message for each metric.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- database_metrics_test.py
+++ database_metrics_test.py
@@ -57,5 +57,9 @@ class TestDatabaseMetrics(Tester):
         final_reads = self.get_metrics(get_ip_from_node(node), metrics=metrics, metric_class=metric_class)
 
         for metric_name in list(initial_reads.keys()):
-            assert final_reads[metric_name] > initial_reads[metric_name],\
+            added = int(final_reads[metric_name])-int(initial_reads[metric_name])
+            debug(f"final_reads[{metric_name}]={final_reads[metric_name]} " +
+                  f"initial_reads[{metric_name}]={initial_reads[metric_name]} " +
+                  f"({added} added)")
+            assert added > 0,\
                 f'{metrics[0]} did not increase as expected. initial={initial_reads}; final={final_reads}'
