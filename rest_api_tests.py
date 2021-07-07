51d3c29ee0a78de2ae34579ce1a24fa9ab6fa0a7
Fabio Gelcer <fabio@scylladb.com>
2021-07-07 22:41:54 +0300

    test(rest_api_tests): adding a test to cover new feature
    introduced on scylladb/scylla@761f89e there is now an
    API to evict row cache, and this commit adds a new
    test to confirm it works.
    
    (cherry picked from commit 994e857db6188916d97ff9db4d576522ead82c4d)

--- rest_api_tests.py
+++ rest_api_tests.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 from time import sleep
 import logging
+import re
 
 import requests
 import pytest
 
-from dtest_class import Tester
+from dtest_class import Tester, get_ip_from_node
+from tools.metrics import prometheus_get
 
 logger = logging.getLogger(__name__)
 
@@ -43,7 +45,8 @@ class TestScyllaARestApi(Tester):
         renewed_node1_uptime = self.request_uptime(node1)
         now = datetime.now()
         assert (now - test_start_time).total_seconds() - 10 > renewed_node1_uptime/1000, \
-            f"The uptime received from scylla does not match the expected uptime\nreceived uptime:{renewed_node1_uptime}"
+            f"The uptime received from scylla does not match the expected uptime\n" \
+            f"received uptime:{renewed_node1_uptime}"
 
     def test_compare_node_uptime(self):
         node1, node2 = self.config_and_create_cluster(2)
@@ -54,4 +57,52 @@ class TestScyllaARestApi(Tester):
         node1_uptime = self.request_uptime(node1)
         node2_uptime = self.request_uptime(node2)
         assert node2_uptime - node1_uptime > 10000, \
-            f"The difference between the nodes' uptime did not match expectations\nnode1: {node1_uptime}\nnode2: {node2_uptime}"
+            f"The difference between the nodes' uptime did not match expectations\n" \
+            f"node1: {node1_uptime}\nnode2: {node2_uptime}"
+
+    def get_metrics(self, node_ip, metrics, port='9180', metric_class=None):
+        metrics_res = {}
+        metric_pattern = re.compile('.*{')
+        prometheus_results = [metric for metric in prometheus_get(node_ip, port).splitlines() if
+                              not metric.startswith('#')]
+        for metric in prometheus_results:
+            for metric_name in metrics:
+                if metric_pattern.match(metric) and re.search(f'{metric_name}{{', metric):
+                    if metric_class and not re.search(f'class="{metric_class}"', metric):
+                        continue
+                    name, val = metric.split()
+                    metrics_res[name.split('{')[0]] = float(val)
+        return metrics_res
+
+    @staticmethod
+    def drop_row_cache(node):
+        url = f"http://{node.address()}:10000/system/drop_sstable_caches"
+        requests.post(url=url)
+
+    @pytest.mark.single_node
+    def test_drop_row_cache(self):
+        node1 = self.config_and_create_cluster(1)[0]
+        increase_metrics = ['scylla_cache_reads_with_misses',
+                            'scylla_cache_row_misses',
+                            'scylla_cache_row_evictions',
+                            ]
+        decrease_metrics = ['scylla_lsa_used_space_bytes',
+                            'scylla_cache_partitions',
+                            'scylla_lsa_memory_allocated',
+                            'scylla_cache_rows',
+                            ]
+
+        metrics = increase_metrics + decrease_metrics
+
+        stress_cmd = ['n=50K', '-rate', 'threads=4']
+        self.cluster.stress(['write'] + stress_cmd)
+        self.cluster.stress(['read'] + stress_cmd)
+
+        before = self.get_metrics(get_ip_from_node(node1), metrics)
+        self.drop_row_cache(node1)
+        after = self.get_metrics(get_ip_from_node(node1), metrics)
+
+        for metric in increase_metrics:
+            assert after[metric] >= before[metric], f'{metric} should have increased after dropping cache'
+        for metric in decrease_metrics:
+            assert after[metric] <= before[metric], f'{metric} should have decreased after dropping cache'
