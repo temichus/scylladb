891a21d525721c983e69b376970fa4dc5bf37081
Israel Fruchter <fruch@scylladb.com>
2021-11-21 10:09:55 +0200

    error_example_test.py: migrate to pytest

--- error_example_test.py
+++ error_example_test.py
@@ -1,19 +1,18 @@
-from dtest import Tester
-
+from dtest_class import Tester, create_ks
 # NOTE: this should be run with scylla dev or debug builds
 #       see  README.md and scylla_dtest_env.sh CASSANDRA_DIR
 
 MY_NODE = 0
 
 
-class Example(Tester):
+class TestExample(Tester):
 
-    def some_test(self, nodes=3, rf=3, jvm_args=None):
+    def test_some(self, nodes=3, rf=3, jvm_args=None):
         cluster = self.cluster
         cluster.populate(nodes).start(wait_for_binary_proto=True)
         node = cluster.nodelist()[MY_NODE]
         session = self.patient_cql_connection(node)
-        self.create_ks(session=session, name="ks", rf=rf)
+        create_ks(session=session, name="ks", rf=rf)
 
         self.enable_error("error1",  MY_NODE)
         self.enable_error("error2",  MY_NODE, one_shot=True)
