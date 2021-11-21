213658e60b9421a1dda33a1715f5ab696cd0d97a
Israel Fruchter <fruch@scylladb.com>
2021-11-21 11:14:50 +0200

    scylla_unsupported_test.py: migrate to pytest

--- scylla_unsupported_test.py
+++ scylla_unsupported_test.py
@@ -1,28 +1,28 @@
 # coding: utf-8
 
-from dtest import Tester
-from unittest import skip
+from dtest_class import Tester
+import pytest
 
 
-class ScyllaUnsupportedTest(Tester):
+class TestScyllaUnsupported(Tester):
 
-    @skip('unimplemented')
-    def unsupported_feature_cql_counters(self):
+    @pytest.mark.skip('unimplemented')
+    def test_unsupported_feature_cql_counters(self):
         """ Check that a create table with counter returns an informative error """
         raise NotImplementedError
 
-    @skip('unimplemented')
-    def unsupported_feature_cql_secondary_index(self):
+    @pytest.mark.skip('unimplemented')
+    def test_unsupported_feature_cql_secondary_index(self):
         """ Check that a create index returns an informative error """
         raise NotImplementedError
 
-    @skip('unimplemented')
-    def unsupported_feature_cql_user_type(self):
+    @pytest.mark.skip('unimplemented')
+    def test_unsupported_feature_cql_user_type(self):
         """ Check that a create type returns an informative error """
         raise NotImplementedError
 
-    @skip('unimplemented')
-    def unsupported_feature_cql_lwt(self):
+    @pytest.mark.skip('unimplemented')
+    def test_unsupported_feature_cql_lwt(self):
         """
         Check that a insert with IF returns an informative error
         Check that a update with IF returns an informative error
