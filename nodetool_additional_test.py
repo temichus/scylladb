c2e4e6a560fa96e56173ff45a934843a55aede78
Israel Fruchter <fruch@scylladb.com>
2021-12-28 12:02:12 +0200

    nodetool_additional_test: revert usage of raw string
    13bb3a4fbf8796feaaa1cdbdcd1fbdedf3b12930 change this one even that is should
    be change to raw, since it meant to replace tab with 4 spaces
    
    In jenkins run it killed the yaml parser that doesn't like tabs
    ```
    >               self.get_mark())
    E       yaml.scanner.ScannerError: while scanning for the next token
    E       found character '\t' that cannot start any token
    E         in "<unicode string>", line 2, column 1:
    E           	Name: test
    E           ^
    
    /usr/local/lib64/python3.7/site-packages/yaml/scanner.py:260: ScannerError
    ```

--- nodetool_additional_test.py
+++ nodetool_additional_test.py
@@ -1047,7 +1047,7 @@ class TestNodetool(Tester):
     @staticmethod
     def describecluster(node):
         out = node.nodetool('describecluster', True)[0]
-        return yaml.safe_load(out.replace(r'\t', "  "))
+        return yaml.safe_load(out.replace('\t', "  "))
 
     def verify_decribecluster(self, node=None):
         node = self.get_node(node)
