3fa8e423741545dc08b028dff1f589e2e8f24971
Israel Fruchter <fruch@scylladb.com>
2021-12-28 15:35:01 +0200

    pipelines: for dtest default to job git info

--- vars/branchProperties.groovy
+++ vars/branchProperties.groovy
@@ -6,4 +6,3 @@ import groovy.transform.Field
 @Field String productName = 'scylla'
 
 @Field String ccmDefaultRepo = 'git@github.com:scylladb/scylla-ccm.git'
-@Field String dtestDefaultRepo = 'git@github.com:scylladb/scylla-dtest.git'
