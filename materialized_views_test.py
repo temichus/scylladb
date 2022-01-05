d5d054cc20a770500448e29957ba3a060030fb38
Benny Halevy <bhalevy@scylladb.com>
2022-01-05 14:20:08 +0200

    materialized_views_test: _do_resharding_test: update expected errors
    Since scylladb/scylla@e80cb51b6ae43f8f928116fb9b37931ee9d6906a
    the distributed loader tolerates compact_stopped_excpetion
    happening when populating tables, when resharding is stopped
    by the test.
    
    Add the respective error message to the ignore_log_patterns.
    
    Seen, e.g. in https://jenkins.scylladb.com/view/master/job/scylla-master/job/dtest-release/1056/testReport/materialized_views_test/TestMaterializedViews/test_interrupt_build_process_and_resharding_half_to_max/
    ```
    failed on teardown with "Failed: Error details:
    Errors seen in logs for: node1
    node1: ERROR 2022-01-04 10:52:39,139 [shard 0] database - Exception while populating keyspace 'ks' with column family 't' from file '/jenkins/workspace/scylla-master/dtest-release/scylla/.dtest/dtest-1slyjukq/test/node1/data/ks/t-46d503706d4c11ec8a3ad3ec12ca9e13': sstables::compaction_stopped_exception (Compaction for ks/t was stopped due to: shutdown)"
    ```
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- materialized_views_test.py
+++ materialized_views_test.py
@@ -2275,6 +2275,8 @@ class TestMaterializedViews(CommonUtils):
             self.fixture_dtest_setup.ignore_log_patterns += [
                 # for now, until the reader is properly aborted and closed
                 r'resharding failed: seastar::broken_promise',
+                r"Exception while populating keyspace 'ks' with column family 't\w*' .*: sstables::compaction_stopped_exception",
+                r'Startup failed: seastar::sleep_aborted',
             ]
         session = self.prepare(options={'hinted_handoff_enabled': False, 'shadow_round_ms': 1000, 'prometheus_port': 0,
                                         'read_request_timeout_in_ms': 100000, 'range_request_timeout_in_ms': 100000},
