54fe5d619ccd8e04f47d6a7506d54ccb6460b9a5
MaciekCisowski <maciej.cisowski@scylladb.com>
2022-03-15 14:28:24 +0100

    refactor(sla): use ScyllaDuration for timeouts

--- tools/sla.py
+++ tools/sla.py
@@ -1,32 +1,18 @@
 import logging
 from dataclasses import dataclass, field, fields
+from typing import List, Optional
+
+from cassandra.cluster import Session
+
+from tools.units import ScyllaDuration
 
 logger = logging.getLogger(__name__)
-
-
-DEFAULT_SERVICE_LEVEL_SHARES = 1000
-
-
-def sla_to_dict(sla_result):
-    # Result example: <type 'list'>: [Row(service_level=u'sla1', shares=1)]
-    sla_list = []
-    for row in sla_result:
-        sla_list.append([row.service_level, row.shares])
-    return sla_list
-
-
-def role_to_dict(sla_result):
-    # Result example: <type 'list'>: [Row(role=u'role1', service_level=u'sla1')]
-    sla_list = []
-    for row in sla_result:
-        sla_list.append([row.role, row.service_level])
-    return sla_list
 
 
 @dataclass
 class ServiceLevelAttributes:
     shares: int = None
-    timeout: str = None
+    timeout: ScyllaDuration = None
     workload_type: str = None
     query_string: str = field(init=False, repr=False)
 
@@ -34,6 +20,13 @@ class ServiceLevelAttributes:
         super().__setattr__(key, value)
         if key != "query_string":
             self._generate_query_string()
+
+    def __eq__(self, other):
+        return all((
+            self.shares == other.shares,
+            self.timeout.get_duration() == other.timeout.get_duration(),
+            self.workload_type == other.workload_type
+        ))
 
     def __post_init__(self):
         self._generate_query_string()
@@ -44,7 +37,9 @@ class ServiceLevelAttributes:
         for item in fields(self):
             value = getattr(self, item.name) if item.repr else None
             if value is not None:
-                if item.type is str:
+                if item.type is ScyllaDuration:
+                    attr_strings.append(f" AND {item.name} = '{value.to_query_string()}'")
+                elif item.type is str:
                     attr_strings.append(f" AND {item.name} = '{value}'")
                 else:
                     attr_strings.append(f" AND {item.name} = {value}")
@@ -64,18 +59,25 @@ class ServiceLevel(object):
     # The class provide interface to manage SERVICE LEVEL
     def __init__(self, session,
                  name: str,
-                 service_shares: int = 1000,
-                 timeout: str = None,
-                 workload_type: str = None, verbose=True):
+                 shares: int = 1000,
+                 timeout: ScyllaDuration = None,
+                 workload_type: str = None,
+                 verbose=True):
         self.session = session
-        self._name = name
+        self._name = f'"{name}"'
         self.verbose = verbose
         self._created = False
         self._sl_attributes = ServiceLevelAttributes(
-            shares=service_shares,
+            shares=shares,
             timeout=timeout,
             workload_type=workload_type
         )
+
+    @classmethod
+    def from_row(cls, session: Session, row):
+        row_dict = row._asdict()
+        row_dict["name"] = row_dict.pop("service_level")
+        return ServiceLevel(session=session, **row_dict)
 
     @property
     def name(self) -> str:
@@ -86,11 +88,11 @@ class ServiceLevel(object):
         self._name = name
 
     @property
-    def service_shares(self) -> int:
+    def shares(self) -> int:
         return self._sl_attributes.shares
 
-    @service_shares.setter
-    def service_shares(self, service_level_shares):
+    @shares.setter
+    def shares(self, service_level_shares):
         self._sl_attributes.shares = service_level_shares
 
     @property
@@ -102,7 +104,7 @@ class ServiceLevel(object):
         self._created = created
 
     @property
-    def timeout(self) -> str:
+    def timeout(self) -> ScyllaDuration:
         return self._sl_attributes.timeout
 
     @timeout.setter
@@ -117,54 +119,85 @@ class ServiceLevel(object):
     def workload_type(self, workload_type: str):
         self._sl_attributes.workload_type = workload_type
 
-    def create(self, if_not_exists=True):
-        query = 'CREATE SERVICE_LEVEL{if_not_exists} {service_level_name}{query_attributes}'\
-                .format(if_not_exists=' IF NOT EXISTS' if if_not_exists else '',
-                        service_level_name=self.name,
-                        query_attributes=self._sl_attributes.query_string)
+    def __eq__(self, other):
+        return (self.name == other.name) and self._sl_attributes == other._sl_attributes
+
+    def __repr__(self):
+        return "%s: name: %s, attributes: %s" % (self.__class__.__name__, self.name, self._sl_attributes)
+
+    def create(self, if_not_exists=True) -> "ServiceLevel":
+        query = 'CREATE SERVICE_LEVEL{if_not_exists} {service_level_name}{query_attributes}' \
+            .format(if_not_exists=' IF NOT EXISTS' if if_not_exists else '',
+                    service_level_name=self.name,
+                    query_attributes=self._sl_attributes.query_string)
         if self.verbose:
             logger.debug('Create service level query: {}'.format(query))
         self.session.execute(query)
         logger.debug('Service level "{}" has been created'.format(self.name))
         self.created = True
+        return self
 
-    def alter(self, new_shares: int = None, new_timeout: str = None, new_workload_type: str = None):
+    def alter(self, new_shares: int = None, new_timeout: ScyllaDuration = None, new_workload_type: str = None):
         sla = ServiceLevelAttributes(shares=new_shares, timeout=new_timeout, workload_type=new_workload_type)
-        query = 'ALTER SERVICE_LEVEL {service_level_name} {query_string}'\
-                .format(service_level_name=self.name,
-                        query_string=sla.query_string)
+        query = 'ALTER SERVICE_LEVEL {service_level_name} {query_string}' \
+            .format(service_level_name=self.name,
+                    query_string=sla.query_string)
         if self.verbose:
             logger.debug('Change service level query: {}'.format(query))
         self.session.execute(query)
         logger.debug('Service level "{}" has been altered'.format(self.name))
-        self.service_shares = new_shares
+        self.shares = new_shares
 
     def drop(self, if_exists=True):
-        query = 'DROP SERVICE_LEVEL{if_exists} {service_level_name}'\
-                .format(service_level_name=self.name,
-                        if_exists=' IF EXISTS' if if_exists else '')
+        query = 'DROP SERVICE_LEVEL{if_exists} {service_level_name}' \
+            .format(service_level_name=self.name,
+                    if_exists=' IF EXISTS' if if_exists else '')
         if self.verbose:
             logger.debug('Drop service level query: {}'.format(query))
         self.session.execute(query)
         logger.debug('Service level "{}" has been dropped'.format(self.name))
         self.created = False
 
-    def list_service_level(self):
+    def list_service_level(self) -> Optional["ServiceLevel"]:
         query = 'LIST SERVICE_LEVEL {}'.format(self.name)
         if self.verbose:
             logger.debug('List service level query: {}'.format(query))
-        res = list(self.session.execute(query))
-        return sla_to_dict(res)
+        res = self.session.execute(query).all()
+        assert len(res) <= 1, "Received %s service levels when expecting to receive only 1" % len(res)
 
-    def list_all_service_levels(self):
+        if len(res) == 0:
+            return []
+
+        result_dict = res[0]._asdict()
+        parsed_result_dict = self._parse_row_dict(result_dict)
+
+        return ServiceLevel(session=self.session, **parsed_result_dict)
+
+    def list_all_service_levels(self) -> List["ServiceLevel"]:
         query = 'LIST ALL SERVICE_LEVELS'
         if self.verbose:
             logger.debug('List all service levels query: {}'.format(query))
-        res = list(self.session.execute(query))
-        return sla_to_dict(res)
+        res_list = self.session.execute(query).all()
+        output = []
+
+        for res in res_list:
+            result_dict = res._asdict()
+            parsed_result_dict = self._parse_row_dict(result_dict)
+            output.append(ServiceLevel(session=self.session, **parsed_result_dict))
+        return output
+
+    def _parse_row_dict(self, row_dict) -> dict:
+        row_dict["name"] = row_dict.pop("service_level")
+        timeout_value = row_dict["timeout"]
+        if timeout_value and timeout_value != "null":
+            row_dict["timeout"] = ScyllaDuration.from_duration(timeout_value)
+
+        row_dict["shares"] = row_dict.get("shares")
+
+        return row_dict
 
 
-class UserRoleBase(object):
+class UserRoleBase:
     # Base class for ROLES and USERS
     AUTHENTICATION_ENTITY = ''
 
@@ -174,6 +207,7 @@ class UserRoleBase(object):
         self.session = session
         self.superuser = superuser
         self.verbose = verbose
+        self._attached_service_level = None
         self._attached_service_level_name = ''
         self._attached_service_level_shares = None
 
@@ -186,50 +220,37 @@ class UserRoleBase(object):
         self._name = name
 
     @property
-    def attached_service_level_name(self):
-        return self._attached_service_level_name
-
-    @attached_service_level_name.setter
-    def attached_service_level_name(self, service_level_name):
-        self._attached_service_level_name = service_level_name
+    def attached_service_level(self):
+        return self._attached_service_level
 
     @property
-    def attached_service_level_shares(self):
-        return self._attached_service_level_shares
-
-    @attached_service_level_shares.setter
-    def attached_service_level_shares(self, service_level_shares):
-        self._attached_service_level_shares = service_level_shares
+    def attached_service_level_name(self):
+        return self._attached_service_level.name
 
     def attach_service_level(self, service_level):
         """
         :param auth_name: it may be role name or user name
         """
-        query = 'ATTACH SERVICE_LEVEL {service_level_name} TO {role_name}'\
-                .format(service_level_name=service_level.name,
-                        role_name=self.name)
+        query = 'ATTACH SERVICE_LEVEL {service_level_name} TO {role_name}' \
+            .format(service_level_name=service_level.name,
+                    role_name=self.name)
         if self.verbose:
             logger.debug('Attach service level query: {}'.format(query))
         self.session.execute(query)
         logger.debug('Service level "{}" has been attached to {} role'.format(service_level.name, self.name))
-        self.attached_service_level_name = service_level.name
-        self.attached_service_level_shares = service_level.service_shares \
-            if service_level.service_shares \
-            else DEFAULT_SERVICE_LEVEL_SHARES
+        self._attached_service_level = service_level
 
     def detach_service_level(self):
         """
         :param auth_name: it may be role name or user name
         """
-        query = 'DETACH SERVICE_LEVEL FROM {role_name}'\
-                .format(role_name=self.name)
+        query = 'DETACH SERVICE_LEVEL FROM {role_name}' \
+            .format(role_name=self.name)
         if self.verbose:
             logger.debug('Detach service level query: {}'.format(query))
         self.session.execute(query)
         logger.debug('The service level has been detached from {} role'.format(self.name))
-
-        self.attached_service_level_name = ''
-        self.attached_service_level_shares = None
+        self._attached_service_level = None
 
     def grant_me_to(self, grant_to):
         role_be_granted = self.name
@@ -257,22 +278,14 @@ class UserRoleBase(object):
         query = 'LIST ATTACHED SERVICE_LEVEL OF {}'.format(self.name)
         if self.verbose:
             logger.debug('List attached service level(s) query: {}'.format(query))
-        res = list(self.session.execute(query))
-        return role_to_dict(res)
+        return self.session.execute(query).all()
 
     def list_all_attached_service_levels(self):
         query = 'LIST ATTACHED ALL SERVICE_LEVELS'
         if self.verbose:
             logger.debug('List attached service level(s) query: {}'.format(query))
         res = list(self.session.execute(query))
-        return sla_to_dict(res)
-
-    def list_effective_service_levels(self, auth_obj):
-        query = 'LIST SERVICE_LEVELS OF {}'.format(auth_obj.name)
-        if self.verbose:
-            logger.debug('List effective service levels query: {}'.format(query))
-        res = list(self.session.execute(query))
-        return sla_to_dict(res)
+        return res
 
     def drop(self, if_exists=True):
         query = 'DROP {entity}{if_exists} {name}'.format(entity=self.AUTHENTICATION_ENTITY,
@@ -283,7 +296,6 @@ class UserRoleBase(object):
 
         self.session.execute(query)
         logger.debug('{entity} "{name}" has been dropped'.format(entity=self.AUTHENTICATION_ENTITY, name=self.name))
-        self.role_name = ''
 
 
 class Role(UserRoleBase):
@@ -295,7 +307,7 @@ class Role(UserRoleBase):
         self.login = login
         self.options_dict = options_dict
 
-    def create(self):
+    def create(self) -> "Role":
         # Example: CREATE ROLE bob WITH PASSWORD = 'password_b'AND LOGIN = true AND SUPERUSER = true;
         # Example: CREATE ROLE carlos WITH OPTIONS = {'custom_option1': 'option1_value', 'custom_option2': 99};
         role_options = {}
@@ -314,6 +326,7 @@ class Role(UserRoleBase):
             logger.debug('CREATE role query: {}'.format(query))
         self.session.execute(query)
         logger.debug('Role "{}" has been created'.format(self.name))
+        return self
 
 
 class User(UserRoleBase):
@@ -323,7 +336,7 @@ class User(UserRoleBase):
     def __init__(self, session, name, password=None, superuser=None, verbose=True):
         super(User, self).__init__(session, name, password, superuser, verbose)
 
-    def create(self):
+    def create(self) -> "User":
         user_options_str = '{password}{superuser}'.format(password=' PASSWORD \'{}\''
                                                           .format(self.password) if self.password else '',
                                                           superuser='' if self.superuser is None else ' SUPERUSER'
@@ -337,3 +350,4 @@ class User(UserRoleBase):
 
         self.session.execute(query)
         logger.debug('User "{}" has been created'.format(self.name))
+        return self
