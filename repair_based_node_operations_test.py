641f6b47efc451bbafcd0e6dc194443b37c7185e
Julia Yakovlev <julia.yakovlev@scylladb.com>
2022-03-24 15:39:06 +0200

    test(repair based node): add test for --ignore-dead-nodes-for-replace parameter
    New --ignore-dead-nodes-for-replace parameter allows to ignore dead nodes
    for replace operation.
    
    Commit:
    https://github.com/scylladb/scylla/commit/eba4a4fba4e6f203742307c16448034f40713711
    
    Task:
    https://trello.com/c/4NrxIRaP/4792-9757-a-replace-operation-in-repair-based-node-operations-mode-
    will-now-ignore-dead-nodes-allowing-replacement-of-multiple-dead-n

--- repair_based_node_operations_test.py
+++ repair_based_node_operations_test.py
@@ -40,6 +40,14 @@ class RepairBasedNodeOperationsScenarios:
                                              operation_name="replace",  # used for validation in the log
                                              repair_on_tested_node=True,
                                              repair_on_all_nodes=False)
+
+        # replace:
+        # replace with allow ignoring dead nodes:
+        # Three nodes are dead. Replacing node is allowed when there are two dead nodes.
+        self.replace_with_dead_nodes_scenario = RBNOperation(operation=self.replace_node_when_two_nodes_dead,
+                                                             operation_name="replace",  # used for validation in the log
+                                                             repair_on_tested_node=True,
+                                                             repair_on_all_nodes=False)
         # bootstrap:
         # It is used to add a new node into the cluster. The token ring changes.
         # New node pulls data from existing nodes that are losing the token ranges.
@@ -73,11 +81,16 @@ class RepairBasedNodeOperationsScenarios:
         self.operations_flow = [self.rebuild_scenario, self.removenode_scenario,
                                 self.bootstrap_scenario, self.replace_scenario, self.decommission_scenario]
 
-    @retrying(num_attempts=5, message="Failed to add new node. Retrying..")
-    def add_node(self, dc: int = 0, replace_address: str = None, is_seed: bool = False) -> ScyllaNode:
+    def add_node(self, dc: int = 0, replace_address: str = None, is_seed: bool = False,
+                 ignore_dead_node_ip: str = '') -> ScyllaNode:
         new_node_index = int(self.tester.cluster.nodelist()[-1].name.replace("node", "")) + 1
         new_node = self.tester.cluster.new_node(i=new_node_index, data_center=dc, is_seed=is_seed)
-        new_node.start(wait_for_binary_proto=True, wait_other_notice=True, jvm_args=self.tester.jvm_args,
+
+        jvm_args = self.tester.jvm_args
+        if ignore_dead_node_ip:
+            jvm_args.extend(['--ignore-dead-nodes-for-replace', ignore_dead_node_ip])
+
+        new_node.start(wait_for_binary_proto=True, wait_other_notice=True, jvm_args=jvm_args,
                        replace_address=replace_address)
         return new_node
 
@@ -92,6 +105,28 @@ class RepairBasedNodeOperationsScenarios:
         logger.debug(f"Added new node {new_node.name} ({new_node.address()})")
 
         self.tester.cluster.remove(node=replaced_node, wait_other_notice=True, remove_node_dir=False)
+
+        return new_node
+
+    def replace_node_when_two_nodes_dead(self, dc: int = 0) -> ScyllaNode:
+        replaced_node, dead_node1, dead_node2 = self.tester.cluster.nodelist()[-3:]
+        replaced_node_address = replaced_node.address()
+        dead_node1_address = dead_node1.address()
+        dead_node2_address = dead_node2.address()
+        logger.debug(f"Stop 3 nodes: {replaced_node_address}, {dead_node1_address}, {dead_node2_address}")
+        for node in [replaced_node, dead_node1, dead_node2]:
+            node.stop(wait_other_notice=True)
+
+        logger.debug(f"Add a new node that replaces {replaced_node_address}")
+        new_node = self.add_node(replace_address=replaced_node_address, dc=dc,
+                                 ignore_dead_node_ip=f"{dead_node1_address},{dead_node2_address}")
+        logger.debug(f"Added the new node {new_node.name} ({new_node.address()})")
+
+        self.tester.cluster.remove(node=replaced_node, wait_other_notice=True, remove_node_dir=False)
+
+        logger.debug(f"Start {dead_node1_address} and {dead_node2_address} nodes")
+        for node in [dead_node1, dead_node2]:
+            node.start(wait_other_notice=True, wait_for_binary_proto=True)
 
         return new_node
 
@@ -152,7 +187,7 @@ class RepairBasedNodeOperationsScenarios:
             for node in self.tester.cluster.nodelist():
                 if node == tested_node:
                     # If test runs with --enable-repair-based-node-ops is false, default behaviour will be changed and
-                    # repair by node won't started for any operation even for default "replace" operation
+                    # repair by node won't be started for any operation even for default "replace" operation
                     repair_expected = rbno_enabled if not rbno_enabled else scenario.repair_on_tested_node
                     self.validate_repair_by_scenario(node=tested_node, repair_expected=repair_expected,
                                                      search_string=search_string)
@@ -296,3 +331,19 @@ class TestRepairBasedNodeOperations(Tester):
         rbnos.run_scenarios(rbno_enabled=enable_repair_based_node_ops,
                             lcs=True,
                             scenarios=[rbnos.bootstrap_scenario, rbnos.replace_scenario])
+
+    def test_ignore_dead_nodes_for_replace_option(self):
+        """
+        --ignore-dead-nodes-for-replace was added by commit
+        https://github.com/scylladb/scylla/commit/eba4a4fba4e6f203742307c16448034f40713711
+
+        This option allows ignoring dead nodes for replace operation.
+        If this option is not set, replace operation will fail because one node is down.
+        """
+        enable_repair_based_node_ops = True
+        self.prepare_cluster(nodes=5, enable_repair_based_node_ops=enable_repair_based_node_ops)
+        self.prepare_schema(node=self.cluster.nodelist()[0])
+
+        rbnos = RepairBasedNodeOperationsScenarios(tester=self)
+        rbnos.run_scenarios(rbno_enabled=enable_repair_based_node_ops,
+                            scenarios=[rbnos.replace_with_dead_nodes_scenario])
