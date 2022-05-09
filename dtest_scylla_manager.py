1dd41784faf2e56e329866e8c3737b8bc489655e
Israel Fruchter <fruch@scylladb.com>
2022-05-09 11:31:05 +0300

    dtest_scylla_manager: remove Version usage
    since manager versions are not needed anymore.
    master dtest support version 3.0 and up.
    
    removeing all checks related to previus versions

--- dtest_scylla_manager.py
+++ dtest_scylla_manager.py
@@ -17,7 +17,6 @@ from dtest_class import wait_for, WaitTimeoutExpired, create_ks, create_cf
 from tools.data import insert_c1c2, insert_c1c2_with_clustering
 from dtest_config import DTestConfig
 from dtest_setup import DTestSetup, copy_logs
-from packaging.version import Version
 from dtest_setup_overrides import DTestSetupOverrides
 
 logger = logging.getLogger(__name__)
@@ -25,9 +24,6 @@ logger = logging.getLogger(__name__)
 SPACE_PLACEHOLDER = r"SPACE"
 C1_PREFIX = "value%d"
 C2_PREFIX = "other_value%d"
-
-
-new_command_structure_minimum_version = Version("3.0")
 
 
 class ComparableHealthCheckField:
@@ -773,8 +769,7 @@ class ScyllaManagerTool(ScyllaManagerBase):
         cluster_add_cmd = "cluster add --host {host} --name {name}".format(**locals())
         versions, _ = self.version
         client_version = versions[0][0].split()[2]
-        if Version(client_version) >= Version('2.0'):
-            cluster_add_cmd += " --auth-token {}".format(node.scylla_manager.auth_token)
+        cluster_add_cmd += " --auth-token {}".format(node.scylla_manager.auth_token)
         res_cluster_add, stderr = self.sctool.run(cmd=cluster_add_cmd)
         if not res_cluster_add or 'Cluster added' not in stderr:
             raise ScyllaManagerError(
