7c25296248f821a9b70d39cb2c45ba4f8f64f3ba
Israel Fruchter <fruch@scylladb.com>
2022-08-03 15:02:42 +0300

    concurrent_schema_changes_test: update test_changes_while_node_toggle
    - revert the usage of `cluster.start` so the order of starting nodes
      is set by the test and not by CCM implemetion.
    - remove unneded sleep, replace then with setting the `wait_for_*`
      arguments for node start()/stop()

--- concurrent_schema_changes_test.py
+++ concurrent_schema_changes_test.py
@@ -442,20 +442,20 @@ class TestConcurrentSchemaChanges(Tester):
         logger.debug("changes_while_node_toggle_test()")
         cluster = self.cluster
 
-        cluster.populate(2).start()
+        cluster.populate(2).start(wait_other_notice=True, wait_for_binary_proto=True)
         node1, node2 = cluster.nodelist()
-        wait(2)
-        session = self.patient_cql_connection(node2)
+        session = self.patient_exclusive_cql_connection(node2)
 
         self.prepare_for_changes(session, namespace='ns2')
-        node1.stop()
-        wait(2)
+        node1.stop(wait_other_notice=True)
+
         fixture_dtest_setup.ignore_log_patterns += [r'Column .* in view .* was not found in the base table']
         self.make_schema_changes(session, namespace='ns2')
-        wait(2)
-        node2.stop()
-        wait(2)
-        cluster.start(wait_other_notice=True, wait_for_binary_proto=True)
+
+        node2.stop(wait_other_notice=True)
+
+        node1.start(wait_other_notice=True, wait_for_binary_proto=True)
+        node2.start(wait_other_notice=True, wait_for_binary_proto=True)
         self.validate_schema_consistent(node1)
 
     @pytest.mark.next_gating
