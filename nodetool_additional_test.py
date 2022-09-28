temichus/scylladb

commit a3356e866b093a8655cc9c3b3e6c8a68e3b3a6cc

cql: add max window restriction for TimeWindowCompactionStrategy
The number of potential compaction windows (or buckets) is defined by the default_time_to_live / sstable_window_size ratio. Every now and then we end up in a situation on where users of TWCS end up underestimating their window buckets when using TWCS. Unfortunately, scenarios on which one employs a default_time_to_live setting of 1 year but a window size of 30 minutes are not rare enough.

Such configuration is known to only make harm to a workload: As more and more windows are created, the number of SSTables will grow in the same pace, and the situation will only get worse as the number of shards increase.

This commit introduces the twcs_max_window_count option, which defaults to 50, and will forbid the Creation or Alter of tables which get past this threshold. A value of 0 will explicitly skip this check.

Note: this option does not forbid the creation of tables with a default_time_to_live=0 as - even though not recommended - it is perfectly possible for a TWCS table with default TTL=0 to have a bound window, provided any ingestion statements make use of 'USING TTL' within the CQL statement, in addition to it.

cql3/statements/create_table_statement.cc
@@ -30,6 +30,7 @@
 #include "service/migration_manager.hh"
 #include "service/storage_proxy.hh"
 #include "db/config.hh"
+#include "compaction/time_window_compaction_strategy.hh"
 
 namespace cql3 {
 
@@ -435,6 +436,19 @@ std::optional<sstring> check_restricted_table_properties(
     // function before cfprops.validate() (there, validate() is only called
     // in prepare_schema_mutations(), in the middle of execute).
     auto strategy = cfprops.get_compaction_strategy_class();
+    sstables::compaction_strategy_type current_strategy = sstables::compaction_strategy_type::null;
+    gc_clock::duration current_ttl = gc_clock::duration::zero();
+    // cfprops doesn't return any of the table attributes unless the attribute
+    // has been specified in the CQL statement. If a schema is defined, then
+    // this was an ALTER TABLE statement.
+    if (schema) {
+        current_strategy = (*schema)->compaction_strategy();
+        current_ttl = (*schema)->default_time_to_live();
+    }
+
+    // Evaluate whether the strategy to evaluate was explicitly passed
+    auto cs = (strategy) ? strategy : current_strategy;
+
     if (strategy && *strategy == sstables::compaction_strategy_type::date_tiered) {
         switch(qp.db().get_config().restrict_dtcs()) {
         case db::tri_mode_restriction_t::mode::TRUE:
@@ -453,6 +467,31 @@ std::optional<sstring> check_restricted_table_properties(
             break;
         }
     }
+    if (cs == sstables::compaction_strategy_type::time_window) {
+        std::map<sstring, sstring> options = (strategy) ? cfprops.get_compaction_type_options() : (*schema)->compaction_strategy_options();
+        sstables::time_window_compaction_strategy_options twcs_options(options);
+        long ttl = (cfprops.has_property(cf_prop_defs::KW_DEFAULT_TIME_TO_LIVE)) ? cfprops.get_default_time_to_live() : current_ttl.count();
+        auto max_windows = qp.db().get_config().twcs_max_window_count();
+
+        // It may happen that an user tries to update an unrelated table property. Allow the request through.
+        if (!cfprops.has_property(cf_prop_defs::KW_DEFAULT_TIME_TO_LIVE) && !strategy) {
+            return std::nullopt;
+        }
+
+        if (ttl > 0) {
+            // Ideally we should not need the window_size check below. However, given #2336 it may happen that some incorrectly
+            // table created with a window_size=0 may exist, which would cause a division by zero (eg: in an ALTER statement).
+            // Given that, an invalid window size is treated as 1 minute, which is the smaller "supported" window size for TWCS.
+            auto window_size = twcs_options.get_sstable_window_size() > std::chrono::seconds::zero() ? twcs_options.get_sstable_window_size() : std::chrono::seconds(60);
+            auto window_count = std::chrono::seconds(ttl) / window_size;
+            if (max_windows > 0 && window_count > max_windows) {
+                throw exceptions::configuration_exception(fmt::format("The setting of default_time_to_live={} and compaction window={}(s) "
+                                                   "can lead to {} windows, which is larger than the allowed number of windows specified "
+                                                   "by the twcs_max_window_count ({}) parameter. Note that default_time_to_live=0 is also "
+                                                   "highly discouraged.", ttl, twcs_options.get_sstable_window_size().count(), window_count, max_windows));
+            }
+        }
+   }
     return std::nullopt;
 }
 

db/config.cc
@@ -840,6 +840,8 @@ db::config::config(std::shared_ptr<db::extensions> exts)
     , max_memory_for_unlimited_query_hard_limit(this, "max_memory_for_unlimited_query_hard_limit", "max_memory_for_unlimited_query", liveness::LiveUpdate, value_status::Used, (uint64_t(100) << 20),
             "Maximum amount of memory a query, whose memory consumption is not naturally limited, is allowed to consume, e.g. non-paged and reverse queries. "
             "This is the hard limit, queries violating this limit will be aborted.")
+    , twcs_max_window_count(this, "twcs_max_window_count", liveness::LiveUpdate, value_status::Used, 50,
+            "The maximum number of compaction windows allowed when making use of TimeWindowCompactionStrategy. A setting of 0 effectively disables the restriction.")
     , initial_sstable_loading_concurrency(this, "initial_sstable_loading_concurrency", value_status::Used, 4u,
             "Maximum amount of sstables to load in parallel during initialization. A higher number can lead to more memory consumption. You should not need to touch this")
     , enable_3_1_0_compatibility_mode(this, "enable_3_1_0_compatibility_mode", value_status::Used, false,

db/config.hh
@@ -335,6 +335,7 @@ public:
     named_value<uint32_t> max_clustering_key_restrictions_per_query;
     named_value<uint64_t> max_memory_for_unlimited_query_soft_limit;
     named_value<uint64_t> max_memory_for_unlimited_query_hard_limit;
+    named_value<uint32_t> twcs_max_window_count;
     named_value<unsigned> initial_sstable_loading_concurrency;
     named_value<bool> enable_3_1_0_compatibility_mode;
     named_value<bool> enable_user_defined_functions;

commit 02a072e681a2724d3ff44e9ee59f95173dc8b019

nodetool_additional_test: test_resetlocalschema_api_issue_7811: support both scylla success and failure scenarios
As discussed on https://github.com/scylladb/scylladb/issues/11643,
scylladb/scylladb@a3356e866b093a8655cc9c3b3e6c8a68e3b3a6cc
inadvertently fixed https://github.com/scylladb/scylladb/issues/7811
for TimeWindowCompactionStrategy with the additional compaction options
validation, but it didn't do so for other strategies.

This change parameterizes the test to both 'TimeWindowCompactionStrategy'
and 'SizeTieredCompactionStrategy', and adds code to debug print the
exceptions and verify their content.

Then it validates the additional schema version only
if recreation attempt failed on scylladb/scylladb#7811.

Closes scylladb/scylladb#11643

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

nodetool_additional_test.py
@@ -1289,9 +1289,9 @@ class TestNodetool(Tester):
         logger.debug("Schema version has been recalculated")
 
     # TODO: This test should be removed when issue #7811 will be fixed
-    def test_resetlocalschema_api_issue_7811(self):
-        self.ignore_log_patterns += ['Invalid window unit NOPE for compaction_window_unit',
-                                     'find a column family with UUID']
+    @pytest.mark.parametrize("strategy", ['TimeWindowCompactionStrategy', 'SizeTieredCompactionStrategy'])
+    def test_resetlocalschema_api_issue_7811(self, strategy):
+        self.ignore_log_patterns += ['find a column family with UUID']
         cluster = self.cluster
         cluster.populate(2).start(wait_for_binary_proto=True)
         node1, node2 = cluster.nodelist()
@@ -1299,23 +1299,49 @@ class TestNodetool(Tester):
 
         create_ks(session=session, name='ks', rf=2)
 
+        compaction_class = {'class': strategy}
+        if strategy == 'TimeWindowCompactionStrategy':
+            invalid_compaction_option = 'compaction_window_unit'
+            expected_error = 'Invalid window unit NOPE for compaction_window_unit'
+        elif strategy == 'SizeTieredCompactionStrategy':
+            invalid_compaction_option = 'min_sstable_size'
+            expected_error = "Invalid long value NOPE for 'min_sstable_size'"
+        self.ignore_log_patterns.append(expected_error)
+
+        compaction_options = compaction_class | {invalid_compaction_option: 'NOPE'}
+        logger.debug(f"Creating ks.cf with invalid compaction_options: {compaction_options}")
         try:
             create_cf(session=session, name='cf',
-                      compaction={'class': 'TimeWindowCompactionStrategy', 'compaction_window_unit': 'NOPE'})
-        except Exception:
-            pass
-
-        self._verify_nodes_schema_versions(node1, 1)
-
-        try:
-            create_cf(session=session, name='cf', compaction={'class': 'TimeWindowCompactionStrategy'})
+                      compaction=compaction_options)
+            pytest.fail("Creating ks.cf with invalid compaction option was expected to fail")
         except Exception as exc:
-            # Issue https://github.com/scylladb/scylla/issues/7811
+            logger.debug(f"Got exception: {exc}")
+            assert expected_error in str(exc), f"Got unexpected exception: {exc}"
             pass
 
         self._verify_nodes_schema_versions(node1, 1)
 
         log_position = node2.mark_log()
+        logger.debug("Recreating ks.cf")
+        recreate_failed = False
+        try:
+            create_cf(session=session, name='cf', compaction=compaction_class)
+            logger.debug(f"Creating ks.cf succeeded. Issue #7811 might have been fixed.")
+        except Exception as exc:
+            # Issue https://github.com/scylladb/scylla/issues/7811
+            logger.debug(f"Got exception: {exc}")
+            assert "Can't find a column family with UUID" in str(exc), f"Got unexpected exception: {exc}"
+            recreate_failed = True
+            pass
+
+        if not recreate_failed:
+            assert node2.watch_log_for("schema_tables - Schema version changed to", from_mark=log_position, timeout=10), \
+                "Schema recalculation was not performed"
+
+        self._verify_nodes_schema_versions(node1, 1)
+
+        log_position = node2.mark_log()
+        logger.debug("Calling relocal_schema api")
         self.send_storage_restful_api(node2, 'relocal_schema')
 
         assert node2.watch_log_for("schema_tables - Schema version changed to", from_mark=log_position, timeout=10), \
@@ -1323,14 +1349,17 @@ class TestNodetool(Tester):
 
         # After schema recalculation the node, where nodetool was run, will get new schema version,
         # different from another node
-        self._verify_nodes_schema_versions(node1, 2)
+        if recreate_failed:
+            self._verify_nodes_schema_versions(node1, 2)
 
-        # By Tomasz's explanation: after 60 sec. the other node should pull and have the same schema version
-        logger.debug("Sleep for 60 sec: the other node should pull new version")
-        time.sleep(60)
-        self._verify_nodes_schema_versions(node1, 1)
+            # By Tomasz's explanation: after 60 sec. the other node should pull and have the same schema version
+            logger.debug("Sleep for 60 sec: the other node should pull new version")
+            time.sleep(60)
+            self._verify_nodes_schema_versions(node1, 1)
 
-        logger.debug("Schema version has been recalculated")
+            logger.debug("Schema version has been recalculated")
+        else:
+            self._verify_nodes_schema_versions(node1, 1)
 
     @pytest.mark.require("scylla-tools-java:#226")
     def test_resetlocalschema_nodetool(self):