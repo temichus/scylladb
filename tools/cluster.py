6bad0a54dc9f0571ee14666ddc3fd94673ef7781
Benny Halevy <bhalevy@scylladb.com>
2022-11-03 11:35:57 +0200

    run_rest_api: make the api_method case insensitive
    In many cases (like curl) we usually refer to the methods
    in upper case, like POST or GET, so allow that too.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- tools/cluster.py
+++ tools/cluster.py
@@ -58,6 +58,7 @@ def run_rest_api(run_on_node: ScyllaNode, cmd, api_method: str = 'post'):
     """
     cmd_prefix = f"http://{run_on_node.address()}:10000"
     full_cmd = cmd_prefix + cmd
+    api_method = api_method.lower()
     logger.debug(f"Send restful api: {full_cmd}")
     if api_method == 'post':
         result = requests.post(full_cmd)
