80c34c68c6de532fb8467759c768e922576161f5
Israel Fruchter <fruch@scylladb.com>
2022-11-22 11:16:11 +0200

    artifact.groovy: handle debug mode correctly
    seem like 56b7260 didn't handled the debug mode correctly
    to download the debug unified package
    
    Fix: #2914

--- vars/artifact.groovy
+++ vars/artifact.groovy
@@ -83,7 +83,7 @@ def getUnifiedRelocArtifact (String cloudUrl, String buildMode) {
     unifiedPackageName = unifiedPackageName.substring(unifiedPackageName.lastIndexOf('/') + 1)
 
     if (buildMode == "debug") {
-        unifiedPackageName.replace('scylla-', 'scylla-debug-')
+        unifiedPackageName = unifiedPackageName.replace("${params.PRODUCT_NAME}-", "${params.PRODUCT_NAME}-debug-")
     }
 
 	target = "$WORKSPACE/${params.PRODUCT_NAME}/build/${buildMode}/dist/tar"
