ddbee17e6db2f59ceec81b12d9463e018d6808dc
Shlomi Balalis <Balalis@scylladb.com>
2022-12-07 13:25:26 +0200

    fix(test_backup_nonexistent_bucket): Altered expected error message
    Due to recent changes in manager (between 3.0 and 3.1), the error message of a backup
    command that uses a nonexistent bucket has changed. I altered the test accordingly.

--- manager_backup_tests.py
+++ manager_backup_tests.py
@@ -315,7 +315,7 @@ class TestScyllaMgmtBackup(Tester, ScyllaManagerMixin):
         try:
             mgr_cluster.run_backup_command(location_list=["s3:{}".format(FALSE_BUCKET)])
         except ScyllaManagerError as err:
-            assert "location is not accessible" in err.args[0] and "404 Not Found" in err.args[0],\
+            assert "specified bucket does not exist" in err.args[0],\
                 "Unexpected error: {}".format(err.args[0])
         else:
             raise ScyllaManagerError("No error occurred when a nonexistent bucket was used as a location"
