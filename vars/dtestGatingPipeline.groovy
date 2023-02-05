34b50bfd17e21924875464373d0a2ddfd44b1109
Yaron Kaikov <yaronkaikov@scylladb.com>
2023-02-05 10:59:01 +0200

    Create dtestGatingPipeline.groovy for dtest gating
    For gating we need to be able to run multiple builds at the same time, since in dtestPipeline.groovy we have disableConcurrentBuilds() configured, we had to duplicate the file.
    Also removed RUN_DTEST_HEAVY and RUN_DTEST_LONG code since they are not needed for gating

---
+++ vars/dtestGatingPipeline.groovy
@@ -0,0 +1,142 @@
+#!groovy
+/*
+   For gating we need to be able to run multiple builds at the same time, since in dtestPipeline.groovy we have disableConcurrentBuilds() configured, we had to duplicate the file.
+   Also removed RUN_DTEST_HEAVY and RUN_DTEST_LONG code since they are not needed for gating
+*/
+def call(Map pipelineParams) {
+    pipeline {
+        parameters {
+            booleanParam(name: 'RUN_DTEST_FULL', defaultValue: true, description: 'Uncheck this to skip FullDtest, when running in parallel mode only!.')
+
+            string(name: 'SPLIT_FLEET_LABEL', defaultValue: '', description: 'On which spot instance fleet to run the parallel jobs. default: ec2-asg-strong-dtest-spot')
+            string(name: 'SPLIT_TIME_TARGET', defaultValue: "${pipelineParams.get('SPLIT_TIME_TARGET', '240')}", description: 'Time period (minutes) for a test group to run. Used to calculate the needed number of spot machines')
+            string(name: 'SPLIT_MAX_NODES', defaultValue: '100', description: 'Maximum number of nodes to run tests on parallel.')
+            string(name: 'BRANCH', defaultValue: "${pipelineParams.get('BRANCH', 'master')}", description: 'Choose: master|branch-4.4')
+            string(name: 'PRODUCT_NAME', defaultValue: "${pipelineParams.get('PRODUCT_NAME', 'scylla')}", description: 'Choose: scylla|scylla-enterprise')
+
+            //Not mandatory
+            string(name: 'TIMEOUT_PARAM', defaultValue: "${pipelineParams.get('TIMEOUT_PARAM', '4')}", description: 'hours. This time includes the time needed to wait for local machines. Could be much less for cloud machines.')
+            string(name: 'BUILD_MODE', defaultValue: "${pipelineParams.get('BUILD_MODE', 'release')}", description: 'Choose: dev|release|debug, If empty, default to release')
+            string(name: 'ARTIFACT_SOURCE_JOB_NAME', defaultValue: '', description: 'Build path to take Relocatable data from')
+            string(name: 'ARTIFACT_SOURCE_BUILD_NUM', defaultValue: '', description: 'Build ID to take relocatable package files from. Leave empty to use last success build.')
+            string(name: 'ARTIFACT_WEB_URL', defaultValue: 'latest', description: 'URL to take reloc items from. Use when reloc is not available on jenkins, or when running on AWS, which will download faster from S3.')
+            booleanParam(name: 'DTEST_DEBUG_INFO', defaultValue: false, description: 'Check this to print debug to stdout when running dtest')
+            booleanParam(name: 'DTEST_KEEP_LOGS', defaultValue: false, description: 'Check this, to keep dtest logs')
+            string(name: 'INCLUDE_DTESTS', defaultValue: "${pipelineParams.get('INCLUDE_DTESTS', '-m next_gating')}" , description: """Specify which dtests to run. default for release:
+                                                                 -m 'not skip and dtest_full and not dtest_heavy and not dtest_long',
+                                                                 for debug: -m dtest_debug """)
+            string(name: 'PYTEST_EXTRA_COMMANDLINE_OPTIONS', defaultValue: "${pipelineParams.get('PYTEST_EXTRA_COMMANDLINE_OPTIONS', '')}" , description: """ String
+                with pytest extra CLI parameters, ex: --experimental-features=raft,cdc """)
+            string(name: 'EXCLUDE_DTESTS', defaultValue: '', description: 'Specify dtests to exclude.')
+            string(name: 'SCYLLA_EXT_OPTS_EXTRA_SETTINGS', defaultValue: '--abort-on-seastar-bad-alloc --abort-on-lsa-bad-alloc=1', description: 'Anything you put here will be added to any default settings of SCYLLA_EXT_OPTS env var sent to nose.')
+            string(name: 'SCYLLA_EXT_ENV_EXTRA_SETTINGS', defaultValue: 'ASAN_OPTIONS=disable_coredump=0:abort_on_error=1;UBSAN_OPTIONS=halt_on_error=1:abort_on_error=1;BOOST_TEST_CATCH_SYSTEM_ERRORS=no', description: 'Anything you put here will be added to any default settings of SCYLLA_EXT_ENV env var sent to nose.')
+            string(name: 'SCYLLA_DTEST_REPO', defaultValue: '', description: '')
+            string(name: 'SCYLLA_DTEST_BRANCH', defaultValue: '', description: '')
+            string(name: 'SCYLLA_CCM_REPO', defaultValue: '', description: '')
+            string(name: 'SCYLLA_CCM_BRANCH', defaultValue: '', description: '')
+            booleanParam(name: 'PRESERVE_WORKSPACE', defaultValue: false, description: 'Check this if you need the workspace to remain (for debug)')
+            booleanParam(name: 'DRY_RUN', defaultValue: false, description: 'Check this to check pipeline syntax. will not perform anything.')
+
+            string(name: 'DRIVER_VERSION', defaultValue: "${pipelineParams.get('DRIVER_VERSION', '')}", description: 'driver version to use during the tests, ex. scylla-driver==3.25.4')
+        }
+
+        agent {
+            label generalProperties.targetDtestBuilder
+        }
+
+        options {
+            timeout(time: params.TIMEOUT_PARAM, unit: 'HOURS')
+            buildDiscarder(logRotator(numToKeepStr: '200'))
+        }
+
+        environment {
+            AWS_ACCESS_KEY_ID     = credentials('jenkins2-aws-secret-key-id')
+            AWS_SECRET_ACCESS_KEY = credentials('jenkins2-aws-secret-access-key')
+            GITHUB_TOKEN = credentials('github-api-access-token')
+        }
+
+        stages {
+            stage ('Prepare') {
+                steps {
+                    script {
+                        lastStage = env.STAGE_NAME
+                        baseRelocJob = params.RELOC_JOB_NAME ?: "next"
+                        buildMode = params.BUILD_MODE
+                        excludeTests = params.EXCLUDE_DTESTS ?: ""
+                        includeDtests = params.INCLUDE_DTESTS ?: "-m 'not skip and dtest_full and not dtest_heavy and not dtest_long'"
+
+                        splitMaxNodesForHeavyAndLong = "10"
+
+                        echo "Build mode upon parameter |${params.BUILD_MODE}| or upon job name |${JOB_NAME}|: |${buildMode}|"
+                    }
+                }
+            }
+
+            stage('FullDtest') {
+                when {
+                    expression { params.RUN_DTEST_FULL }
+                }
+                steps {
+                    script {
+                        runDtest (params.SPLIT_MAX_NODES, includeDtests, "full", params.SPLIT_FLEET_LABEL,
+                            params.SPLIT_TIME_TARGET)
+                    }
+                }
+            }
+        }
+
+        post {
+            //Order is: always, changed, fixed, regression, aborted, failure, success, unstable, and cleanup.
+            always {
+                script {
+                    jenkins.isSpotTermination(lastStage)
+                    jenkins.cleanWorkSpaceUponRequest(params.PRESERVE_WORKSPACE)
+                }
+            }
+        }
+    }
+}
+
+def runDtest(String splitMaxNodes, String includeDtestsTag, String dtestType, String splitFleetLabel, String splitTimeTarget) {
+    echo "runDtest"
+    lastStage = env.STAGE_NAME
+    dtest.prepareDtestLocalTree (
+        preserveWorkspace: params.PRESERVE_WORKSPACE,
+        dtestBranch: params.SCYLLA_DTEST_BRANCH,
+        dtestRepo: params.SCYLLA_DTEST_REPO,
+        ccmBranch: params.SCYLLA_CCM_BRANCH,
+        ccmRepo: params.SCYLLA_CCM_REPO,
+        relocWebUrl: params.ARTIFACT_WEB_URL,
+        baseRelocJob: baseRelocJob,
+        relocBuildID: params.RELOC_BUILD_ID,
+        buildMode: buildMode,
+    )
+    numOfSplitFiles = dtest.splitAndCopyDtestJobs (
+        splitTimeTarget: splitTimeTarget,
+        splitMaxNodes: splitMaxNodes,
+        buildMode: buildMode,
+        includeTests: includeDtestsTag,
+        excludeTests: excludeTests,
+        dtestType: dtestType,
+    )
+    dtest.doParallelDtest(
+        dryRun: params.DRY_RUN,
+        dtestMode: buildMode,
+        downloadfromCloud: true,
+        cloudUrl: params.ARTIFACT_WEB_URL,
+        dtestDebugInfoFlag: params.DTEST_DEBUG_INFO,
+        dtestKeepLogsFlag: params.DTEST_KEEP_LOGS,
+        extOpts: params.SCYLLA_EXT_OPTS_EXTRA_SETTINGS,
+        extEnv: params.SCYLLA_EXT_ENV_EXTRA_SETTINGS,
+        numOfSplitFiles: numOfSplitFiles,
+        runningUserID: jenkins.getRunningUserInfo().userId,
+        dtestRepo: params.SCYLLA_DTEST_REPO,
+        dtestBranch: params.SCYLLA_DTEST_BRANCH,
+        ccmBranch: params.SCYLLA_CCM_BRANCH,
+        ccmRepo: params.SCYLLA_CCM_REPO,
+        splitFleetLabel: splitFleetLabel,
+        dtestType: dtestType,
+        driverVersion: params.DRIVER_VERSION,
+        pyTestExtraCLIOptions: params.PYTEST_EXTRA_COMMANDLINE_OPTIONS,
+    )
+}
