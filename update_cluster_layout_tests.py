5c86edab317fbaba2d5586d4d6bb3e7e2b42b1ae
Artsiom Mishuta <artsiom.mishuta@scylladb.com>
2023-02-15 15:31:22 +0100

    Update test cases after fix 11302
    list of updated tests:
    test_decommission_node_while_gossip_partly_blocked

--- update_cluster_layout_tests.py
+++ update_cluster_layout_tests.py
@@ -3,6 +3,7 @@ import os
 import logging
 import collections
 import random
+import traceback
 import re
 
 from datetime import datetime
@@ -2335,7 +2336,6 @@ class TestUpdateClusterLayout(Tester):
         except AssertionError as ex:
             logger.warning(f'chain already delete: {ex}')
 
-    @pytest.mark.require("scylladb/scylladb#11302")
     def test_decommission_node_while_gossip_partly_blocked(self, ip_tables):
         """ reproducer scylladb/scylla-operator#982 and scylladb/scylladb#11302
 
@@ -2366,52 +2366,20 @@ class TestUpdateClusterLayout(Tester):
             logger.info(stdout)
             logger.info(stderr)
 
+        logger.info("try to decommission node3")
+        try:
+            node3.decommission()
+        except NodetoolError as exc:
+            logger.info(traceback.format_exc())
+            assert "Rejected decommission operation" in repr(exc)
+        else:
+            raise AssertionError("decommission must fail")
+
+        logger.info("resume gossip communication")
+        ip_tables.delete_chain()
+
         logger.info("decommission node3")
         retry_till_success(node3.decommission, timeout=120)
-
-        logger.info("resume gossip communication")
-        ip_tables.delete_chain()
-
-        logger.info("add new node4")
-        node4 = cluster.new_node(4)
-        node4.start(wait_for_binary_proto=True)
-        logger.info("done")
-
-    @pytest.mark.require("scylladb/scylladb#11302")
-    def test_removenode_while_gossip_partly_blocked(self, ip_tables):
-        """
-            restart and remove a node while other nodes can't send gossip communication to it
-        """
-        logger.info("populating cluster with three nodes")
-        cluster = self.cluster
-        cluster.populate(3)
-        logger.info("starting cluster")
-        cluster.start(wait_for_binary_proto=True, wait_other_notice=True)
-
-        logger.info("stopping node3")
-        node1, node2, node3 = cluster.nodelist()
-        node3.stop(gently=False)
-
-        logger.info("block gossip communication to node3")
-        node1_ip_address = get_ip_from_node(node=node1)
-        node2_ip_address = get_ip_from_node(node=node2)
-        rule1 = IPTableRule(protocol='tcp', source=f'{node1_ip_address}/32', destination_port=7000, target='DROP')
-        rule2 = IPTableRule(protocol='tcp', source=f'{node2_ip_address}/32', destination_port=7000, target='DROP')
-        ip_tables.add_rule(rule1)
-        ip_tables.add_rule(rule2)
-
-        logger.info("start node3")
-        node3.start(wait_for_binary_proto=True, wait_other_notice=False)
-        for n in cluster.nodelist():
-            stdout, stderr = n.nodetool("status")
-            logger.info(stdout)
-            logger.info(stderr)
-
-        logger.info("remove node node3")
-        retry_till_success(node2.nodetool, f'removenode {node3.hostid()}', timeout=120)
-
-        logger.info("resume gossip communication")
-        ip_tables.delete_chain()
 
         logger.info("add new node4")
         node4 = cluster.new_node(4)
