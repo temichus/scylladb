c516eb31d82b5ad54840e3cb5f8edc7d0394048e
Benny Halevy <bhalevy@scylladb.com>
2023-02-26 19:40:10 +0200

    test_cdc_large_values: s/-/_/ in max_memory_for_unlimited_query config option
    Otherwise:
    ```
    WARN  2023-02-26 17:22:55,704 [shard 0] init - Unknown option : max-memory-for-unlimited-query
    ```
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- test_cdc_large_values.py
+++ test_cdc_large_values.py
@@ -27,7 +27,7 @@ class TestLargeColumnsWithCDC(Tester, CDCInitializeHelper):
     def fixture_dtest_setup_overrides(self, dtest_config):
         dtest_setup_overrides = DTestSetupOverrides()
         dtest_setup_overrides.cluster_options = ImmutableMapping({'max_memory_for_unlimited_query_soft_limit': 20 * MB,
-                                                                  'max-memory-for-unlimited-query': 20 * MB,
+                                                                  'max_memory_for_unlimited_query': 20 * MB,
                                                                   'compaction_large_row_warning_threshold_mb': 20 * MB,
                                                                   'compaction_large_cell_warning_threshold_mb': 20 * MB})
         return dtest_setup_overrides
