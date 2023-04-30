1d14ae30d4dcac1d5684bae675b9cc218d990350
Israel Fruchter <fruch@scylladb.com>
2023-04-30 18:56:33 +0300

    encryption_at_rest_test: remove in_memory tests
    enterprise doesn't support in_memory feature anymore
    those tests are not needed.

--- encryption_at_rest_test.py
+++ encryption_at_rest_test.py
@@ -25,53 +25,6 @@ from tools.ldap_docker import running_in_docker
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.single_node
-@pytest.mark.dtest_full
-@pytest.mark.dtest_enterprise
-class TestInMemory(Tester):
-    """
-    Test in memory sstable when Encryption at-rest is enabled.
-    a reproducer for scylla-enterprise/issues/925
-    """
-
-    def test_restart_query(self):
-        self.cluster.set_configuration_options(values={'in_memory_storage_size_mb': 100})
-        self.cluster.populate(1).start(wait_for_binary_proto=True, wait_other_notice=True)
-        node1 = self.cluster.nodelist()[0]
-        session = self.patient_cql_connection(node1)
-        session.execute(
-            "CREATE KEYSPACE rest WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'}")
-        session.execute("""CREATE TABLE rest.table1(key text PRIMARY KEY, name text) WITH scylla_encryption_options =
-            {'key_provider': 'LocalFileSystemKeyProviderFactory', 'secret_key_file': '/tmp/secret_key'} AND
-            in_memory=true AND compaction={'class': 'InMemoryCompactionStrategy'}""")
-
-        session.execute("insert into rest.table1 (key, name) values ('key1', 'name1')")
-        node1.flush()
-        result = session.execute("select * from rest.table1")
-        logger.debug(list(result))
-
-        for node in self.cluster.nodelist():
-            node.stop()
-            node.start(wait_for_binary_proto=True, wait_other_notice=True)
-        session = self.patient_cql_connection(node1)
-        result = session.execute("select * from rest.table1")
-        logger.debug(list(result))
-
-    def test_workload_without_restart(self):
-        self.cluster.set_configuration_options(values={'in_memory_storage_size_mb': 100})
-        self.cluster.populate(1).start(wait_for_binary_proto=True, wait_other_notice=True)
-        node1 = self.cluster.nodelist()[0]
-        session = self.patient_cql_connection(node1)
-        session.execute(
-            "CREATE KEYSPACE keyspace1 WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'}")
-        session.execute("""CREATE TABLE keyspace1.standard1 (key blob PRIMARY KEY,"C0" blob,"C1" blob,"C2" blob,"C3" blob,"C4" blob)
-            WITH scylla_encryption_options = {'key_provider': 'LocalFileSystemKeyProviderFactory', 'secret_key_file': '/tmp/secret_key'} AND
-            in_memory=true AND compaction={'class': 'InMemoryCompactionStrategy'} """)
-        node1.stress(['write', 'n=100000', 'cl=QUORUM', '-rate', 'threads=8'])
-        logger.debug('flushing ...')
-        self.cluster.flush()
-        node1.stress(['read', 'n=100000', 'cl=QUORUM', '-rate', 'threads=8'])
-
 # remove "default", as this is same as None, and gives us a replicated provider
 
 
