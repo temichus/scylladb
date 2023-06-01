d5475ec38d8e676084ebe8310e79716fd5bd3bd1
Jan Ciolek <jan.ciolek@scylladb.com>
2023-06-01 21:18:48 +0200

    repair_additional_test.py: remove SimpleStrategy
    Signed-off-by: Jan Ciolek <jan.ciolek@scylladb.com>

--- repair_additional_test.py
+++ repair_additional_test.py
@@ -2673,7 +2673,7 @@ class RepairAdditionalBase(Tester):
         session = self.patient_cql_connection(node1)
 
         session.execute(
-            "CREATE KEYSPACE ks WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };")
+            "CREATE KEYSPACE ks WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'replication_factor' : 3 };")
         session.execute("CREATE TABLE ks.tb (pk int, ck int, c0 int, c1 int, PRIMARY KEY(pk, ck));")
 
         nr_rows = 3
