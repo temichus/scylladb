47c1200e104f7adc38b6e52b1a1d539235e115c3
Israel Fruchter <fruch@scylladb.com>
2023-07-26 16:22:04 +0300

    upgarde_tests: install old version missing direcotry
    creating `~/.config/systemd/user` since in CI we might
    fail to install old version before 5.1, when they are
    trying to access this direcotry.
    
    after 5.1 those issue were fixed and addressed in
    scylla install.sh.
    
    ```
    install: cannot change permissions of '/jenkins/workspace/scylla-staging/fruch/new-dtest-pytest-parallel/scylla/
      .config/systemd/user/scylla-housekeeping-daily.service': No such file or directory
    ```

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -112,6 +112,7 @@ mkdir -p ${HOME}/.ccm
 mkdir -p ${HOME}/.certs
 chmod 0700 ${HOME}/.certs
 mkdir -p ${HOME}/.config
+mkdir -p ${HOME}/.config/systemd/user
 mkdir -p ${HOME}/.cassandra
 mkdir -p ${HOME}/.cache/pre-commit
 mkdir -p ${HOME}/.docker
