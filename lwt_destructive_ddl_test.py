f4642d80bf59b6696ea33048e05ac677aa4e1aef
igorsimb <igor.simbirtsev@scylladb.com>
2023-08-01 19:06:21 +0300

    lwt_destructive_ddl_test: add tests to next_gating
    All tests were added
    For last 30 days most of the test passed.
    Every test took less than 40 minutes
    No `dtest_heavy` or `dtest_long` marks
    
    The following tests were still added because they only failed as part of a debug job:
    - `test_ks_netw_topology_set_nonexistent_dc`
    - `test_drop_keyspace`

--- lwt_destructive_ddl_test.py
+++ lwt_destructive_ddl_test.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.mark.dtest_full
+@pytest.mark.next_gating
 class TestLwtDestructiveDDL(Tester):
     """
     Destructive DDL in presence of LWT: execute destructive DDL
