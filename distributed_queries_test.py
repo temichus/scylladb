9e74e2d3f064be9ca3597c38d42ece254ad11d08
yarongilor <yarongilor@scylla.com>
2023-08-01 14:17:03 +0300

    distributed_queries_test: add all of TestDistributedAggregations tests to gating
            those test were proven to be stable for the last 30 days promoating them to gating

--- distributed_queries_test.py
+++ distributed_queries_test.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.mark.dtest_full
+@pytest.mark.next_gating
 class TestDistributedAggregations(Tester):
     CS_KEYSPACE = "ks"
     CS_TABLE = "counter_cf"
