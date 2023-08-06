c619b2fa9ba882868e797006aa70cb648204dd20
Kefu Chai <kefu.chai@scylladb.com>
2023-08-06 23:49:40 +0800

    Revert "ttl_test: test uuid_sstable_identifiers_enabled=False"
    This reverts commit c49a324c0be7a0cd0ed3785612a24c426bbc144b.
    
    Now that we use the native tool for printing sstables, there is no
    need to disable uuid_sstable_identifiers_enabled anymore.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

--- ttl_test.py
+++ ttl_test.py
@@ -737,7 +737,6 @@ class TestDistributedTTL(Tester):
     # Removed temporarily from gating de to #13137 / #13276
     # @pytest.mark.next_gating
     @pytest.mark.dtest_debug
-    @pytest.mark.cluster_options(uuid_sstable_identifiers_enabled=False)
     @pytest.mark.parametrize("enable_parallized_aggregation", ['true', 'false'], ids=["parallel_aggregates_enabled", "parallel_aggregates_disabled"])
     def test_ttl_is_respected_on_repair(self, enable_parallized_aggregation):
         """ Test that ttl is respected on repair """
