5ddb63672cc9527e74f02cb04bcff94ff6f10df1
Nadav Har'El <nyh@scylladb.com>
2023-08-14 15:57:21 +0300

    alternator_utils: wait for stream in the right place
    The utility function prefill_dynamodb_table() waits for the DynamoDB
    stream to be enabled, but before this patch did this in the wrong place:
    it waited *after* writing the data in the table, but it should wait
    *before*. If we don't wait before writing, some of the writes may not
    reach the stream - and worse, they may even fail (as noted in
    https://github.com/scylladb/scylladb/pull/6901). We saw this happening
    in practice in several runs of  Closed alternator_stream_tests.py::
    TestAlternatorStreams::test_verify_all_nodes_have_same_stream
    
    So this patch inserts the wait in the middle of prefill_dynamodb_table(),
    after creating the table but before filling it with data.
    
    This bug only recently began to make a difference and cause tests to
    fail, because until recently we have very long unnecessary sleeps when
    dtest boots nodes, and in particular, the wait in prefill_dynamodb_table
    became a no-op (after the long delay, there was nothing to wait for)
    so it didn't matter if we did it in the right place or not.
    
    Fixes #3498.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- alternator_utils.py
+++ alternator_utils.py
@@ -872,10 +872,12 @@ class BaseAlternatorStream(BaseAlternator, CDCInitializeHelper):
 
     def prefill_dynamodb_table(self, node: ScyllaNode, table_name: str = TABLE_NAME, num_of_items: int = NUM_OF_ITEMS,
                                wait_for_active_stream=True, **kwargs):
+        self.create_table(table_name=table_name, node=node, **kwargs)
         stream_arn_details = None
-        table = super().prefill_dynamodb_table(node=node, table_name=table_name, num_of_items=num_of_items, **kwargs)
         if wait_for_active_stream:
-            stream_arn_details = self.wait_for_active_stream(node=node, table_name=table.name)
+            stream_arn_details = self.wait_for_active_stream(node=node, table_name=table_name)
+        new_items = self.create_items(num_of_items=num_of_items)
+        self.batch_write_actions(table_name=table_name, node=node, new_items=new_items)
         return stream_arn_details
 
     @staticmethod
