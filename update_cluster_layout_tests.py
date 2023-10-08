792b08e14582d56750dc44d84e5f4ef7fb8fe149
Israel Fruchter <fruch@scylladb.com>
2023-10-08 15:22:39 +0300

    update_cluster_layout_tests: use specific keyspace
    test_simple_add_new_node_while_schema_changes_with_stream/repair was failing
    a lot with:
    ```
    >   ???
    E   cassandra.InvalidRequest: Error from server: code=2200 [Invalid query] message="No keyspace has been specified. USE a keyspace, or explicitly specify keyspace.tablename"
    ```
    
    we should just use a fully qulified table name, and not count on any side effects
    of `create_ks` that was called before (i.e. it can be called from multiple threads
    and and `USE` isn't neccesscry thread safe

--- update_cluster_layout_tests.py
+++ update_cluster_layout_tests.py
@@ -776,7 +776,7 @@ class TestUpdateClusterLayout(Tester):
 
         session = self.patient_cql_connection(node1)
         create_ks(session, 'ks', rf)
-        create_cf(session, 'cf', read_repair=0.0, columns={'c1': 'text', 'c2': 'text'})
+        create_cf(session, 'ks.cf', read_repair=0.0, columns={'c1': 'text', 'c2': 'text'})
 
         insert_c1c2(session, keys=range(4000), consistency=consistency)
 
@@ -785,7 +785,7 @@ class TestUpdateClusterLayout(Tester):
             list(session.execute(query))
 
             create_ks(session, 'ks1', rf)
-            create_cf(session, 'cf1', read_repair=0.0, columns={'c1': 'text', 'c2': 'text'})
+            create_cf(session, 'ks1.cf1', read_repair=0.0, columns={'c1': 'text', 'c2': 'text'})
             for i in range(0, 100):
                 insert = SimpleStatement("insert into ks1.cf1 (key,c1,c2) values ('%d','%d','%d')" %
                                          (i, i, i), consistency_level=consistency)
