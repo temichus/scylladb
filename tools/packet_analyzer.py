0596c96b1967b52ab3301caa7cade325ee21d1bd
Lukasz Sojka <lukasz.sojka@scylladb.com>
2023-10-20 13:23:07 +0200

    fix(internode-compression-test): fix for no packets captured
    When collected lines from tcpdump don't contain data about packet length
    then it fails on finding max of it. Current protection does not work
    correctly as it could collect some data but none of them fit the packet
    length regexp.
    
    Fix protection by returning [-1] array when no packet matches regexp.
    
    refs: https://github.com/scylladb/scylla-dtest/issues/3612

--- tools/packet_analyzer.py
+++ tools/packet_analyzer.py
@@ -55,10 +55,8 @@ class PacketAnalyzer:
         logger.debug(f"stderr: {self._tcpdump_process.stderr.read()}")
 
     def get_max_packet_length(self):
-        if not self._captured_packets:
-            return -1
         return max([int(packet_length.group(1)) for packet in self._captured_packets
-                    if (packet_length := self.packet_length_regexp.search(packet))])
+                    if (packet_length := self.packet_length_regexp.search(packet))] or [-1])
 
     def get_packets_with_length(self, length):
         return [packet for packet in self._captured_packets
