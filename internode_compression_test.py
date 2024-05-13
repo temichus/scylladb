c2f7a8c87fe0a3415748018b0f304733aa445ef1
Lukasz Sojka <lukasz.sojka@scylladb.com>
2024-05-13 11:36:56 +0200

    fix(internode-compression): measure packets in 2 directions
    Test was measuring packet transmission from node1 to node2 but it may
    happen that row was transferred from node2 to node1.
    
    Measuring both directions for improved test stability.
    
    fixes: https://github.com/scylladb/scylla-dtest/issues/3725
    (cherry picked from commit 8f68cc3006f2ce12ec4f909c07bee51c4670d383)

--- internode_compression_test.py
+++ internode_compression_test.py
@@ -1,18 +1,20 @@
+import logging
 import time
 
 import pytest
 from cassandra import ConsistencyLevel
-from cassandra.cluster import DCAwareRoundRobinPolicy
+from cassandra.policies import WhiteListRoundRobinPolicy
 from cassandra.query import SimpleStatement
 
 from dtest_class import Tester, create_ks
 from tools.packet_analyzer import PacketAnalyzer
 
+logger = logging.getLogger(__name__)
+
 
 @pytest.mark.dtest_full
 class TestInternodeCompression(Tester):
-
-    def test_internode_compression_compress_packets_between_nodes(self):
+    def test_internode_compression_compress_packets_between_nodes(self) -> None:
         """
         Verify that internode compression compress internode packets.
         """
@@ -22,34 +24,43 @@ class TestInternodeCompression(Tester):
         node1, node2 = cluster.nodelist()
 
         session = self.patient_cql_connection(node1)
-        create_ks(session, 'ks', 2)
+        create_ks(session, "ks", 2)
         session.execute("CREATE TABLE ks.cf (key int PRIMARY KEY, val TEXT)")
 
-        packet_analyzer_1 = PacketAnalyzer(node1, node2)
-        packet_analyzer_2 = PacketAnalyzer(node2, node1)  # other direction
+        msg_size = 8192
+        insert_stmt = session.prepare("insert into ks.cf (key, val) values (?, ?)")
+        insert_stmt.consistency_level = ConsistencyLevel.ALL
 
-        packet_analyzer_1.start()
-        packet_analyzer_2.start()
+        def insert_once():
+            session.execute(insert_stmt, [1, "1" * msg_size])
 
-        # insert >8kb size (compressible) row and wait for tcpdump to analyze packets for several seconds
-        session.execute(
-            SimpleStatement(f"insert into ks.cf (key, val) values (1, '{'1' * 8192}')",
-                            consistency_level=ConsistencyLevel.ALL))
-        session.shutdown()
-        time.sleep(1)  # wait for tcpdump to print packets
-        packet_analyzer_1.stop()
-        packet_analyzer_2.stop()
+        while True:
+            packet_analyzer_1 = PacketAnalyzer(node1, node2)
+            packet_analyzer_2 = PacketAnalyzer(node2, node1)  # other direction
 
-        biggest_packet_length_1 = packet_analyzer_1.get_max_packet_length()
-        biggest_packet_length_2 = packet_analyzer_2.get_max_packet_length()
-        if biggest_packet_length_1 > biggest_packet_length_2:
-            biggest_packet = packet_analyzer_1.get_packets_with_length(biggest_packet_length_1)
-        else:
-            biggest_packet = packet_analyzer_2.get_packets_with_length(biggest_packet_length_2)
-        assert max(biggest_packet_length_1, biggest_packet_length_2) < 8000, \
-            f"max packet length is bigger than 8192 bytes - compression is not working properly. Packet data: {biggest_packet}"
+            packet_analyzer_1.start()
+            packet_analyzer_2.start()
+            time.sleep(1)  # Let tcpdump start.
 
-    def test_internode_compression_between_datacenters(self):
+            # insert a compressible row of size ~8 kiB
+            insert_once()
+
+            time.sleep(1)  # Let tcpdump print everything it's got.
+            packet_analyzer_1.stop()
+            packet_analyzer_2.stop()
+
+            biggest_packet_length_1 = packet_analyzer_1.get_max_packet_length()
+            biggest_packet_length_2 = packet_analyzer_2.get_max_packet_length()
+
+            actual = max(biggest_packet_length_1, biggest_packet_length_2)
+            expected = msg_size / 2
+            if actual > expected:
+                logger.info(
+                    f"RPC frame wasn't compressed. Max expected size: {expected}, actual size: {actual}. Retrying.")
+                continue
+            break
+
+    def test_internode_compression_between_datacenters(self) -> None:
         """
         Verify that compression between datacenters is compressed if internode_compression is set to dc and
         not compressed for intra-dc communication.
@@ -60,37 +71,51 @@ class TestInternodeCompression(Tester):
 
         node1, node2 = cluster.nodelist()[:2]
         node3 = cluster.nodelist()[2]
-        session = self.patient_cql_connection(node1, load_balancing_policy=DCAwareRoundRobinPolicy(local_dc='dc1'))
-        create_ks(session, 'ks', {'dc1': 2, 'dc2': 1})
+        session = self.patient_cql_connection(node1, load_balancing_policy=WhiteListRoundRobinPolicy([node1.address()]))
+        create_ks(session, "ks", {"dc1": 2, "dc2": 1})
         session.execute("CREATE TABLE ks.cf (key int PRIMARY KEY, val TEXT)")
 
-        # start tcpdump sniffing on traffic between node1 and node2 on port 7000
-        intra_dc_packet_analyzer_1 = PacketAnalyzer(node1, node2)
-        intra_dc_packet_analyzer_2 = PacketAnalyzer(node2, node1)
+        msg_size = 8192
+        insert_stmt = session.prepare("insert into ks.cf (key, val) values (?, ?)")
+        insert_stmt.consistency_level = ConsistencyLevel.ALL
 
-        dc_packet_analyzer_1 = PacketAnalyzer(node1, node3)
-        dc_packet_analyzer_2 = PacketAnalyzer(node2, node3)
-        intra_dc_packet_analyzer_1.start()
-        intra_dc_packet_analyzer_2.start()
-        dc_packet_analyzer_1.start()
-        dc_packet_analyzer_2.start()
-        # insert >1kb size (compressible) row and wait for tcpdump to analyze packets for several seconds
-        session.execute(
-            SimpleStatement(f"insert into ks.cf (key, val) values (1, '{'1' * 8192}')",
-                            consistency_level=ConsistencyLevel.ALL))
-        session.shutdown()
-        time.sleep(1)  # wait for tcpdump to print packets
-        intra_dc_packet_analyzer_1.stop()
-        intra_dc_packet_analyzer_2.stop()
-        dc_packet_analyzer_1.stop()
-        dc_packet_analyzer_2.stop()
-        biggest_packet_length_1 = dc_packet_analyzer_1.get_max_packet_length()
-        biggest_packet_length_2 = dc_packet_analyzer_2.get_max_packet_length()
-        if biggest_packet_length_1 > biggest_packet_length_2:
-            biggest_packet = dc_packet_analyzer_1.get_packets_with_length(biggest_packet_length_1)
-        else:
-            biggest_packet = dc_packet_analyzer_2.get_packets_with_length(biggest_packet_length_2)
-        assert max(intra_dc_packet_analyzer_1.get_max_packet_length(), intra_dc_packet_analyzer_2.get_max_packet_length()) > 8192, \
-            "intra-datacenter was compressed but shouldn't be"
-        assert max(biggest_packet_length_1, biggest_packet_length_2) < 8000, \
-            f"between datacenters communication should be compressed but it wasn't. Packet data: {biggest_packet}"
+        def insert_once():
+            session.execute(insert_stmt, [1, "1" * msg_size])
+
+        while True:
+            # start tcpdump sniffing on traffic between node1 and node2 on port 7000
+            intra_dc_packet_analyzer_1 = PacketAnalyzer(node1, node2)
+            intra_dc_packet_analyzer_2 = PacketAnalyzer(node2, node1)
+
+            dc_packet_analyzer_1 = PacketAnalyzer(node1, node3)
+            dc_packet_analyzer_2 = PacketAnalyzer(node2, node3)
+
+            intra_dc_packet_analyzer_1.start()
+            intra_dc_packet_analyzer_2.start()
+
+            dc_packet_analyzer_1.start()
+            dc_packet_analyzer_2.start()
+            time.sleep(1)  # Let tcpdump start.
+
+            # insert a compressible row of size ~8 kiB
+            insert_once()
+
+            time.sleep(1)  # Let tcpdump print everything it's got.
+            intra_dc_packet_analyzer_1.stop()
+            intra_dc_packet_analyzer_2.stop()
+            dc_packet_analyzer_1.stop()
+            dc_packet_analyzer_2.stop()
+
+            biggest_packet_length_1 = dc_packet_analyzer_1.get_max_packet_length()
+            biggest_packet_length_2 = dc_packet_analyzer_2.get_max_packet_length()
+            max_intra_packet_length = max(intra_dc_packet_analyzer_1.get_max_packet_length(),
+                                          intra_dc_packet_analyzer_2.get_max_packet_length())
+
+            expected = msg_size / 2
+            actual = max(biggest_packet_length_1, biggest_packet_length_2)
+            if actual > expected:
+                logger.info(
+                    f"An inter-DC RPC message wasn't compressed. Max expected size: {expected}, actual size: {actual}. Retrying.")
+                continue
+            assert max_intra_packet_length > msg_size, f"An intra-DC RPC message was compressed unexpectedly. Min expected size: {msg_size}, actual size: {max_intra_packet_length}"
+            break
